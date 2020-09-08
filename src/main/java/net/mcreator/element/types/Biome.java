93d5d1ab5e1b52d80b9321d91609c25a39d6904d
Goldorion <toutankamon@outlook.com>
2020-09-08 07:39:53 -0400

    Remove generateLakes

--- src/main/java/net/mcreator/element/types/Biome.java
+++ src/main/java/net/mcreator/element/types/Biome.java
@@ -38,7 +38,6 @@ import java.util.List;
 	public String name;
 	public MItemBlock groundBlock;
 	public MItemBlock undergroundBlock;
-	public boolean generateLakes;
 
 	public int treeType;
 	public String vanillaTreeType;
