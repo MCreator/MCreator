e6ee004e5c5e0b8ba171228898e39aaf0de8103d
AlsoSomeoneElse <2000someoneelse@gmail.com>
2020-09-27 21:28:07 +0200

    Added a "Set event result" procedure block

--- src/main/java/net/mcreator/blockly/java/BlocklyToJava.java
+++ src/main/java/net/mcreator/blockly/java/BlocklyToJava.java
@@ -67,6 +67,7 @@ public abstract class BlocklyToJava extends BlocklyToCode {
 		blockGenerators.add(new NullBlock());
 		blockGenerators.add(new MCItemBlock());
 		blockGenerators.add(new CancelEventBlock());
+		blockGenerators.add(new SetEventResultBlock());
 		blockGenerators.add(new SetVariableBlock());
 		blockGenerators.add(new GetVariableBlock());
 		blockGenerators.add(new ReturnBlock());
