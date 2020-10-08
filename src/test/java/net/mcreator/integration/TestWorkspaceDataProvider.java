89a346d1afbe8d909b09a9f427e1775ffb9795bf
KlemenDEV <klemen.pylo@gmail.com>
2020-10-08 16:01:49 +0200

    Improved custom fluid lake spawning - Fixes #116

--- src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
+++ src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -367,6 +367,19 @@ public class TestWorkspaceDataProvider {
 			fluid.onEntityCollides = new Procedure("procedure4");
 			fluid.type = _true ? "WATER" : "LAVA";
 			fluid.spawnWorldTypes = new ArrayList<>(Arrays.asList("Nether", "End"));
+			fluid.restrictionBiomes = new ArrayList<>();
+			if (!emptyLists) {
+				fluid.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(),
+						ListUtils.getRandomItem(random, ElementUtil.loadAllBiomes(modElement.getWorkspace()))));
+				fluid.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(),
+						ListUtils.getRandomItem(random, ElementUtil.loadAllBiomes(modElement.getWorkspace()))));
+				fluid.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(),
+						ListUtils.getRandomItem(random, ElementUtil.loadAllBiomes(modElement.getWorkspace()))));
+				fluid.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(),
+						ListUtils.getRandomItem(random, ElementUtil.loadAllBiomes(modElement.getWorkspace()))));
+			}
+			fluid.frequencyOnChunks = 13;
+			fluid.generateCondition = emptyLists ? null : new Procedure("condition1");
 			return fluid;
 		case FOOD:
 			Food food = new Food(modElement);
@@ -835,6 +848,16 @@ public class TestWorkspaceDataProvider {
 			plant.flammability = 5;
 			plant.fireSpreadSpeed = 12;
 			plant.restrictionBiomes = new ArrayList<>();
+			if (!emptyLists) {
+				plant.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(),
+						ListUtils.getRandomItem(random, ElementUtil.loadAllBiomes(modElement.getWorkspace()))));
+				plant.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(),
+						ListUtils.getRandomItem(random, ElementUtil.loadAllBiomes(modElement.getWorkspace()))));
+				plant.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(),
+						ListUtils.getRandomItem(random, ElementUtil.loadAllBiomes(modElement.getWorkspace()))));
+				plant.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(),
+						ListUtils.getRandomItem(random, ElementUtil.loadAllBiomes(modElement.getWorkspace()))));
+			}
 			plant.onNeighbourBlockChanges = new Procedure("procedure1");
 			plant.onTickUpdate = new Procedure("procedure2");
 			plant.onDestroyedByPlayer = new Procedure("procedure3");
@@ -845,7 +868,7 @@ public class TestWorkspaceDataProvider {
 			plant.onBlockAdded = new Procedure("procedure8");
 			plant.onBlockPlacedBy = new Procedure("procedure9");
 			plant.onRandomUpdateEvent = new Procedure("procedure10");
-			plant.generateCondition = new Procedure("condition1");
+			plant.generateCondition = emptyLists ? null : new Procedure("condition1");
 			plant.renderType = 12;
 			plant.customModelName = "Cross model";
 			return plant;
