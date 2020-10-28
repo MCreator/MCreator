79cf3d16179bccba887e9bbdf0789f1177ba843b
AlsoSomeoneElse <2000someoneelse@gmail.com>
2020-10-28 18:39:01 +0100

    Minor fix

--- src/main/java/net/mcreator/ui/modgui/ItemGUI.java
+++ src/main/java/net/mcreator/ui/modgui/ItemGUI.java
@@ -154,7 +154,7 @@ public class ItemGUI extends ModElementGUI<Item> {
 				Dependency.fromString("x:number/y:number/z:number/world:world/entity:entity/itemstack:itemstack"));
 		dispenseSuccessCondition = new ProcedureSelector(this.withEntry("item/dispense_success_condition"), mcreator,
 				L10N.t("elementgui.item.dispense_success_condition"), VariableElementType.LOGIC,
-				Dependency.fromString("x:number/y:number/z:number/world:world/direction:direction"));
+				Dependency.fromString("x:number/y:number/z:number/world:world/itemstack:itemstack/direction:direction"));
 		dispenseResultItemstack = new ProcedureSelector(this.withEntry("item/dispense_result_stack"), mcreator,
 				L10N.t("elementgui.item.dispense_result_itemstack"), VariableElementType.ITEMSTACK,
 				Dependency.fromString("x:number/y:number/z:number/world:world/itemstack:itemstack/direction:direction/success:boolean"))
