314e6e7e5ecb686eb38e55a70f03f8b6c8e76d56
Max094Reikeb <maxencebekier@gmail.com>
2020-11-16 12:28:35 +0100

    Externalized AI conditions and NewModElement dialogs

--- src/main/java/net/mcreator/ui/dialogs/AIConditionEditor.java
+++ src/main/java/net/mcreator/ui/dialogs/AIConditionEditor.java
@@ -22,6 +22,7 @@ import net.mcreator.blockly.data.Dependency;
 import net.mcreator.ui.MCreator;
 import net.mcreator.ui.component.util.PanelUtils;
 import net.mcreator.ui.help.IHelpContext;
+import net.mcreator.ui.init.L10N;
 import net.mcreator.ui.minecraft.ProcedureSelector;
 import net.mcreator.workspace.elements.VariableElementType;
 import org.jetbrains.annotations.Nullable;
@@ -34,17 +35,17 @@ public class AIConditionEditor {
 
 	public static List<String> open(MCreator parent, @Nullable String[] data) {
 		ProcedureSelector startCondition = new ProcedureSelector(
-				IHelpContext.NONE.withEntry("entity/ai_start_condition"), parent, "Additional start condition",
+				IHelpContext.NONE.withEntry("entity/ai_start_condition"), parent, L10N.t("dialog.ai_condition.additional_start"),
 				ProcedureSelector.Side.BOTH, false, VariableElementType.LOGIC,
 				Dependency.fromString("x:number/y:number/z:number/world:world/entity:entity"));
 
 		ProcedureSelector continueCondition = new ProcedureSelector(
-				IHelpContext.NONE.withEntry("entity/ai_continue_condition"), parent, "Additional continue condition",
+				IHelpContext.NONE.withEntry("entity/ai_continue_condition"), parent, L10N.t("dialog.ai_condition.additional_continue"),
 				ProcedureSelector.Side.BOTH, false, VariableElementType.LOGIC,
 				Dependency.fromString("x:number/y:number/z:number/world:world/entity:entity"));
 
-		startCondition.setDefaultName("(no additional condition)").refreshList();
-		continueCondition.setDefaultName("(no additional condition)").refreshList();
+		startCondition.setDefaultName(L10N.t("dialog.ai_condition.no_additional")).refreshList();
+		continueCondition.setDefaultName(L10N.t("dialog.ai_condition.no_additional")).refreshList();
 
 		if (data != null && data.length == 2) {
 			startCondition.setSelectedProcedure(data[0]);
@@ -52,7 +53,7 @@ public class AIConditionEditor {
 		}
 
 		JOptionPane pane = new JOptionPane(PanelUtils.gridElements(1, 2, 10, 10, startCondition, continueCondition));
-		JDialog dialog = pane.createDialog(parent, "AI condition editor");
+		JDialog dialog = pane.createDialog(parent, L10N.t("dialog.ai_condition.panel_name"));
 		dialog.setVisible(true);
 
 		return Arrays.asList(startCondition.getSelectedProcedure() != null ?
