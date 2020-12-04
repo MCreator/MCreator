1bcb45be2a0ae55dddd9705de7a69835da1e6a82
Max094Reikeb <maxencebekier@gmail.com>
2020-12-04 11:04:20 +0100

    Changed every "Potion" into "PotionEffect"

---
+++ plugins/generator-1.16.4/forge-1.16.4/templates/potioneffect.java.ftl
@@ -0,0 +1,133 @@
+<#--
+ # MCreator (https://mcreator.net/)
+ # Copyright (C) 2020 Pylo and contributors
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ #
+ # Additional permission for code generator templates (*.ftl files)
+ #
+ # As a special exception, you may create a larger work that contains part or
+ # all of the MCreator code generator templates (*.ftl files) and distribute
+ # that work under terms of your choice, so long as that work isn't itself a
+ # template for code generation. Alternatively, if you modify or redistribute
+ # the template itself, you may (at your option) remove this special exception,
+ # which will cause the template and the resulting code generator output files
+ # to be licensed under the GNU General Public License without this special
+ # exception.
+-->
+
+<#-- @formatter:off -->
+<#include "mcitems.ftl">
+<#include "procedures.java.ftl">
+
+package ${package}.potion;
+
+@${JavaModName}Elements.ModElement.Tag public class ${name}Potion extends ${JavaModName}Elements.ModElement{
+
+	@ObjectHolder("${modid}:${registryname}")
+	public static final Effect potion = null;
+
+	public ${name}Potion (${JavaModName}Elements instance) {
+		super(instance, ${data.getModElement().getSortID()});
+
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
+
+	@SubscribeEvent public void registerEffect(RegistryEvent.Register<Effect> event) {
+		event.getRegistry().register(new EffectCustom());
+	}
+
+	public static class EffectCustom extends Effect {
+
+		private final ResourceLocation potionIcon;
+
+		public EffectCustom() {
+			super(EffectType.<#if data.isBad>HARMFUL<#elseif data.isBenefitical>BENEFICIAL<#else>NEUTRAL</#if>, ${data.color.getRGB()});
+			setRegistryName("${registryname}");
+			potionIcon = new ResourceLocation("${modid}:textures/${data.icon}");
+		}
+
+		@Override public String getName() {
+      		return "effect.${registryname}";
+   		}
+
+		@Override public boolean isBeneficial() {
+        	return ${data.isBenefitical};
+    	}
+
+		@Override public boolean isInstant() {
+        	return ${data.isInstant};
+    	}
+
+   	 	@Override public boolean shouldRenderInvText(EffectInstance effect) {
+    	    return ${data.renderStatusInInventory};
+    	}
+
+		@Override public boolean shouldRender(EffectInstance effect) {
+			return ${data.renderStatusInInventory};
+		}
+
+    	@Override public boolean shouldRenderHUD(EffectInstance effect) {
+    	    return ${data.renderStatusInHUD};
+    	}
+
+		<#if hasProcedure(data.onStarted)>
+			<#if data.isInstant>
+			@Override public void affectEntity(Entity source, Entity indirectSource, LivingEntity entity, int amplifier, double health) {
+				World world = entity.world;
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+				<@procedureOBJToCode data.onStarted/>
+			}
+			<#else>
+			@Override public void applyAttributesModifiersToEntity(LivingEntity entity, AttributeModifierManager attributeMapIn, int amplifier) {
+				World world = entity.world;
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+				<@procedureOBJToCode data.onStarted/>
+			}
+			</#if>
+		</#if>
+
+		<#if hasProcedure(data.onActiveTick)>
+		@Override public void performEffect(LivingEntity entity, int amplifier) {
+			World world = entity.world;
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			<@procedureOBJToCode data.onActiveTick/>
+		}
+		</#if>
+
+    	<#if hasProcedure(data.onExpired)>
+		@Override public void removeAttributesModifiersFromEntity(LivingEntity entity, AttributeModifierManager attributeMapIn, int amplifier) {
+    		super.removeAttributesModifiersFromEntity(entity, attributeMapIn, amplifier);
+    		World world = entity.world;
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			<@procedureOBJToCode data.onExpired/>
+		}
+		</#if>
+
+		@Override public boolean isReady(int duration, int amplifier) {
+    		return true;
+    	}
+
+	}
+
+}
+<#-- @formatter:on -->
---
+++ src/main/java/net/mcreator/ui/modgui/PotionEffectGUI.java
@@ -0,0 +1,239 @@
+/*
+ * MCToolkit (https://mctoolkit.net/)
+ * Copyright (C) 2020 MCToolkit and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.mcreator.ui.modgui;
+
+import net.mcreator.blockly.data.Dependency;
+import net.mcreator.element.types.PotionEffect;
+import net.mcreator.ui.MCreator;
+import net.mcreator.ui.MCreatorApplication;
+import net.mcreator.ui.component.JColor;
+import net.mcreator.ui.component.SearchableComboBox;
+import net.mcreator.ui.component.util.ComboBoxUtil;
+import net.mcreator.ui.component.util.ComponentUtils;
+import net.mcreator.ui.component.util.PanelUtils;
+import net.mcreator.ui.dialogs.GeneralTextureSelector;
+import net.mcreator.ui.dialogs.TextureImportDialogs;
+import net.mcreator.ui.help.HelpUtils;
+import net.mcreator.ui.init.L10N;
+import net.mcreator.ui.init.UIRES;
+import net.mcreator.ui.laf.renderer.WTextureComboBoxRenderer;
+import net.mcreator.ui.minecraft.ProcedureSelector;
+import net.mcreator.ui.validation.AggregatedValidationResult;
+import net.mcreator.ui.validation.ValidationGroup;
+import net.mcreator.ui.validation.Validator;
+import net.mcreator.ui.validation.component.VComboBox;
+import net.mcreator.ui.validation.component.VTextField;
+import net.mcreator.ui.validation.validators.ConditionalTextFieldValidator;
+import net.mcreator.ui.validation.validators.TextFieldValidator;
+import net.mcreator.util.ListUtils;
+import net.mcreator.util.StringUtils;
+import net.mcreator.workspace.elements.ModElement;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class PotionEffectGUI extends ModElementGUI<PotionEffect> {
+
+	private final VTextField effectName = new VTextField(20);
+	private final JColor color = new JColor(mcreator);
+	private final VComboBox<String> icon = new SearchableComboBox<>();
+
+	private final JCheckBox isInstant = L10N.checkbox("elementgui.potioneffect.is_instant");
+	private final JCheckBox isBad = L10N.checkbox("elementgui.potioneffect.is_bad");
+	private final JCheckBox isBenefitical = L10N.checkbox("elementgui.potioneffect.is_benefitical");
+	private final JCheckBox renderStatusInInventory = L10N.checkbox("elementgui.common.enable");
+	private final JCheckBox renderStatusInHUD = L10N.checkbox("elementgui.common.enable");
+
+	private final ValidationGroup page1group = new ValidationGroup();
+
+	private ProcedureSelector onStarted;
+	private ProcedureSelector onActiveTick;
+	private ProcedureSelector onExpired;
+
+	public PotionEffectGUI(MCreator mcreator, ModElement modElement, boolean editingMode) {
+		super(mcreator, modElement, editingMode);
+		this.initGUI();
+		super.finalizeGUI();
+	}
+
+	@Override protected void initGUI() {
+		onStarted = new ProcedureSelector(this.withEntry("potion/when_potion_applied"), mcreator,
+				L10N.t("elementgui.potioneffect.event_potion_applied"),
+				Dependency.fromString("entity:entity/x:number/y:number/z:number/world:world/amplifier:number"));
+		onActiveTick = new ProcedureSelector(this.withEntry("potion/when_active_tick"), mcreator,
+				L10N.t("elementgui.potioneffect.event_potion_tick"),
+				Dependency.fromString("entity:entity/x:number/y:number/z:number/world:world/amplifier:number"));
+		onExpired = new ProcedureSelector(this.withEntry("potion/when_potion_expires"), mcreator,
+				L10N.t("elementgui.potioneffect.event_potion_expires"),
+				Dependency.fromString("entity:entity/x:number/y:number/z:number/world:world/amplifier:number"));
+
+		renderStatusInInventory.setSelected(true);
+		renderStatusInHUD.setSelected(true);
+
+		icon.setRenderer(new WTextureComboBoxRenderer.OtherTextures(mcreator.getWorkspace()));
+
+		icon.setPrototypeDisplayValue("XXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+		JPanel pane3 = new JPanel(new BorderLayout());
+		JPanel pane4 = new JPanel(new BorderLayout());
+
+		JPanel selp = new JPanel(new GridLayout(9, 2, 50, 11));
+
+		ComponentUtils.deriveFont(effectName, 16);
+
+		isInstant.setOpaque(false);
+		isBad.setOpaque(false);
+		isBenefitical.setOpaque(false);
+		renderStatusInInventory.setOpaque(false);
+		renderStatusInHUD.setOpaque(false);
+
+		selp.add(HelpUtils.wrapWithHelpButton(this.withEntry("potion/effect_display_name"),
+				L10N.label("elementgui.potioneffect.display_name")));
+		selp.add(effectName);
+
+		JButton importicontexture = new JButton(UIRES.get("18px.add"));
+		importicontexture.setToolTipText(L10N.t("elementgui.potioneffect.import_icon"));
+		importicontexture.setOpaque(false);
+		importicontexture.addActionListener(e -> {
+			TextureImportDialogs.importTexturesGeneral(mcreator, GeneralTextureSelector.TextureType.OTHER);
+			icon.removeAllItems();
+			icon.addItem("");
+			mcreator.getWorkspace().getFolderManager().getOtherTexturesList().forEach(el -> icon.addItem(el.getName()));
+		});
+
+		selp.add(HelpUtils.wrapWithHelpButton(this.withEntry("potion/color"), L10N.label("elementgui.potioneffect.color")));
+		selp.add(color);
+
+		selp.add(HelpUtils.wrapWithHelpButton(this.withEntry("potion/icon"), L10N.label("elementgui.potioneffect.icon")));
+		selp.add(PanelUtils.centerAndEastElement(icon, importicontexture));
+
+		selp.add(HelpUtils
+				.wrapWithHelpButton(this.withEntry("potion/instant"), L10N.label("elementgui.potioneffect.instant")));
+		selp.add(isInstant);
+
+		selp.add(HelpUtils.wrapWithHelpButton(this.withEntry("potion/bad"), L10N.label("elementgui.potioneffect.bad")));
+		selp.add(isBad);
+
+		selp.add(HelpUtils
+				.wrapWithHelpButton(this.withEntry("potion/benefitical"), L10N.label("elementgui.potioneffect.benefitical")));
+		selp.add(isBenefitical);
+
+		selp.add(HelpUtils.wrapWithHelpButton(this.withEntry("potion/render_in_inventory"),
+				L10N.label("elementgui.potioneffect.render_status_inventory")));
+		selp.add(renderStatusInInventory);
+
+		selp.add(HelpUtils.wrapWithHelpButton(this.withEntry("potion/render_in_hud"),
+				L10N.label("elementgui.potioneffect.render_status_hud")));
+		selp.add(renderStatusInHUD);
+
+		selp.setOpaque(false);
+
+		pane3.add(PanelUtils.totalCenterInPanel(selp));
+		pane3.setOpaque(false);
+
+		JPanel events = new JPanel();
+		events.setLayout(new BoxLayout(events, BoxLayout.PAGE_AXIS));
+		JPanel events2 = new JPanel(new GridLayout(1, 3, 8, 8));
+		events2.setOpaque(false);
+		events2.add(onStarted);
+		events2.add(onActiveTick);
+		events2.add(onExpired);
+		events.add(PanelUtils.join(events2));
+		events.setOpaque(false);
+		pane4.add("Center", PanelUtils.totalCenterInPanel(events));
+		pane4.setOpaque(false);
+		pane4.setOpaque(false);
+
+		icon.setValidator(() -> {
+			if (icon.getSelectedItem() == null || icon.getSelectedItem().equals(""))
+				return new Validator.ValidationResult(Validator.ValidationResultType.ERROR,
+						L10N.t("elementgui.potioneffect.error_effect_needs_icon"));
+			return new Validator.ValidationResult(Validator.ValidationResultType.PASSED, "");
+		});
+		effectName.setValidator(
+				new TextFieldValidator(effectName, L10N.t("elementgui.potioneffect.error_effect_needs_display_name")));
+		effectName.enableRealtimeValidation();
+		page1group.addValidationElement(effectName);
+
+		page1group.addValidationElement(icon);
+
+		addPage(L10N.t("elementgui.common.page_properties"), pane3);
+		addPage(L10N.t("elementgui.common.page_triggers"), pane4);
+	}
+
+	@Override public void reloadDataLists() {
+		super.reloadDataLists();
+
+		onStarted.refreshListKeepSelected();
+		onActiveTick.refreshListKeepSelected();
+		onExpired.refreshListKeepSelected();
+
+		ComboBoxUtil.updateComboBoxContents(icon, ListUtils.merge(Collections.singleton(""),
+				mcreator.getWorkspace().getFolderManager().getOtherTexturesList().stream().map(File::getName)
+						.collect(Collectors.toList())), "");
+	}
+
+	@Override protected AggregatedValidationResult validatePage(int page) {
+		if (page == 0) {
+			return new AggregatedValidationResult(page1group);
+		}
+		return new AggregatedValidationResult.PASS();
+	}
+
+	@Override public void openInEditingMode(PotionEffect potion) {
+		effectName.setText(potion.effectName);
+		icon.setSelectedItem(potion.icon);
+		color.setColor(potion.color);
+		isInstant.setSelected(potion.isInstant);
+		isBad.setSelected(potion.isBad);
+		isBenefitical.setSelected(potion.isBenefitical);
+		renderStatusInInventory.setSelected(potion.renderStatusInInventory);
+		renderStatusInHUD.setSelected(potion.renderStatusInHUD);
+		onStarted.setSelectedProcedure(potion.onStarted);
+		onActiveTick.setSelectedProcedure(potion.onActiveTick);
+		onExpired.setSelectedProcedure(potion.onExpired);
+	}
+
+	@Override public PotionEffect getElementFromGUI() {
+		PotionEffect potion = new PotionEffect(modElement);
+		potion.effectName = effectName.getText();
+		potion.icon = icon.getSelectedItem();
+		potion.color = color.getColor();
+		potion.isInstant = isInstant.isSelected();
+		potion.isBad = isBad.isSelected();
+		potion.isBenefitical = isBenefitical.isSelected();
+		potion.renderStatusInInventory = renderStatusInInventory.isSelected();
+		potion.renderStatusInHUD = renderStatusInHUD.isSelected();
+		potion.onStarted = onStarted.getSelectedProcedure();
+		potion.onActiveTick = onActiveTick.getSelectedProcedure();
+		potion.onExpired = onExpired.getSelectedProcedure();
+		return potion;
+	}
+
+	@Override public @Nullable URI getContextURL() throws URISyntaxException {
+		return new URI(MCreatorApplication.SERVER_DOMAIN + "/wiki/how-make-potion");
+	}
+
+}
