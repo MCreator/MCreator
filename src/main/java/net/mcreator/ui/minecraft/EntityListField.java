555c50309bdf7bc2da3f8e647daf6afc42f75104
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2021-01-07 20:42:29 +0100

    Externalization of ListFields strings (#741)
    * Externalized new ListFields strings
    
    * Changed datalist combobox key
    
    * Modified sound selector key

--- src/main/java/net/mcreator/ui/minecraft/EntityListField.java
+++ src/main/java/net/mcreator/ui/minecraft/EntityListField.java
@@ -24,6 +24,7 @@ import net.mcreator.ui.component.JItemListField;
 import net.mcreator.minecraft.ElementUtil;
 import net.mcreator.ui.MCreator;
 import net.mcreator.ui.component.util.PanelUtils;
+import net.mcreator.ui.init.L10N;
 
 import javax.swing.*;
 import java.util.Collections;
@@ -41,10 +42,10 @@ public class EntityListField extends JItemListField<EntityEntry> {
 	@Override protected List<EntityEntry> getElementsToAdd() {
 		JList<String> vlist = new JList<>(ElementUtil.loadAllEntities(mcreator.getWorkspace()).stream()
 				.map(DataListEntry::getName).toArray(String[]::new));
-		int option = JOptionPane.showOptionDialog(mcreator, PanelUtils.northAndCenterElement(
-				new JLabel("Select entities you would like to add to the list:"), new JScrollPane(vlist)),
-				"Select entities", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
-				null, null, null);
+		int option = JOptionPane.showOptionDialog(mcreator, PanelUtils
+						.northAndCenterElement(L10N.label("dialog.list_field.entity_message"),
+								new JScrollPane(vlist)), L10N.t("dialog.list_field.entity_title"), JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.PLAIN_MESSAGE, null, null, null);
 
 		if (option == JOptionPane.OK_OPTION && vlist.getSelectedValue() != null) {
 			return vlist.getSelectedValuesList().stream().map(e -> new EntityEntry(mcreator.getWorkspace(), e))
