8b93bb13ced97b2e108a46a7d1875eb40e22dcaa
KlemenDEV <klemen.pylo@gmail.com>
2021-01-17 17:08:17 +0100

    Fixed inconsitencies and bugs
    Signed-off-by: KlemenDEV <klemen.pylo@gmail.com>

--- src/main/java/net/mcreator/ui/help/HelpLoader.java
+++ src/main/java/net/mcreator/ui/help/HelpLoader.java
@@ -88,20 +88,20 @@ public class HelpLoader {
 				if (getFromCache(helpContext.getEntry()) != null) {
 					helpString.append(renderer.render(parser.parse(getFromCache(helpContext.getEntry()))));
 				} else {
-					helpString.append(L10N.t("help.loader.no_help_entry",helpContext.getEntry()));
+					helpString.append(L10N.t("help_loader.no_help_entry", helpContext.getEntry()));
 				}
 
 				if (uri != null && helpContext.getContextName() != null) {
-					helpString.append(L10N.t("help.loader.learn_about", uri.toString(), helpContext.getContextName()));
+					helpString.append(L10N.t("help_loader.learn_about", uri.toString(), helpContext.getContextName()));
 				}
 
 				return helpString.toString();
 			} else if (uri != null && helpContext.getContextName() != null) {
-				return L10N.t("help.loader.no_entry_learn", uri.toString(), helpContext.getContextName());
+				return L10N.t("help_loader.no_entry_learn_more", uri.toString(), helpContext.getContextName());
 			}
 		}
 
-		return L10N.t("help.loader.no_help_found");
+		return L10N.t("help_loader.no_help_found");
 	}
 
 }
