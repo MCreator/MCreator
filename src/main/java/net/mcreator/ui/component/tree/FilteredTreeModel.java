97b1bd7444624c9ea808f3398e5a9339d48fb670
KlemenDEV <klemen.pylo@gmail.com>
2021-01-19 19:07:57 +0100

    Filter tree refactor
    Signed-off-by: KlemenDEV <klemen.pylo@gmail.com>

--- src/main/java/net/mcreator/ui/component/tree/FilteredTreeModel.java
+++ src/main/java/net/mcreator/ui/component/tree/FilteredTreeModel.java
@@ -16,7 +16,25 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package net.mcreator.ui.browser;
+/*
+ * MCreator (https://mcreator.net/)
+ * Copyright (C) 2020 Pylo and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.mcreator.ui.component.tree;
 
 import javax.swing.tree.DefaultTreeModel;
 
@@ -25,7 +43,7 @@ public class FilteredTreeModel extends DefaultTreeModel {
 	/**
 	 * @param node the node to apply filtering to
 	 */
-	FilteredTreeModel(ProjectBrowserFilterTreeNode node) {
+	public FilteredTreeModel(FilterTreeNode node) {
 		this(node, null);
 	}
 
@@ -33,7 +51,7 @@ public class FilteredTreeModel extends DefaultTreeModel {
 	 * @param node   the node to apply filtering to
 	 * @param filter the actual filter we will apply
 	 */
-	private FilteredTreeModel(ProjectBrowserFilterTreeNode node, String filter) {
+	private FilteredTreeModel(FilterTreeNode node, String filter) {
 		super(node);
 		if (node != null)
 			node.setFilter(filter);
@@ -41,22 +59,22 @@ public class FilteredTreeModel extends DefaultTreeModel {
 
 	public void setFilter(String filter) {
 		if (root != null) {
-			((ProjectBrowserFilterTreeNode) root).setFilter(filter);
+			((FilterTreeNode) root).setFilter(filter);
 			Object[] path = { root };
 			fireTreeStructureChanged(this, path, null, null);
 		}
 	}
 
 	@Override public int getChildCount(Object parent) {
-		if (parent instanceof ProjectBrowserFilterTreeNode) {
-			return (((ProjectBrowserFilterTreeNode) parent).getChildCount());
+		if (parent instanceof FilterTreeNode) {
+			return (((FilterTreeNode) parent).getChildCount());
 		}
 		return 0;
 	}
 
 	@Override public Object getChild(Object parent, int index) {
-		if (parent instanceof ProjectBrowserFilterTreeNode) {
-			return (((ProjectBrowserFilterTreeNode) parent).getChildAt(index));
+		if (parent instanceof FilterTreeNode) {
+			return (((FilterTreeNode) parent).getChildAt(index));
 		}
 		return null;
 	}
