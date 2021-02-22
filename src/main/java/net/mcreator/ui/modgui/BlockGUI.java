0466ea156fdab11ed11dbeeea18251032d6e5091
Goldorion <38427877+Goldorion@users.noreply.github.com>
2021-02-22 10:40:18 -0500

    Update BlockGUI.java

--- src/main/java/net/mcreator/ui/modgui/BlockGUI.java
+++ src/main/java/net/mcreator/ui/modgui/BlockGUI.java
@@ -180,8 +180,8 @@ public class BlockGUI extends ModElementGUI<Block> {
 	private final JSpinner particleSpawningRadious = new JSpinner(new SpinnerNumberModel(0.5, 0, 2, 0.1f));
 	private final JSpinner particleAmount = new JSpinner(new SpinnerNumberModel(4, 0, 1000, 1));
 	private final JSpinner slipperiness = new JSpinner(new SpinnerNumberModel(0.6, 0.01, 5, 0.1));
-	private final JSpinner speedFactor = new JSpinner(new SpinnerNumberModel(1, 0.01, 1000, 0.1));
-	private final JSpinner jumpFactor = new JSpinner(new SpinnerNumberModel(1, 0.01, 1000, 0.1));
+	private final JSpinner speedFactor = new JSpinner(new SpinnerNumberModel(1, 0, 1000, 0.1));
+	private final JSpinner jumpFactor = new JSpinner(new SpinnerNumberModel(1, -1000, 1000, 0.1));
 
 	private final JComboBox<String> rotationMode = new JComboBox<>(
 			new String[] { "<html>No rotation<br><small>Fixed block orientation",
