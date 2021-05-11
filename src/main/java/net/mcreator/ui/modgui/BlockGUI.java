f21e5ed61a626cb8c121f8fee920c5a32a75c99b
Max094Reikeb <maxencebekier@gmail.com>
2021-05-11 12:59:31 +0200

    Re-modified EndRod blockbase

--- src/main/java/net/mcreator/ui/modgui/BlockGUI.java
+++ src/main/java/net/mcreator/ui/modgui/BlockGUI.java
@@ -228,7 +228,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 
 	private final JComboBox<String> blockBase = new JComboBox<>(
 			new String[] { "Default basic block", "Stairs", "Slab", "Fence", "Wall", "Leaves", "TrapDoor", "Pane",
-					"Door", "FenceGate", "Rod" });
+					"Door", "FenceGate", "EndRod" });
 
 	private final JSpinner flammability = new JSpinner(new SpinnerNumberModel(0, 0, 1024, 1));
 	private final JSpinner fireSpreadSpeed = new JSpinner(new SpinnerNumberModel(0, 0, 1024, 1));
@@ -363,14 +363,14 @@ public class BlockGUI extends ModElementGUI<Block> {
 				rotationMode.setSelectedIndex(0);
 				isWaterloggable.setSelected(false);
 
-				if (blockBase.getSelectedItem().equals("Rod")) {
+				if (blockBase.getSelectedItem().equals("EndRod")) {
 					rotationMode.setEnabled(true);
 					rotationMode.setSelectedIndex(4);
 				}
 
 				if (blockBase.getSelectedItem().equals("Wall") || blockBase.getSelectedItem().equals("Fence")
 						|| blockBase.getSelectedItem().equals("TrapDoor") || blockBase.getSelectedItem().equals("Door")
-						|| blockBase.getSelectedItem().equals("FenceGate") || blockBase.getSelectedItem().equals("Rod")) {
+						|| blockBase.getSelectedItem().equals("FenceGate") || blockBase.getSelectedItem().equals("EndRod")) {
 					if (!isEditingMode()) {
 						hasTransparency.setSelected(true);
 						lightOpacity.setValue(0);
