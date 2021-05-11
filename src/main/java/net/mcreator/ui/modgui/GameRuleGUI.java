bac1da56b52d011c18b4b4c5d89ee1498fef75af
Klemen <klemen.pylo@gmail.com>
2021-05-11 10:41:40 +0200

    Update GameRuleGUI.java

--- src/main/java/net/mcreator/ui/modgui/GameRuleGUI.java
+++ src/main/java/net/mcreator/ui/modgui/GameRuleGUI.java
@@ -144,6 +144,7 @@ public class GameRuleGUI extends ModElementGUI<GameRule> {
 	}
 
 	@Override public void openInEditingMode(GameRule gamerule) {
+		name.setText(gamerule.name);
 		displayName.setText(gamerule.displayName);
 		description.setText(gamerule.description);
 		gameruleCategory.setSelectedItem(gamerule.category);
@@ -158,6 +159,7 @@ public class GameRuleGUI extends ModElementGUI<GameRule> {
 
 	@Override public GameRule getElementFromGUI() {
 		GameRule gamerule = new GameRule(modElement);
+		name.displayName = name.getText();
 		gamerule.displayName = displayName.getText();
 		gamerule.description = description.getText();
 		gamerule.category = (String) gameruleCategory.getSelectedItem();
