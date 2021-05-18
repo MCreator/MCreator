0a289ee43908bbeb48bbb91a62cd89c10ee4d12a
LoicMaitreDuFeu <60476871+LoicMaitreDuFeu@users.noreply.github.com>
2021-05-18 20:06:00 +0400

    Update WorkspacePanel.java

--- src/main/java/net/mcreator/ui/workspace/WorkspacePanel.java
+++ src/main/java/net/mcreator/ui/workspace/WorkspacePanel.java
@@ -862,8 +862,7 @@ import java.util.stream.Collectors;
 		if (!Pattern.matches(".*" + filter + ".*", search.getText())) {
 			search.setText(search.getText() + " " + filter);
 		} else {
-			search.setText(search.getText().replaceAll(" " + filter, ""));
-			search.setText(search.getText().replaceAll("  ", " "));
+			search.setText(search.getText().replaceAll(" " + filter, "").replaceAll(" ", " "));
 		}
 	}
 
