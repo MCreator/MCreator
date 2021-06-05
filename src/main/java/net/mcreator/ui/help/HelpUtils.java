22436271da1c2c6476d125e4cb8226b33b0bab84
Defeatomizer <drag7398@gmail.com>
2021-06-05 22:23:23 +0300

    Fixed varargs parameter position

--- src/main/java/net/mcreator/ui/help/HelpUtils.java
+++ src/main/java/net/mcreator/ui/help/HelpUtils.java
@@ -37,19 +37,19 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class HelpUtils {
 
-	public static Component wrapWithHelpButton(IHelpContext context, @Nullable Object... arguments, Component ca) {
-		return wrapWithHelpButton(context, arguments, ca, null, SwingConstants.RIGHT);
+	public static Component wrapWithHelpButton(IHelpContext context, Component ca, @Nullable Object... context_arguments) {
+		return wrapWithHelpButton(context, ca, null, SwingConstants.RIGHT, context_arguments);
 	}
 
-	public static Component wrapWithHelpButton(IHelpContext context, @Nullable Object... arguments, Component ca, int direction) {
-		return wrapWithHelpButton(context, arguments, ca, null, direction);
+	public static Component wrapWithHelpButton(IHelpContext context, Component ca, int direction, @Nullable Object... context_arguments) {
+		return wrapWithHelpButton(context, ca, null, direction, context_arguments);
 	}
 
-	public static Component wrapWithHelpButton(IHelpContext context, @Nullable Object... arguments, Component ca, @Nullable Color ac) {
-		return wrapWithHelpButton(context, arguments, ca, ac, SwingConstants.RIGHT);
+	public static Component wrapWithHelpButton(IHelpContext context, Component ca, @Nullable Color ac, @Nullable Object... arguments) {
+		return wrapWithHelpButton(context, ca, ac, SwingConstants.RIGHT, context_arguments);
 	}
 
-	public static Component wrapWithHelpButton(IHelpContext context, @Nullable Object... arguments, Component ca, @Nullable Color ac, int direction) {
+	public static Component wrapWithHelpButton(IHelpContext context, Component ca, @Nullable Color ac, int direction, @Nullable Object... arguments) {
 		JLabel lab = new JLabel(HelpLoader.hasFullHelp(context) ? UIRES.get("help") : UIRES.get("help_partial"));
 		lab.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
@@ -74,7 +74,7 @@ public class HelpUtils {
 						}
 					});
 
-					editorPane.setText(HelpLoader.loadHelpFor(context, arguments));
+					editorPane.setText(HelpLoader.loadHelpFor(context, context_arguments));
 
 					JScrollPane scrollPane = new JScrollPane(editorPane);
 					scrollPane.setPreferredSize(new Dimension(335, 190));
