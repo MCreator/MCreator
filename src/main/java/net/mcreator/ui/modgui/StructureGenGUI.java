fbdc87f9973c94f930a1786b66623b107982f3b8
Goldorion <toutankamon@outlook.com>
2021-06-09 18:24:31 -0400

    Revert "Revert "Merge branch 'MCreator:master' into goldo/command-arguments""
    This reverts commit af7496cf414bc845022e5526b7e140cfa27546ef.

--- src/main/java/net/mcreator/ui/modgui/StructureGenGUI.java
+++ src/main/java/net/mcreator/ui/modgui/StructureGenGUI.java
@@ -100,7 +100,7 @@ public class StructureGenGUI extends ModElementGUI<Structure> {
 				L10N.t("elementgui.structuregen.event_additional_structure_condition_is"),
 				ProcedureSelector.Side.SERVER, true, VariableElementTypeLoader.BuiltInTypes.LOGIC,
 				Dependency.fromString("x:number/y:number/z:number/world:world"))
-				.setDefaultName(L10N.t("elementgui.structuregen.event_additional_structure_condition_none"));
+				.setDefaultName(L10N.t("condition.common.no_additional"));
 
 		restrictionBlocks = new MCItemListField(mcreator, ElementUtil::loadBlocks);
 		restrictionBiomes = new BiomeListField(mcreator);
