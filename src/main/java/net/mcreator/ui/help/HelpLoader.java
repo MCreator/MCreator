MCreator/MCreator

commit 6143004249a22c220028d4428ceb15ad23ddab92

Help tips arguments (#1379)
* Help tips now support arguments

* Arguments are already used in some tips

* Fixed varargs parameter position

* Arguments now always pass as an array except the final handling

* Resolved methods' parameters

* An attempt to fix methods mismatch

* Removed unnecessary annotation

* Reverted back to varargs

* Lazy load context + fix in way keybinding was getting keys + improved varargs use

* Moved setting caret position back after setting help text

* Some more improvements

Co-authored-by: KlemenDEV <klemen.pylo@gmail.com>

src/main/java/net/mcreator/ui/help/HelpLoader.java
@@ -21,6 +21,7 @@ package net.mcreator.ui.help;
 import net.mcreator.io.FileIO;
 import net.mcreator.plugin.PluginLoader;
 import net.mcreator.ui.init.L10N;
+import net.mcreator.util.HtmlUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.commonmark.Extension;
 import org.commonmark.ext.autolink.AutolinkExtension;
@@ -32,10 +33,12 @@ import javax.annotation.Nullable;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import java.util.regex.Pattern;
 
 public class HelpLoader {
@@ -72,7 +75,7 @@ public class HelpLoader {
 		return helpContext != null && helpContext.getEntry() != null && getFromCache(helpContext.getEntry()) != null;
 	}
 
-	public static String loadHelpFor(IHelpContext helpContext) {
+	public static String loadHelpFor(IHelpContext helpContext, @Nullable Supplier<?>... contextArguments) {
 		if (helpContext != null) {
 			URI uri = null;
 			try {
@@ -85,8 +88,14 @@ public class HelpLoader {
 							+ "th { border: 1px solid #a0a0a0; } td { border: 1px solid #a0a0a0; } </style></head><body>");
 
 			if (helpContext.getEntry() != null) {
-				if (getFromCache(helpContext.getEntry()) != null) {
-					helpString.append(renderer.render(parser.parse(getFromCache(helpContext.getEntry()))));
+				String helpText = getFromCache(helpContext.getEntry());
+				if (helpText != null) {
+					if (contextArguments != null)
+						helpString.append(renderer.render(parser.parse(MessageFormat.format(helpText,
+								Arrays.stream(contextArguments).map(Supplier::get)
+										.map(e -> e == null ? "" : HtmlUtils.unescapeHtml(e.toString())).toArray()))));
+					else
+						helpString.append(renderer.render(parser.parse(helpText)));
 				} else {
 					helpString.append(L10N.t("help_loader.no_help_entry", helpContext.getEntry()));
 				}

src/main/java/net/mcreator/ui/help/HelpUtils.java
@@ -33,34 +33,53 @@ import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 public class HelpUtils {
 
 	public static Component wrapWithHelpButton(IHelpContext context, Component ca) {
-		return wrapWithHelpButton(context, ca, null, SwingConstants.RIGHT);
+		return wrapWithHelpButton(context, ca, null, SwingConstants.RIGHT, (Supplier<?>) null);
 	}
 
 	public static Component wrapWithHelpButton(IHelpContext context, Component ca, int direction) {
-		return wrapWithHelpButton(context, ca, null, direction);
+		return wrapWithHelpButton(context, ca, null, direction, (Supplier<?>) null);
 	}
 
 	public static Component wrapWithHelpButton(IHelpContext context, Component ca, @Nullable Color ac) {
-		return wrapWithHelpButton(context, ca, ac, SwingConstants.RIGHT);
+		return wrapWithHelpButton(context, ca, ac, SwingConstants.RIGHT, (Supplier<?>) null);
 	}
 
-	public static Component wrapWithHelpButton(IHelpContext context, Component ca, @Nullable Color ac, int direction) {
+	public static Component wrapWithHelpButton(IHelpContext context, Component ca,
+			@Nullable Supplier<?>... contextArguments) {
+		return wrapWithHelpButton(context, ca, null, SwingConstants.RIGHT, contextArguments);
+	}
+
+	public static Component wrapWithHelpButton(IHelpContext context, Component ca, int direction,
+			@Nullable Supplier<?>... contextArguments) {
+		return wrapWithHelpButton(context, ca, null, direction, contextArguments);
+	}
+
+	public static Component wrapWithHelpButton(IHelpContext context, Component ca, @Nullable Color ac,
+			@Nullable Supplier<?>... contextArguments) {
+		return wrapWithHelpButton(context, ca, ac, SwingConstants.RIGHT, contextArguments);
+	}
+
+	public static Component wrapWithHelpButton(IHelpContext context, Component ca, @Nullable Color ac, int direction,
+			@Nullable Supplier<?>... contextArguments) {
 		JLabel lab = new JLabel(HelpLoader.hasFullHelp(context) ? UIRES.get("help") : UIRES.get("help_partial"));
 		lab.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
-		AtomicReference<BalloonTip> balloonTipAtomicReference = new AtomicReference<>();
 		lab.addMouseListener(new MouseAdapter() {
+
+			BalloonTip balloonTip = null;
+			JTextPane editorPane = null;
+
 			@Override public void mouseClicked(MouseEvent e) {
 				super.mouseClicked(e);
 
 				// lazy load tooltip for performance reasons
-				if (balloonTipAtomicReference.get() == null) {
-					JTextPane editorPane = new JTextPane();
+				if (balloonTip == null) {
+					editorPane = new JTextPane();
 
 					editorPane.setContentType("text/html");
 					editorPane.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true);
@@ -74,12 +93,10 @@ public class HelpUtils {
 						}
 					});
 
-					editorPane.setText(HelpLoader.loadHelpFor(context));
-
 					JScrollPane scrollPane = new JScrollPane(editorPane);
 					scrollPane.setPreferredSize(new Dimension(335, 190));
 
-					BalloonTip balloonTip = new BalloonTip(lab, scrollPane,
+					balloonTip = new BalloonTip(lab, scrollPane,
 							new EdgedBalloonStyle((Color) UIManager.get("MCreatorLAF.DARK_ACCENT"),
 									(Color) UIManager.get("MCreatorLAF.GRAY_COLOR")), BalloonTip.Orientation.LEFT_BELOW,
 							BalloonTip.AttachLocation.ALIGNED, 10, 10, false);
@@ -108,16 +125,15 @@ public class HelpUtils {
 					closeButton.setContentAreaFilled(false);
 					closeButton.setIcon(UIRES.get("close_small"));
 					balloonTip.setCloseButton(closeButton, false);
-
-					editorPane.setCaretPosition(0);
-
-					balloonTipAtomicReference.set(balloonTip);
 				}
 
-				balloonTipAtomicReference.get().setVisible(!balloonTipAtomicReference.get().isVisible());
+				editorPane.setText(HelpLoader.loadHelpFor(context, contextArguments));
+				editorPane.setCaretPosition(0);
 
-				if (balloonTipAtomicReference.get().isVisible())
-					balloonTipAtomicReference.get().requestFocus(true);
+				balloonTip.setVisible(!balloonTip.isVisible());
+
+				if (balloonTip.isVisible())
+					balloonTip.requestFocus(true);
 			}
 		});
 

src/main/java/net/mcreator/ui/modgui/BlockGUI.java
@@ -701,7 +701,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 		selp3.add(dropAmount);
 
 		selp3.add(HelpUtils.wrapWithHelpButton(this.withEntry("block/use_loot_table_for_drops"),
-				L10N.label("elementgui.common.use_loot_table_for_drop")));
+				L10N.label("elementgui.common.use_loot_table_for_drop"), modElement::getRegistryName));
 		selp3.add(PanelUtils.centerInPanel(useLootTableForDrops));
 
 		selp3.add(HelpUtils.wrapWithHelpButton(this.withEntry("block/creative_pick_item"),

src/main/java/net/mcreator/ui/modgui/FunctionGUI.java
@@ -81,7 +81,8 @@ public class FunctionGUI extends ModElementGUI<Function> {
 		northPanel.add(name);
 
 		northPanel.add(HelpUtils
-				.wrapWithHelpButton(this.withEntry("function/namespace"), L10N.label("elementgui.function.namespace")));
+				.wrapWithHelpButton(this.withEntry("function/namespace"), L10N.label("elementgui.function.namespace"),
+						modElement.getWorkspace().getWorkspaceSettings()::getModID, modElement::getRegistryName));
 		northPanel.add(namespace);
 
 		RTextScrollPane sp = new RTextScrollPane(te, true);

src/main/java/net/mcreator/ui/modgui/ItemGUI.java
@@ -277,7 +277,7 @@ public class ItemGUI extends ModElementGUI<Item> {
 		subpane2.add(stayInGridWhenCrafting);
 
 		subpane2.add(HelpUtils.wrapWithHelpButton(this.withEntry("item/container_item_damage"),
-				L10N.label("elementgui.item.container_item_damage")));
+				L10N.label("elementgui.item.container_item_damage"), () -> L10N.t("elementgui.item.container_item")));
 		subpane2.add(damageOnCrafting);
 
 		subpane2.add(HelpUtils.wrapWithHelpButton(this.withEntry("item/recipe_remainder"),

src/main/java/net/mcreator/ui/modgui/KeyBindGUI.java
@@ -84,7 +84,9 @@ public class KeyBindGUI extends ModElementGUI<KeyBinding> {
 		enderpanel.add(keyBindingName);
 
 		enderpanel.add(HelpUtils.wrapWithHelpButton(this.withEntry("keybinding/category"),
-				L10N.label("elementgui.keybind.key_binding_category")));
+				L10N.label("elementgui.keybind.key_binding_category"), () -> L10N.t("tab.workspace"),
+				() -> L10N.t("workspace.category.localization"), () -> L10N.t("workspace.localization.add_entry"),
+				keyBindingCategoryKey.getEditor()::getItem));
 		enderpanel.add(PanelUtils.westAndCenterElement(L10N.label("elementgui.keybind.key_binding_category_key"),
 				keyBindingCategoryKey));
 
@@ -116,6 +118,7 @@ public class KeyBindGUI extends ModElementGUI<KeyBinding> {
 
 		keyBindingCategoryKey.setValidator(new RegistryNameValidator(keyBindingCategoryKey,
 				L10N.t("elementgui.keybind.error_key_category_needs_name")));
+		keyBindingCategoryKey.enableRealtimeValidation();
 
 		addPage(pane5);
 

commit 5e8c0e86b6603a7f304c509da0158597a33bc0a8

Help tips without arguments did not load (caused by #1379)

src/main/java/net/mcreator/ui/help/HelpLoader.java
@@ -92,8 +92,7 @@ public class HelpLoader {
 				if (helpText != null) {
 					if (contextArguments != null)
 						helpString.append(renderer.render(parser.parse(MessageFormat.format(helpText,
-								Arrays.stream(contextArguments).map(Supplier::get)
-										.map(e -> e == null ? "" : HtmlUtils.unescapeHtml(e.toString())).toArray()))));
+								Arrays.stream(contextArguments).map(e -> HtmlUtils.unescapeHtml(e.get().toString())).toArray()))));
 					else
 						helpString.append(renderer.render(parser.parse(helpText)));
 				} else {

src/main/java/net/mcreator/ui/help/HelpUtils.java
@@ -38,15 +38,15 @@ import java.util.function.Supplier;
 public class HelpUtils {
 
 	public static Component wrapWithHelpButton(IHelpContext context, Component ca) {
-		return wrapWithHelpButton(context, ca, null, SwingConstants.RIGHT, (Supplier<?>) null);
+		return wrapWithHelpButton(context, ca, null, SwingConstants.RIGHT, () -> "");
 	}
 
 	public static Component wrapWithHelpButton(IHelpContext context, Component ca, int direction) {
-		return wrapWithHelpButton(context, ca, null, direction, (Supplier<?>) null);
+		return wrapWithHelpButton(context, ca, null, direction, () -> "");
 	}
 
 	public static Component wrapWithHelpButton(IHelpContext context, Component ca, @Nullable Color ac) {
-		return wrapWithHelpButton(context, ca, ac, SwingConstants.RIGHT, (Supplier<?>) null);
+		return wrapWithHelpButton(context, ca, ac, SwingConstants.RIGHT, () -> "");
 	}
 
 	public static Component wrapWithHelpButton(IHelpContext context, Component ca,