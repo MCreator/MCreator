d6a32d68ab324490737a4f0080db46a8c275ae38
LoicMaitreDuFeu <60476871+LoicMaitreDuFeu@users.noreply.github.com>
2021-06-14 19:00:49 +0400

    Added a Template Search Bar

--- src/main/java/net/mcreator/ui/dialogs/TextureSelectorDialog.java
+++ src/main/java/net/mcreator/ui/dialogs/TextureSelectorDialog.java
@@ -27,15 +27,23 @@ import net.mcreator.ui.init.UIRES;
 import net.mcreator.util.image.ImageUtils;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class TextureSelectorDialog extends MCreatorDialog {
 
 	public JButton naprej = new JButton(UIManager.getString("OptionPane.okButtonText"));
 
-	private final DefaultListModel<ResourcePointer> model = new DefaultListModel<>();
+	private final FilterModel model = new FilterModel();
+
 	public JList<ResourcePointer> list = new JList<>(model);
+
+	private final JTextField filterField = new JTextField(20);
 
 	public TextureSelectorDialog(Iterable<ResourcePointer> block, JFrame f) {
 		super(f, L10N.t("dialog.textures_selector.title_window"), true);
@@ -53,12 +61,34 @@ public class TextureSelectorDialog extends MCreatorDialog {
 		list.addListSelectionListener(event -> naprej.doClick());
 		naprej2.addActionListener(event -> setVisible(false));
 
+		ComponentUtils.deriveFont(filterField, 12);
+		filterField.getDocument().addDocumentListener(new DocumentListener() {
+			@Override public void removeUpdate(DocumentEvent arg0) {
+				model.refilter();
+			}
+
+			@Override public void insertUpdate(DocumentEvent arg0) {
+				model.refilter();
+			}
+
+			@Override public void changedUpdate(DocumentEvent arg0) {
+				model.refilter();
+			}
+		});
+
+		JPanel pno2 = new JPanel();
+
+		pno2.add(L10N.label("dialog.textures_selector.search"));
+		pno2.add(filterField);
+
 		JLabel jtf = L10N.label("dialog.textures_selector.select_texture");
 		ComponentUtils.deriveFont(jtf, 17);
+		jtf.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
 
 		add("North", PanelUtils.join(FlowLayout.LEFT, jtf));
 		add("South", buttons);
 		add("Center", new JScrollPane(list));
+		add("North", PanelUtils.westAndEastElement(jtf, PanelUtils.totalCenterInPanel(pno2)));
 
 		setSize(740, 370);
 		setLocationRelativeTo(f);
@@ -90,5 +120,45 @@ public class TextureSelectorDialog extends MCreatorDialog {
 			return this;
 		}
 	}
+	private class FilterModel extends DefaultListModel<ResourcePointer> {
+		ArrayList<ResourcePointer> items;
+		ArrayList<ResourcePointer> filterItems;
 
+		FilterModel() {
+			super();
+			items = new ArrayList<>();
+			filterItems = new ArrayList<>();
+		}
+		@Override public ResourcePointer getElementAt(int index) {
+			if (index < filterItems.size())
+				return filterItems.get(index);
+			else
+				return null;
+		}
+
+		@Override public int getSize() {
+			return filterItems.size();
+		}
+
+		@Override public void addElement(ResourcePointer o) {
+			items.add(o);
+			refilter();
+		}
+
+		@Override public boolean removeElement(Object a) {
+			if (a instanceof ResourcePointer) {
+				items.remove(a);
+				filterItems.remove(a);
+			}
+			return super.removeElement(a);
+		}
+
+		private void refilter() {
+			filterItems.clear();
+			String term = filterField.getText();
+			filterItems.addAll(items.stream().filter(item -> item.toString().toLowerCase(Locale.ENGLISH)
+					.contains(term.toLowerCase(Locale.ENGLISH))).collect(Collectors.toList()));
+			fireContentsChanged(this, 0, getSize());
+		}
+	}
 }
