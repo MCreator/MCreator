96d750573ee1939e00e1a35f259322443ab6f5d1
Defeatomizer <drag7398@gmail.com>
2021-06-19 19:33:26 +0300

    One more fix for new mod element registry name label (#1428)
    * Fixed new mod element registry name label again
    
    * Added checking for an empty string along with null

--- src/main/java/net/mcreator/ui/dialogs/NewModElementDialog.java
+++ src/main/java/net/mcreator/ui/dialogs/NewModElementDialog.java
@@ -49,8 +49,10 @@ public class NewModElementDialog {
 						L10N.t("dialog.new_modelement.title_window", type.getReadableName()),
 						TiledImageCache.getModTypeIcon(type), new OptionPaneValidatior() {
 							@Override public Validator.ValidationResult validate(JComponent component) {
-								regName.setText(L10N.t("dialog.new_modelement.registry_name", RegistryNameFixer
-										.fromCamelCase(((VTextField) component).getText())));
+								String regNameString = RegistryNameFixer.fromCamelCase(((VTextField) component).getText());
+								regName.setText(L10N.t("dialog.new_modelement.registry_name", regNameString == null || regNameString.equals("") ?
+										L10N.t("dialog.new_modelement.registry_name.empty") :
+										regNameString));
 								return new ModElementNameValidator(mcreator.getWorkspace(), (VTextField) component)
 										.validate();
 							}
