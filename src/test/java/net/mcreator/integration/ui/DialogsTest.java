cccaf1568a7b4c9b539be6291e125a2bf1b5225b
KlemenDEV <klemen.pylo@gmail.com>
2021-06-26 14:55:44 +0200

    More dialog tests

--- src/test/java/net/mcreator/integration/ui/DialogsTest.java
+++ src/test/java/net/mcreator/integration/ui/DialogsTest.java
@@ -19,6 +19,7 @@
 
 package net.mcreator.integration.ui;
 
+import net.mcreator.element.ModElementType;
 import net.mcreator.generator.Generator;
 import net.mcreator.generator.GeneratorConfiguration;
 import net.mcreator.generator.GeneratorFlavor;
@@ -27,14 +28,14 @@ import net.mcreator.minecraft.ElementUtil;
 import net.mcreator.ui.MCreator;
 import net.mcreator.ui.action.impl.AboutAction;
 import net.mcreator.ui.blockly.BlocklyPanel;
-import net.mcreator.ui.dialogs.BlockItemTextureSelector;
-import net.mcreator.ui.dialogs.ElementOrderEditor;
-import net.mcreator.ui.dialogs.MCItemSelectorDialog;
-import net.mcreator.ui.dialogs.TextureImportDialogs;
+import net.mcreator.ui.dialogs.*;
 import net.mcreator.ui.dialogs.preferences.PreferencesDialog;
+import net.mcreator.ui.dialogs.tools.*;
 import net.mcreator.ui.dialogs.workspace.GeneratorSelector;
 import net.mcreator.ui.dialogs.workspace.NewWorkspaceDialog;
+import net.mcreator.ui.dialogs.wysiwyg.*;
 import net.mcreator.ui.workspace.selector.WorkspaceSelector;
+import net.mcreator.ui.wysiwyg.WYSIWYGEditor;
 import net.mcreator.workspace.Workspace;
 import net.mcreator.workspace.settings.WorkspaceSettings;
 import org.apache.logging.log4j.LogManager;
@@ -126,6 +127,64 @@ public class DialogsTest {
 		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> TextureImportDialogs
 				.importTexturesBlockOrItem(mcreator, BlockItemTextureSelector.TextureType.ITEM));
 		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> TextureImportDialogs.importOtherTextures(mcreator));
+	}
+
+	@Test public void testToolsDialogs() throws Throwable {
+		UITestUtil.waitUntilWindowIsOpen(mcreator,
+				() -> ArmorPackMakerTool.getAction(mcreator.actionRegistry).doAction());
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> InjectTagsTool.getAction(mcreator.actionRegistry).doAction());
+		UITestUtil.waitUntilWindowIsOpen(mcreator,
+				() -> MaterialPackMakerTool.getAction(mcreator.actionRegistry).doAction());
+		UITestUtil
+				.waitUntilWindowIsOpen(mcreator, () -> OrePackMakerTool.getAction(mcreator.actionRegistry).doAction());
+		UITestUtil
+				.waitUntilWindowIsOpen(mcreator, () -> ToolPackMakerTool.getAction(mcreator.actionRegistry).doAction());
+		UITestUtil
+				.waitUntilWindowIsOpen(mcreator, () -> WoodPackMakerTool.getAction(mcreator.actionRegistry).doAction());
+	}
+
+	@Test public void testWYSIWYGDialogs() throws Throwable {
+		WYSIWYGEditor editor = new WYSIWYGEditor(mcreator, true);
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> new ButtonDialog(editor, null));
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> new CheckboxDialog(editor, null));
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> new ImageDialog(editor, null));
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> new InputSlotDialog(editor, null));
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> new LabelDialog(editor, null));
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> new OutputSlotDialog(editor, null));
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> new TextFieldDialog(editor, null));
+	}
+
+	@Test public void testAIConditionEditor() throws Throwable {
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> AIConditionEditor.open(mcreator, null));
+	}
+
+	@Test public void testFileDialogs() throws Throwable {
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> FileDialogs
+				.getWorkspaceDirectorySelectDialog(mcreator, new File(System.getProperty("user.home"))));
+		UITestUtil.waitUntilWindowIsOpen(mcreator,
+				() -> FileDialogs.getMultiOpenDialog(mcreator, new String[] { ".png" }));
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> FileDialogs.getOpenDialog(mcreator, new String[] { ".png" }));
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> FileDialogs.getSaveDialog(mcreator, new String[] { ".png" }));
+	}
+
+	@Test public void testSoundElementDialog() throws Throwable {
+		UITestUtil.waitUntilWindowIsOpen(mcreator, () -> SoundElementDialog.soundDialog(mcreator, null, null));
+	}
+
+	@Test public void testNewModElementDialog() throws Throwable {
+		UITestUtil.waitUntilWindowIsOpen(mcreator,
+				() -> NewModElementDialog.showNameDialog(mcreator, ModElementType.BLOCK));
+		UITestUtil.waitUntilWindowIsOpen(mcreator,
+				() -> NewModElementDialog.showNameDialog(mcreator, ModElementType.CODE));
+	}
+
+	@Test public void testBlockItemTextureSelector() throws Throwable {
+		UITestUtil.waitUntilWindowIsOpen(mcreator,
+				() -> new BlockItemTextureSelector(mcreator, BlockItemTextureSelector.TextureType.BLOCK)
+						.setVisible(true));
+		UITestUtil.waitUntilWindowIsOpen(mcreator,
+				() -> new BlockItemTextureSelector(mcreator, BlockItemTextureSelector.TextureType.ITEM)
+						.setVisible(true));
 	}
 
 }
