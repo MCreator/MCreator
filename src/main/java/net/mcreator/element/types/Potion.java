ffaec9ae8bbde907a08a339b314c123973fe3e66
KlemenDEV <klemen.pylo@gmail.com>
2021-06-28 15:30:18 +0200

    Potion preview system with true colors

--- src/main/java/net/mcreator/element/types/Potion.java
+++ src/main/java/net/mcreator/element/types/Potion.java
@@ -21,7 +21,9 @@ package net.mcreator.element.types;
 
 import net.mcreator.element.GeneratableElement;
 import net.mcreator.element.parts.EffectEntry;
+import net.mcreator.minecraft.DataListLoader;
 import net.mcreator.minecraft.MinecraftImageGenerator;
+import net.mcreator.workspace.Workspace;
 import net.mcreator.workspace.elements.ModElement;
 
 import java.awt.*;
@@ -46,9 +48,67 @@ public class Potion extends GeneratableElement {
 		public EffectEntry effect;
 		public int duration;
 		public int amplifier;
+
+		public int getAmplifier() {
+			return amplifier;
+		}
+
+		public boolean doesShowParticles() {
+			return true;
+		}
+
+		public int getLiquidColor(Workspace workspace) {
+			if (effect.getUnmappedValue().contains("CUSTOM:")) {
+				ModElement modElement = workspace.getModElementByName(effect.getUnmappedValue().replace("CUSTOM:", ""));
+				if (modElement != null) {
+					GeneratableElement generatableElement = modElement.getGeneratableElement();
+					if (generatableElement instanceof PotionEffect) {
+						return ((PotionEffect) generatableElement).color.getRGB();
+					}
+				}
+			} else {
+				if (DataListLoader.loadDataMap("potions").containsKey(effect.getUnmappedValue())) {
+					return Integer.parseInt(
+							DataListLoader.loadDataMap("potions").get(effect.getUnmappedValue()).getTexture());
+				}
+			}
+			return 0;
+		}
+
 	}
 
 	@Override public BufferedImage generateModElementPicture() {
-		return MinecraftImageGenerator.Preview.generatePotionIcon(Color.RED);
+		return MinecraftImageGenerator.Preview.generatePotionIcon(getPotionColor());
 	}
+
+	private Color getPotionColor() {
+		if (effects.isEmpty()) {
+			return new Color(3694022);
+		}
+		float r = 0;
+		float g = 0;
+		float b = 0;
+		int count = 0;
+
+		for (CustomEffectEntry effectinstance : effects) {
+			if (effectinstance.doesShowParticles()) {
+				int k = effectinstance.getLiquidColor(getModElement().getWorkspace());
+				int l = effectinstance.getAmplifier() + 1;
+				r += (float) (l * (k >> 16 & 255)) / 255.0F;
+				g += (float) (l * (k >> 8 & 255)) / 255.0F;
+				b += (float) (l * (k & 255)) / 255.0F;
+				count += l;
+			}
+		}
+
+		if (count == 0) {
+			return Color.black;
+		} else {
+			r = r / (float) count * 255.0F;
+			g = g / (float) count * 255.0F;
+			b = b / (float) count * 255.0F;
+			return new Color((int) r << 16 | (int) g << 8 | (int) b);
+		}
+	}
+
 }
