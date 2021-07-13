2f3dc3759932c7446fd1c94162d806bc6db925e7
Goldorion <toutankamon@outlook.com>
2021-07-12 23:45:21 -0400

    Add untar method using Apache commons compress
    Need to be tested on a Linux VM

--- src/main/java/net/mcreator/io/zip/ZipIO.java
+++ src/main/java/net/mcreator/io/zip/ZipIO.java
@@ -18,6 +18,11 @@
 
 package net.mcreator.io.zip;
 
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarUtils;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -164,6 +169,25 @@ public class ZipIO {
 				} else {
 					toPath.toFile().getParentFile().mkdirs();
 					Files.copy(zipInputStream, toPath, StandardCopyOption.REPLACE_EXISTING);
+				}
+			}
+		} catch (IOException e) {
+			LOG.error(e.getMessage(), e);
+			LOG.info("Failed to extract zip file:" + e.getMessage());
+		}
+	}
+
+	public static void untar(String strTarFile, String dst) {
+		Path extractFolder = new File(dst).toPath();
+		try (TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(Files.newInputStream(new File(strTarFile).toPath()))) {
+			TarArchiveEntry entry;
+			while ((entry = tarArchiveInputStream.getNextTarEntry()) != null) {
+				Path toPath = extractFolder.resolve(entry.getName());
+				if (entry.isDirectory()) {
+					toPath.toFile().mkdirs();
+				} else {
+					toPath.toFile().getParentFile().mkdirs();
+					Files.copy(tarArchiveInputStream, toPath, StandardCopyOption.REPLACE_EXISTING);
 				}
 			}
 		} catch (IOException e) {
