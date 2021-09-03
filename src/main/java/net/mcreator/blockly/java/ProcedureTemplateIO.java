6699bb15f2292edcb2c7e4761febcf13c30859c8
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2021-09-03 11:46:55 +0200

    Fix #1701

--- src/main/java/net/mcreator/blockly/java/ProcedureTemplateIO.java
+++ src/main/java/net/mcreator/blockly/java/ProcedureTemplateIO.java
@@ -108,23 +108,4 @@ public class ProcedureTemplateIO {
 		return BinaryStringIO.readResourceToString(template);
 	}
 
-	public static Set<VariableElement> tryToExtractVariables(String xml) {
-		Set<VariableElement> retval = new HashSet<>();
-		for (VariableType elementType : VariableTypeLoader.INSTANCE.getAllVariableTypes()) {
-			Matcher m = Pattern.compile("<block type=\"(?:variables_set_" + elementType.getName() + "|variables_get_"
-							+ elementType.getName() + ")\">(?:<mutation.*\"/>)?<field name=\"VAR\">local:(.*?)</field>")
-					.matcher(xml);
-			try {
-				while (m.find()) {
-					VariableElement element = new VariableElement();
-					element.setName(m.group(1));
-					element.setType(elementType);
-					retval.add(element);
-				}
-			} catch (Exception ignored) {
-			}
-		}
-		return retval;
-	}
-
 }
