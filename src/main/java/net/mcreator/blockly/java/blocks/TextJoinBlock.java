e146f4bb7431e5242c687f93fd6b72fb554cf91d
AlsoSomeoneElse <2000someoneelse@gmail.com>
2021-10-02 00:27:40 +0200

    Fixed some issues with join text block and parentheses removal

--- src/main/java/net/mcreator/blockly/java/blocks/TextJoinBlock.java
+++ src/main/java/net/mcreator/blockly/java/blocks/TextJoinBlock.java
@@ -66,10 +66,10 @@ public class TextJoinBlock implements IBlockGenerator {
 			if (sumnum == 1) {
 				if (inputCodes.get(0).matches("\"[^\"]*\"")) { // The only element is a string, we return it as is
 					master.append(inputCodes.get(0));
-					return;
 				} else {
-					master.append("(\"\" + ");
+					master.append("(\"\" + " + inputCodes.get(0) + ")");
 				}
+				return;
 			} else {
 				master.append("(");
 			}
