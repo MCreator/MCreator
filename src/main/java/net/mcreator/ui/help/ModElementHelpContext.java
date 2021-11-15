a2d49f567f901ad86ac9f710f5aa252ca57d66b4
Defeatomizer <drag7398@gmail.com>
2021-11-15 20:39:53 +0300

    Fixed french tip (hopefully) and header comment

--- src/main/java/net/mcreator/ui/help/ModElementHelpContext.java
+++ src/main/java/net/mcreator/ui/help/ModElementHelpContext.java
@@ -1,6 +1,7 @@
 /*
  * MCreator (https://mcreator.net/)
- * Copyright (C) 2020 Pylo and contributors
+ * Copyright (C) 2012-2020, Pylo
+ * Copyright (C) 2020-2021, Pylo, opensource contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
