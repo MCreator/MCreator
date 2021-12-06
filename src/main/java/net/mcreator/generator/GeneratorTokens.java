f4cd8de787b2bcee8850760c574e0a07162e6005
Defeatomizer <drag7398@gmail.com>
2021-12-06 11:20:35 +0300

    Pardon my dumbness

--- src/main/java/net/mcreator/generator/GeneratorTokens.java
+++ src/main/java/net/mcreator/generator/GeneratorTokens.java
@@ -58,20 +58,19 @@ public class GeneratorTokens {
 
 	private static final Pattern brackets = Pattern.compile("@\\[(.*?)]");
 
-	static String replaceVariableTokens(GeneratableElement element, String rawname, boolean ignoreErrors) {
+	static String replaceVariableTokens(GeneratableElement element, String rawname, boolean printErrors) {
 		if (containsVariableTokens(rawname)) {
 			Matcher m = brackets.matcher(rawname);
 			while (m.find()) {
 				String match = m.group(1);
 				Object value = null;
 				try {
-					if (match.contains("()")) {
+					if (match.contains("()"))
 						value = element.getClass().getMethod(match.replace("()", "").trim()).invoke(element);
-					} else {
+					else
 						value = element.getClass().getField(match.replace("()", "").trim()).get(element);
-					}
 				} catch (Exception e) {
-					if (!ignoreErrors)
+					if (printErrors)
 						LOG.warn("Failed to load token value " + match, e);
 				}
 				rawname = rawname.replace("@[" + match + "]", value != null ? value.toString() : "null");
