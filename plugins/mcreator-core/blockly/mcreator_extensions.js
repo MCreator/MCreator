9353b6cef9b1a8bb361d9565160e38c2f8c2cabe
AlsoSomeoneElse <2000someoneelse@gmail.com>
2021-12-30 13:15:11 +0100

    Replaced the old selectors with the new ones

--- plugins/mcreator-core/blockly/mcreator_extensions.js
+++ plugins/mcreator-core/blockly/mcreator_extensions.js
@@ -101,16 +101,12 @@ Blockly.Extensions.registerMutator('variable_entity_input',
 
 Blockly.Extensions.register('biome_list_provider',
     function () {
-        this.appendDummyInput().appendField(new Blockly.FieldDropdown(
-            arrayToBlocklyDropDownArrayWithReadableNames(javabridge.getListOf("biome"),
-                javabridge.getReadableListOf("biome"))), 'biome');
+        this.appendDummyInput().appendField(new FieldDataListSelector('biome'), 'biome');
     });
 
 Blockly.Extensions.register('entity_list_provider',
     function () {
-        this.appendDummyInput().appendField(new Blockly.FieldDropdown(
-            arrayToBlocklyDropDownArrayWithReadableNames(javabridge.getListOf("entity"),
-                javabridge.getReadableListOf("entity"))), 'entity');
+        this.appendDummyInput().appendField(new FieldDataListSelector('entity'), 'entity');
     });
 
 Blockly.Extensions.register('gui_list_provider',
@@ -170,8 +166,7 @@ Blockly.Extensions.register('enhancement_list_provider',
 Blockly.Extensions.register('sound_list_provider',
     function () {
         this.appendDummyInput().appendField(javabridge.t("blockly.extension.sound_list"))
-            .appendField(new Blockly.FieldDropdown(
-                arrayToBlocklyDropDownArray(javabridge.getListOf("sound"))), 'sound');
+            .appendField(new FieldDataListSelector('sound'), 'sound');
     });
 
 Blockly.Extensions.register('particle_list_provider',
@@ -202,24 +197,6 @@ Blockly.Extensions.register('dimension_custom_list_provider',
     function () {
         this.appendDummyInput().appendField(new Blockly.FieldDropdown(
             arrayToBlocklyDropDownArray(javabridge.getListOf("dimension_custom"))), 'dimension');
-    });
-
-Blockly.Extensions.register('searchable_entity_provider',
-    function () {
-        this.appendDummyInput()
-            .appendField(new FieldDataListSelector('entity'), 'entity');
-    });
-
-Blockly.Extensions.register('searchable_biome_provider',
-    function () {
-        this.appendDummyInput()
-            .appendField(new FieldDataListSelector('biome'), 'biome');
-    });
-
-Blockly.Extensions.register('searchable_sound_provider',
-    function () {
-        this.appendDummyInput().appendField(javabridge.t("blockly.extension.sound_list"))
-            .appendField(new FieldDataListSelector('sound'), 'sound');
     });
 
 /**
