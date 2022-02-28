MCreator/MCreator

commit 4fba106476a253e213d7e7ab80825f79149bc341

[1.18] Ported fluid mod element (#2371)
* Initial, lakes don't generate

* Fixed lakes generation

* Proper fluid block models

* I'll just believe it works

* Underground lakes fix

Co-authored-by: AlsoSomeoneElse <64546452+AlsoSomeoneElse@users.noreply.github.com>

plugins/generator-1.18.1/forge-1.18.1/templates/json/fluidtag.json.ftl
@@ -1,0 +1,11 @@
+{
+    "replace": false,
+    "values": [
+        <#list w.getElementsOfType("fluid") as mod>
+            <#if mod.getGeneratableElement().type.toString()?lower_case == var_type>
+                "${modid}:${mod.getRegistryName()?lower_case}",
+                "${modid}:flowing_${mod.getRegistryName()?lower_case}"<#sep>,
+            </#if>
+        </#list>
+    ]
+}

plugins/generator-1.18.1/forge-1.18.1/fluid.definition.yaml
@@ -1,0 +1,47 @@
+templates:
+  - template: fluid/fluid.java.ftl
+    name: "@SRCROOT/@BASEPACKAGEPATH/fluid/@NAMEFluid.java"
+  - template: fluid/fluidblock.java.ftl
+    name: "@SRCROOT/@BASEPACKAGEPATH/block/@NAMEBlock.java"
+  - template: fluid/fluidattributes.java.ftl
+    deleteWhenConditionFalse: true
+    condition: extendsFluidAttributes()
+    name: "@SRCROOT/@BASEPACKAGEPATH/fluid/attributes/@NAMEFluidAttributes.java"
+  - template: fluid/fluidbucket.java.ftl
+    deleteWhenConditionFalse: true
+    condition: generateBucket
+    name: "@SRCROOT/@BASEPACKAGEPATH/item/@NAMEItem.java"
+  - template: fluid/fluidgen.java.ftl
+    deleteWhenConditionFalse: true
+    condition: doesGenerateInWorld()
+    name: "@SRCROOT/@BASEPACKAGEPATH/world/features/lakes/@NAMEFeature.java"
+
+  - template: json/bucket.json.ftl
+    writer: json
+    name: "@MODASSETSROOT/models/item/@registryname_bucket.json"
+  - template: json/fluid_states.json.ftl
+    writer: json
+    name: "@MODASSETSROOT/blockstates/@registryname.json"
+  - template: json/fluid.json.ftl
+    writer: json
+    name: "@MODASSETSROOT/models/block/@registryname.json"
+
+global_templates:
+  - template: elementinits/fluids.java.ftl
+    name: "@SRCROOT/@BASEPACKAGEPATH/init/@JavaModNameFluids.java"
+  - template: json/fluidtag.json.ftl
+    writer: json
+    variables: "type=water"
+    name: "@RESROOT/data/minecraft/tags/fluids/water.json"
+  - template: json/fluidtag.json.ftl
+    writer: json
+    variables: "type=lava"
+    name: "@RESROOT/data/minecraft/tags/fluids/lava.json"
+
+localizationkeys:
+  - key: fluid.@modid.@registryname
+    mapto: name
+  - key: block.@modid.@registryname
+    mapto: name
+  - key: item.@modid.@registryname_bucket
+    mapto: bucketName

plugins/generator-1.18.1/forge-1.18.1/templates/elementinits/features.java.ftl
@@ -41,9 +41,13 @@ package ${package}.init;
 	private static final Map<Feature<?>, FeatureRegistration> REGISTRY = new HashMap<>();
 
 	static {
-    <#list features as feature>
+	<#list features as feature>
 		<#if feature.getModElement().getTypeString() == "block">
 			REGISTRY.put(${feature.getModElement().getName()}Feature.FEATURE, new FeatureRegistration(GenerationStep.Decoration.UNDERGROUND_ORES,
+						${feature.getModElement().getName()}Feature.GENERATE_BIOMES, ${feature.getModElement().getName()}Feature.CONFIGURED_FEATURE,
+						${feature.getModElement().getName()}Feature.PLACED_FEATURE));
+		<#elseif feature.getModElement().getTypeString() == "fluid">
+			REGISTRY.put(${feature.getModElement().getName()}Feature.FEATURE, new FeatureRegistration(GenerationStep.Decoration.LAKES,
 						${feature.getModElement().getName()}Feature.GENERATE_BIOMES, ${feature.getModElement().getName()}Feature.CONFIGURED_FEATURE,
 						${feature.getModElement().getName()}Feature.PLACED_FEATURE));
 		<#elseif feature.getModElement().getTypeString() == "plant">
@@ -56,10 +60,10 @@ package ${package}.init;
 						${feature.getModElement().getName()}Feature.GENERATE_BIOMES, ${feature.getModElement().getName()}Feature.CONFIGURED_FEATURE,
 						${feature.getModElement().getName()}Feature.PLACED_FEATURE));
 		</#if>
-    </#list>
+	</#list>
 	}
 
-	@SubscribeEvent public static void registerFeature(RegistryEvent.Register<Feature<?>> event) {
+	@SubscribeEvent public static void registerFeatures(RegistryEvent.Register<Feature<?>> event) {
 		event.getRegistry().registerAll(REGISTRY.keySet().toArray(new Feature[0]));
 
 		REGISTRY.forEach((feature, registration) -> {
@@ -70,7 +74,7 @@ package ${package}.init;
 
 	@Mod.EventBusSubscriber private static class BiomeFeatureLoader {
 
-		@SubscribeEvent public static void addFeatureToBiomes(BiomeLoadingEvent event) {
+		@SubscribeEvent public static void addFeaturesToBiomes(BiomeLoadingEvent event) {
 			for (FeatureRegistration registration : REGISTRY.values()) {
 				if (registration.biomes() == null || registration.biomes().contains(event.getName()))
 					event.getGeneration().getFeatures(registration.stage()).add(() -> registration.placedFeature());

plugins/generator-1.18.1/forge-1.18.1/templates/elementinits/fluids.java.ftl
@@ -1,0 +1,67 @@
+<#--
+ # MCreator (https://mcreator.net/)
+ # Copyright (C) 2012-2020, Pylo
+ # Copyright (C) 2020-2021, Pylo, opensource contributors
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ #
+ # Additional permission for code generator templates (*.ftl files)
+ #
+ # As a special exception, you may create a larger work that contains part or
+ # all of the MCreator code generator templates (*.ftl files) and distribute
+ # that work under terms of your choice, so long as that work isn't itself a
+ # template for code generation. Alternatively, if you modify or redistribute
+ # the template itself, you may (at your option) remove this special exception,
+ # which will cause the template and the resulting code generator output files
+ # to be licensed under the GNU General Public License without this special
+ # exception.
+-->
+
+<#-- @formatter:off -->
+
+/*
+ * MCreator note: This file will be REGENERATED on each build.
+ */
+
+package ${package}.init;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD) public class ${JavaModName}Fluids {
+
+	private static final List<Fluid> REGISTRY = new ArrayList<>();
+
+	<#list fluids as fluid>
+	public static final FlowingFluid ${fluid.getModElement().getRegistryNameUpper()} = register(new ${fluid.getModElement().getName()}Fluid.Source());
+	public static final FlowingFluid FLOWING_${fluid.getModElement().getRegistryNameUpper()} = register(new ${fluid.getModElement().getName()}Fluid.Flowing());
+	</#list>
+
+	private static FlowingFluid register(FlowingFluid fluid) {
+		REGISTRY.add(fluid);
+		return fluid;
+	}
+
+	@SubscribeEvent public static void registerFluids(RegistryEvent.Register<Fluid> event) {
+		event.getRegistry().registerAll(REGISTRY.toArray(new Fluid[0]));
+	}
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT) public static class ClientSideHandler {
+		@SubscribeEvent public static void clientSetup(FMLClientSetupEvent event) {
+			<#list fluids as fluid>
+			ItemBlockRenderTypes.setRenderLayer(${fluid.getModElement().getRegistryNameUpper()}, renderType -> renderType == RenderType.translucent());
+			ItemBlockRenderTypes.setRenderLayer(FLOWING_${fluid.getModElement().getRegistryNameUpper()}, renderType -> renderType == RenderType.translucent());
+			</#list>
+		}
+	}
+}
+
+<#-- @formatter:on -->

plugins/generator-1.18.1/forge-1.18.1/templates/fluid/fluid.java.ftl
@@ -1,0 +1,152 @@
+<#--
+ # MCreator (https://mcreator.net/)
+ # Copyright (C) 2020 Pylo and contributors
+ # 
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ # 
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ # 
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ # 
+ # Additional permission for code generator templates (*.ftl files)
+ # 
+ # As a special exception, you may create a larger work that contains part or 
+ # all of the MCreator code generator templates (*.ftl files) and distribute 
+ # that work under terms of your choice, so long as that work isn't itself a 
+ # template for code generation. Alternatively, if you modify or redistribute 
+ # the template itself, you may (at your option) remove this special exception, 
+ # which will cause the template and the resulting code generator output files 
+ # to be licensed under the GNU General Public License without this special 
+ # exception.
+-->
+
+<#-- @formatter:off -->
+<#include "../procedures.java.ftl">
+
+package ${package}.fluid;
+
+public abstract class ${name}Fluid extends ForgeFlowingFluid {
+
+	public static final ForgeFlowingFluid.Properties PROPERTIES = new ForgeFlowingFluid.Properties(
+			() -> ${JavaModName}Fluids.${data.getModElement().getRegistryNameUpper()},
+			() -> ${JavaModName}Fluids.FLOWING_${data.getModElement().getRegistryNameUpper()},
+			<#if data.extendsFluidAttributes()>${name}</#if>FluidAttributes
+			.builder(new ResourceLocation("${modid}:blocks/${data.textureStill}"), new ResourceLocation("${modid}:blocks/${data.textureFlowing}"))
+				<#if data.luminosity != 0>.luminosity(${data.luminosity})</#if>
+				<#if data.density != 1000>.density(${data.density})</#if>
+				<#if data.viscosity != 1000>.viscosity(${data.viscosity})</#if>
+				<#if data.temperature != 300>.temperature(${data.temperature})</#if>
+				<#if data.isGas>.gaseous()</#if>
+				<#if data.rarity != "COMMON">.rarity(Rarity.${data.rarity})</#if>
+				<#if data.emptySound?has_content && data.emptySound.getMappedValue()?has_content>
+				.sound(ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("${data.emptySound}")))
+				</#if>
+				<#if data.isFluidTinted()>
+				.color(<#if data.tintType == "Grass">
+					-6506636
+					<#elseif data.tintType == "Foliage">
+					-12012264
+					<#elseif data.tintType == "Water">
+					-13083194
+					<#elseif data.tintType == "Sky">
+					-8214273
+					<#elseif data.tintType == "Fog">
+					-4138753
+					<#else>
+					-16448205
+					</#if>)
+				</#if>)
+				.explosionResistance(${data.resistance}f)
+				<#if data.canMultiply>.canMultiply()</#if>
+				<#if data.flowRate != 5>.tickRate(${data.flowRate})</#if>
+				<#if data.levelDecrease != 1>.levelDecreasePerBlock(${data.levelDecrease})</#if>
+				<#if data.slopeFindDistance != 4>.slopeFindDistance(${data.slopeFindDistance})</#if>
+				<#if data.generateBucket>.bucket(() -> ${JavaModName}Items.${data.getModElement().getRegistryNameUpper()}_BUCKET)</#if>
+				.block(() -> (LiquidBlock) ${JavaModName}Blocks.${data.getModElement().getRegistryNameUpper()});
+
+	private ${name}Fluid() {
+		super(PROPERTIES);
+	}
+
+	<#if data.spawnParticles>
+	@Override public ParticleOptions getDripParticle() {
+		return ${data.dripParticle};
+	}
+	</#if>
+
+	<#if data.flowStrength != 1>
+	@Override public Vec3 getFlow(BlockGetter world, BlockPos pos, FluidState fluidstate) {
+		return super.getFlow(world, pos, fluidstate).scale(${data.flowStrength});
+	}
+	</#if>
+
+	<#if hasProcedure(data.flowCondition)>
+	@Override protected boolean canSpreadTo(BlockGetter worldIn, BlockPos fromPos, BlockState blockstate,
+			Direction direction, BlockPos toPos, BlockState intostate, FluidState toFluidState, Fluid fluidIn) {
+		boolean condition = true;
+		if (worldIn instanceof LevelAccessor world) {
+			int x = fromPos.getX();
+			int y = fromPos.getY();
+			int z = fromPos.getZ();
+			condition = <@procedureOBJToConditionCode data.flowCondition/>;
+		}
+		return super.canSpreadTo(worldIn, fromPos, blockstate, direction, toPos, intostate, toFluidState, fluidIn) && condition;
+	}
+	</#if>
+
+	<#if hasProcedure(data.beforeReplacingBlock)>
+	@Override protected void beforeDestroyingBlock(LevelAccessor world, BlockPos pos, BlockState blockstate) {
+		<@procedureCode data.beforeReplacingBlock, {
+			"x": "pos.getX()",
+			"y": "pos.getY()",
+			"z": "pos.getZ()",
+			"world": "world",
+			"blockstate": "blockstate"
+		}/>
+	}
+	</#if>
+
+	public static class Source extends ${name}Fluid {
+		public Source() {
+			super();
+			setRegistryName("${registryname}");
+		}
+
+		public int getAmount(FluidState state) {
+			return 8;
+		}
+
+		public boolean isSource(FluidState state) {
+			return true;
+		}
+	}
+
+	public static class Flowing extends ${name}Fluid {
+		public Flowing() {
+			super();
+			setRegistryName("flowing_${registryname}");
+		}
+
+		protected void createFluidStateDefinition(StateDefinition.Builder<Fluid, FluidState> builder) {
+			super.createFluidStateDefinition(builder);
+			builder.add(LEVEL);
+		}
+
+		public int getAmount(FluidState state) {
+			return state.getValue(LEVEL);
+		}
+
+		public boolean isSource(FluidState state) {
+			return false;
+		}
+	}
+
+}
+<#-- @formatter:on -->

plugins/generator-1.18.1/forge-1.18.1/templates/fluid/fluidattributes.java.ftl
@@ -1,0 +1,71 @@
+<#--
+ # MCreator (https://mcreator.net/)
+ # Copyright (C) 2020 Pylo and contributors
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ #
+ # Additional permission for code generator templates (*.ftl files)
+ #
+ # As a special exception, you may create a larger work that contains part or
+ # all of the MCreator code generator templates (*.ftl files) and distribute
+ # that work under terms of your choice, so long as that work isn't itself a
+ # template for code generation. Alternatively, if you modify or redistribute
+ # the template itself, you may (at your option) remove this special exception,
+ # which will cause the template and the resulting code generator output files
+ # to be licensed under the GNU General Public License without this special
+ # exception.
+-->
+
+<#-- @formatter:off -->
+
+package ${package}.fluid.attributes;
+
+public class ${name}FluidAttributes extends FluidAttributes {
+
+	public static CustomBuilder builder(ResourceLocation stillTexture, ResourceLocation flowingTexture) {
+		return new CustomBuilder(stillTexture, flowingTexture, ${name}FluidAttributes::new);
+	}
+
+	public static class CustomBuilder extends FluidAttributes.Builder {
+		protected CustomBuilder(ResourceLocation stillTexture, ResourceLocation flowingTexture,
+				BiFunction<FluidAttributes.Builder, Fluid, FluidAttributes> factory) {
+			super(stillTexture, flowingTexture, factory);
+		}
+	}
+
+	protected ${name}FluidAttributes(Builder builder, Fluid fluid) {
+		super(builder, fluid);
+	}
+
+	<#if data.isFluidTinted()>
+	@Override
+	public int getColor(BlockAndTintGetter world, BlockPos pos) {
+		return
+		<#if data.tintType == "Grass">
+			BiomeColors.getAverageGrassColor(world, pos)
+		<#elseif data.tintType == "Foliage">
+			BiomeColors.getAverageFoliageColor(world, pos)
+		<#elseif data.tintType == "Water">
+			BiomeColors.getAverageWaterColor(world, pos)
+		<#elseif data.tintType == "Sky">
+			Minecraft.getInstance().level.getBiome(pos).getSkyColor()
+		<#elseif data.tintType == "Fog">
+			Minecraft.getInstance().level.getBiome(pos).getFogColor()
+		<#else>
+			Minecraft.getInstance().level.getBiome(pos).getWaterFogColor()
+		</#if> | 0xFF000000;
+	}
+	</#if>
+
+}

plugins/generator-1.18.1/forge-1.18.1/templates/fluid/fluidblock.java.ftl
@@ -1,0 +1,87 @@
+<#--
+ # MCreator (https://mcreator.net/)
+ # Copyright (C) 2020 Pylo and contributors
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ #
+ # Additional permission for code generator templates (*.ftl files)
+ #
+ # As a special exception, you may create a larger work that contains part or
+ # all of the MCreator code generator templates (*.ftl files) and distribute
+ # that work under terms of your choice, so long as that work isn't itself a
+ # template for code generation. Alternatively, if you modify or redistribute
+ # the template itself, you may (at your option) remove this special exception,
+ # which will cause the template and the resulting code generator output files
+ # to be licensed under the GNU General Public License without this special
+ # exception.
+-->
+
+<#-- @formatter:off -->
+<#include "../procedures.java.ftl">
+<#include "../triggers.java.ftl">
+
+package ${package}.block;
+
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+
+public class ${name}Block extends LiquidBlock {
+	public ${name}Block() {
+		super(${JavaModName}Fluids.${data.getModElement().getRegistryNameUpper()},
+			<#if generator.map(data.colorOnMap, "mapcolors") != "DEFAULT">
+			BlockBehaviour.Properties.of(Material.${data.type}, MaterialColor.${generator.map(data.colorOnMap, "mapcolors")})
+			<#else>
+			BlockBehaviour.Properties.of(Material.${data.type})
+			</#if>
+			.strength(${data.resistance}f)
+			<#if data.emissiveRendering>.hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true)</#if>
+			<#if data.luminance != 0>.lightLevel(s -> ${data.luminance})</#if>
+		);
+		setRegistryName("${registryname}");
+	}
+
+	<#if data.flammability != 0>
+	@Override public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return ${data.flammability};
+	}
+	</#if>
+
+	<#if data.fireSpreadSpeed != 0>
+	@Override public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+		return ${data.fireSpreadSpeed};
+	}
+	</#if>
+
+	<#if data.lightOpacity == 0>
+	@Override public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+		return true;
+	}
+	<#elseif data.lightOpacity != 1>
+	@Override public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return ${data.lightOpacity};
+	}
+	</#if>
+
+	<@onBlockAdded data.onBlockAdded, hasProcedure(data.onTickUpdate) && data.tickRate gt 0, data.tickRate/>
+
+	<@onRedstoneOrNeighborChanged "", "", data.onNeighbourChanges/>
+
+	<@onBlockTick data.onTickUpdate, data.tickRate gt 0, data.tickRate/>
+
+	<@onEntityCollides data.onEntityCollides/>
+
+	<@onAnimateTick data.onRandomUpdateEvent/>
+
+	<@onDestroyedByExplosion data.onDestroyedByExplosion/>
+}

plugins/generator-1.18.1/forge-1.18.1/templates/fluid/fluidbucket.java.ftl
@@ -1,0 +1,55 @@
+<#--
+ # MCreator (https://mcreator.net/)
+ # Copyright (C) 2012-2020, Pylo
+ # Copyright (C) 2020-2021, Pylo, opensource contributors
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ #
+ # Additional permission for code generator templates (*.ftl files)
+ #
+ # As a special exception, you may create a larger work that contains part or
+ # all of the MCreator code generator templates (*.ftl files) and distribute
+ # that work under terms of your choice, so long as that work isn't itself a
+ # template for code generation. Alternatively, if you modify or redistribute
+ # the template itself, you may (at your option) remove this special exception,
+ # which will cause the template and the resulting code generator output files
+ # to be licensed under the GNU General Public License without this special
+ # exception.
+-->
+
+<#-- @formatter:off -->
+
+package ${package}.item;
+
+import net.minecraft.network.chat.Component;
+
+public class ${name}Item extends BucketItem {
+
+	public ${name}Item() {
+		super(() -> ${JavaModName}Fluids.${data.getModElement().getRegistryNameUpper()},
+			new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1).rarity(Rarity.${data.rarity})
+			<#if data.creativeTab?has_content>.tab(${data.creativeTab})<#else>.tab(CreativeModeTab.TAB_MISC)</#if>);
+		setRegistryName("${registryname}_bucket");
+	}
+
+	<#if data.specialInfo?has_content>
+	@Override public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		<#list data.specialInfo as entry>
+		list.add(new TextComponent("${JavaConventions.escapeStringForJava(entry)}"));
+		</#list>
+	}
+	</#if>
+}
+<#-- @formatter:on -->

commit 10bcbcff72037bf16395d0dd674e0a2de5390d82

Fix bug introduced by #2371

plugins/generator-1.18.1/forge-1.18.1/templates/json/fluidtag.json.ftl
@@ -1,11 +1,16 @@
 {
     "replace": false,
     "values": [
+        <#assign elements = []>
         <#list w.getElementsOfType("fluid") as mod>
             <#if mod.getGeneratableElement().type.toString()?lower_case == var_type>
-                "${modid}:${mod.getRegistryName()?lower_case}",
-                "${modid}:flowing_${mod.getRegistryName()?lower_case}"<#sep>,
+                <#assign elements += ["${modid}:${mod.getRegistryName()?lower_case}"]>
+                <#assign elements += ["${modid}:flowing_${mod.getRegistryName()?lower_case}"]>
             </#if>
+        </#list>
+
+        <#list elements as e>
+            "${e}"<#if e?has_next>,</#if>
         </#list>
     ]
 }