08004ab040f18f0201e5c53129087c33138c6179
AlsoSomeoneElse <2000someoneelse@gmail.com>
2022-03-24 17:48:35 +0100

    Removed unused import

--- src/main/java/net/mcreator/blockly/feature/BlocklyToFeature.java
+++ src/main/java/net/mcreator/blockly/feature/BlocklyToFeature.java
@@ -24,7 +24,6 @@ import net.mcreator.blockly.IBlockGenerator;
 import net.mcreator.blockly.java.BlocklyToJava;
 import net.mcreator.generator.template.TemplateGenerator;
 import net.mcreator.generator.template.TemplateGeneratorException;
-import net.mcreator.ui.blockly.BlocklyEditorType;
 import net.mcreator.ui.init.L10N;
 import net.mcreator.util.XMLUtil;
 import net.mcreator.workspace.Workspace;
