bc4abc29657dfb737608ad9d96c5ab1d7c751460
Max094Reikeb <maxencebekier@gmail.com>
2022-04-16 17:36:51 +0200

    Modified border thickness

--- src/main/java/net/mcreator/ui/minecraft/villagers/JVillagerTradeProfession.java
+++ src/main/java/net/mcreator/ui/minecraft/villagers/JVillagerTradeProfession.java
@@ -97,7 +97,7 @@ public class JVillagerTradeProfession extends JEntriesList {
 		add("Center", entries);
 
 		setBorder(BorderFactory.createTitledBorder(
-				BorderFactory.createLineBorder((Color) UIManager.get("MCreatorLAF.BRIGHT_COLOR"), 2),
+				BorderFactory.createLineBorder((Color) UIManager.get("MCreatorLAF.BRIGHT_COLOR"), 1),
 				L10N.t("elementgui.villager_trade.profession_trades"), 0, 0, getFont().deriveFont(12.0f),
 				(Color) UIManager.get("MCreatorLAF.BRIGHT_COLOR")));
 
