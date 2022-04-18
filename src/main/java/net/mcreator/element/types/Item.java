MCreator/MCreator

commit eb7812ae18acfdae85bced7ad2223d2028d0c46d

Move the Food ME to the Item ME (#2002)
* Add food parameters to items (no generated code yet)

* Remove food ME (the MET is kept for to convert old workspaces)

* Add/remove code templates + Remove eating speed (same param as use duration)

* Update tests

* Add a converter

* Remove the deprecated annotation

* Remove food mod type icons

* RU translation + right def file removal

* Fix indentations

* Fix an indentation

Co-authored-by: Defeatomizer <drag7398@gmail.com>

* Update fv

* Some code improvements + 1.18.1 support

* Fix wrong fv version inside the converter class

* Some changes

* Some text fixes

* Update texts.properties

* Re-add a needed key

* Fix opening old workpsaces

Same fix as https://github.com/MCreator/MCreator/pull/2479/commits/1299694b91f2155d9e11fef4521c4ea01ff89460

* Unregister food

* Change the converter

* Converter system from #2497

THIS IS ONLY FOR TESTING

* Revert "Converter system from #2497"

This reverts commit 9a0c7f9974610e37ef19d873976ff6bb3ce6c7f3.

* fv 27

* Fix a problematic problem

* Manual merge

* Minor things

* Move food templates to items in addons

Not tested as I don't have Bedrock

* Fix tests

* forDogs -> isMeat

* Final fix?

* 28

* Some fixes (seems add-on UI was not tested?)

* Minor UI changes

* Better converter

* Missing removed icons..

* Some fixes

* Better condition

* Better method name

* Formatting

* Refactor

* Update item.java.ftl

* Fix eatResultItem

* hasEatResultItem() everywhere

Co-authored-by: Defeatomizer <drag7398@gmail.com>
Co-authored-by: KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>

src/main/java/net/mcreator/element/types/Item.java
@@ -78,10 +78,20 @@ import java.util.Map;
 	public Procedure onStoppedUsing;
 	public Procedure onEntitySwing;
 	public Procedure onDroppedByPlayer;
+	public Procedure onFinishUsingItem;
 
 	public boolean hasDispenseBehavior;
 	public Procedure dispenseSuccessCondition;
 	public Procedure dispenseResultItemstack;
+
+	// Food
+	public boolean isFood;
+	public int nutritionalValue;
+	public double saturation;
+	public MItemBlock eatResultItem;
+	public boolean isMeat;
+	public boolean isAlwaysEdible;
+	public String animation;
 
 	private Item() {
 		this(null);
@@ -93,6 +103,8 @@ import java.util.Map;
 		this.rarity = "COMMON";
 		this.inventorySize = 9;
 		this.inventoryStackSize = 64;
+		this.saturation = 0.3f;
+		this.animation = "eat";
 	}
 
 	@Override public BufferedImage generateModElementPicture() {
@@ -136,4 +148,11 @@ import java.util.Map;
 		return guiBoundTo != null && !guiBoundTo.isEmpty() && !guiBoundTo.equals("<NONE>");
 	}
 
+	public boolean hasNonDefaultAnimation() {
+		return isFood ? !animation.equals("eat") : !animation.equals("none");
+	}
+
+	public boolean hasEatResultItem() {
+		return isFood && !eatResultItem.isEmpty();
+	}
 }

plugins/generator-1.16.5/forge-1.16.5/food.definition.yaml
@@ -1,20 +1,0 @@
-templates:
-  - template: food.java.ftl
-    name: "@SRCROOT/@BASEPACKAGEPATH/item/@NAMEItem.java"
-  - template: json/item.json.ftl
-    writer: json
-    condition: "renderType #= 0"
-    name: "@MODASSETSROOT/models/item/@registryname.json"
-  - template: json/item_cmodel.json.ftl
-    writer: json
-    condition: "renderType #= 1"
-    name: "@MODASSETSROOT/models/item/@registryname.json"
-  - template: json/item_cmodel_obj.json.ftl
-    writer: json
-    condition: "renderType #= 2"
-    variables: "type=item"
-    name: "@MODASSETSROOT/models/item/@registryname.json"
-
-localizationkeys:
-  - key: item.@modid.@registryname
-    mapto: name

plugins/generator-1.16.5/forge-1.16.5/templates/food.java.ftl
@@ -1,229 +1,0 @@
-<#--
- # MCreator (https://mcreator.net/)
- # Copyright (C) 2020 Pylo and contributors
- # 
- # This program is free software: you can redistribute it and/or modify
- # it under the terms of the GNU General Public License as published by
- # the Free Software Foundation, either version 3 of the License, or
- # (at your option) any later version.
- # 
- # This program is distributed in the hope that it will be useful,
- # but WITHOUT ANY WARRANTY; without even the implied warranty of
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- # GNU General Public License for more details.
- # 
- # You should have received a copy of the GNU General Public License
- # along with this program.  If not, see <https://www.gnu.org/licenses/>.
- # 
- # Additional permission for code generator templates (*.ftl files)
- # 
- # As a special exception, you may create a larger work that contains part or 
- # all of the MCreator code generator templates (*.ftl files) and distribute 
- # that work under terms of your choice, so long as that work isn't itself a 
- # template for code generation. Alternatively, if you modify or redistribute 
- # the template itself, you may (at your option) remove this special exception, 
- # which will cause the template and the resulting code generator output files 
- # to be licensed under the GNU General Public License without this special 
- # exception.
--->
-
-<#-- @formatter:off -->
-<#include "procedures.java.ftl">
-<#include "mcitems.ftl">
-
-package ${package}.item;
-
-@${JavaModName}Elements.ModElement.Tag
-public class ${name}Item extends ${JavaModName}Elements.ModElement{
-
-	@ObjectHolder("${modid}:${registryname}")
-	public static final Item block = null;
-
-	public ${name}Item (${JavaModName}Elements instance) {
-		super(instance, ${data.getModElement().getSortID()});
-	}
-
-	@Override public void initElements() {
-		elements.items.add(() -> new FoodItemCustom());
-	}
-
-	public static class FoodItemCustom extends Item {
-
-		public FoodItemCustom() {
-			super(new Item.Properties().group(${data.creativeTab}).maxStackSize(${data.stackSize}).rarity(Rarity.${data.rarity})
-				.food((new Food.Builder()).hunger(${data.nutritionalValue}).saturation(${data.saturation}f)
-				<#if data.isAlwaysEdible>.setAlwaysEdible()</#if>
-				<#if data.forDogs>.meat()</#if>
-				.build()
-			));
-			setRegistryName("${registryname}");
-		}
-
-		<#if data.eatingSpeed != 32>
-		@Override public int getUseDuration(ItemStack stack) {
-			return ${data.eatingSpeed};
-		}
-        </#if>
-
-		<#if data.hasGlow>
-		@Override @OnlyIn(Dist.CLIENT) public boolean hasEffect(ItemStack itemstack) {
-		    <#if hasProcedure(data.glowCondition)>
-			PlayerEntity entity = Minecraft.getInstance().player;
-			World world = entity.world;
-			double x = entity.getPosX();
-			double y = entity.getPosY();
-			double z = entity.getPosZ();
-        	if (!(<@procedureOBJToConditionCode data.glowCondition/>)) {
-        	    return false;
-        	}
-        	</#if>
-			return true;
-		}
-        </#if>
-
-		@Override public UseAction getUseAction(ItemStack itemstack) {
-			return UseAction.${data.animation?upper_case};
-		}
-
-		<#if data.animation == "drink">
-		@Override public net.minecraft.util.SoundEvent getEatSound() {
-			return net.minecraft.util.SoundEvents.ENTITY_GENERIC_DRINK;
-		}
-		</#if>
-
-		<#if data.specialInfo?has_content>
-		@Override public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
-			super.addInformation(itemstack, world, list, flag);
-			<#list data.specialInfo as entry>
-			list.add(new StringTextComponent("${JavaConventions.escapeStringForJava(entry)}"));
-			</#list>
-		}
-		</#if>
-
-		<#if hasProcedure(data.onRightClicked)>
-		@Override public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand) {
-			ActionResult<ItemStack> ar = super.onItemRightClick(world, entity, hand);
-			ItemStack itemstack = ar.getResult();
-			double x = entity.getPosX();
-			double y = entity.getPosY();
-			double z = entity.getPosZ();
-			<@procedureOBJToCode data.onRightClicked/>
-			return ar;
-		}
-        </#if>
-
-		<#if hasProcedure(data.onRightClickedOnBlock)>
-		@Override public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
-			ActionResultType retval = super.onItemUseFirst(stack, context);
-			World world = context.getWorld();
-      		BlockPos pos = context.getPos();
-      		PlayerEntity entity = context.getPlayer();
-      		Direction direction = context.getFace();
-      		BlockState blockstate = world.getBlockState(pos);
-			int x = pos.getX();
-			int y = pos.getY();
-			int z = pos.getZ();
-			ItemStack itemstack = context.getItem();
-			<#if hasReturnValueOf(data.onRightClickedOnBlock, "actionresulttype")>
-			return <@procedureOBJToActionResultTypeCode data.onRightClickedOnBlock/>;
-			<#else>
-			<@procedureOBJToCode data.onRightClickedOnBlock/>
-			return retval;
-			</#if>
-		}
-        </#if>
-
-		<#if hasProcedure(data.onEaten) || (data.resultItem?? && !data.resultItem.isEmpty())>
-		@Override public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
-			ItemStack retval =
-				<#if data.resultItem?? && !data.resultItem.isEmpty()>
-					${mappedMCItemToItemStackCode(data.resultItem, 1)};
-				</#if>
-			super.onItemUseFinish(itemstack, world, entity);
-
-			<#if hasProcedure(data.onEaten)>
-				double x = entity.getPosX();
-				double y = entity.getPosY();
-				double z = entity.getPosZ();
-				<@procedureOBJToCode data.onEaten/>
-			</#if>
-
-			<#if data.resultItem?? && !data.resultItem.isEmpty()>
-				if (itemstack.isEmpty()) {
-					return retval;
-				} else {
-					if (entity instanceof PlayerEntity) {
-						PlayerEntity player = (PlayerEntity) entity;
-						if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
-							player.dropItem(retval, false);
-					}
-					return itemstack;
-				}
-			<#else>
-				return retval;
-			</#if>
-		}
-        </#if>
-
-		<#if hasProcedure(data.onEntityHitWith)>
-		@Override public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
-			boolean retval = super.hitEntity(itemstack, entity, sourceentity);
-			double x = entity.getPosX();
-			double y = entity.getPosY();
-			double z = entity.getPosZ();
-			World world = entity.world;
-			<@procedureOBJToCode data.onEntityHitWith/>
-			return retval;
-		}
-        </#if>
-
-		<#if hasProcedure(data.onEntitySwing)>
-		@Override public boolean onEntitySwing(ItemStack itemstack, LivingEntity entity) {
-			boolean retval = super.onEntitySwing(itemstack, entity);
-			double x = entity.getPosX();
-			double y = entity.getPosY();
-			double z = entity.getPosZ();
-			World world = entity.world;
-			<@procedureOBJToCode data.onEntitySwing/>
-			return retval;
-		}
-		</#if>
-
-		<#if hasProcedure(data.onCrafted)>
-		@Override public void onCreated(ItemStack itemstack, World world, PlayerEntity entity) {
-			super.onCreated(itemstack, world, entity);
-			double x = entity.getPosX();
-			double y = entity.getPosY();
-			double z = entity.getPosZ();
-			<@procedureOBJToCode data.onCrafted/>
-		}
-        </#if>
-
-		<#if hasProcedure(data.onItemInUseTick) || hasProcedure(data.onItemInInventoryTick)>
-		@Override public void inventoryTick(ItemStack itemstack, World world, Entity entity, int slot, boolean selected) {
-			super.inventoryTick(itemstack, world, entity, slot, selected);
-			double x = entity.getPosX();
-			double y = entity.getPosY();
-			double z = entity.getPosZ();
-    		<#if hasProcedure(data.onItemInUseTick)>
-			if (selected)
-    	    <@procedureOBJToCode data.onItemInUseTick/>
-    		</#if>
-    		<@procedureOBJToCode data.onItemInInventoryTick/>
-		}
-		</#if>
-
-		<#if hasProcedure(data.onDroppedByPlayer)>
-        @Override public boolean onDroppedByPlayer(ItemStack itemstack, PlayerEntity entity) {
-            double x = entity.getPosX();
-            double y = entity.getPosY();
-            double z = entity.getPosZ();
-            World world = entity.world;
-            <@procedureOBJToCode data.onDroppedByPlayer/>
-            return true;
-        }
-        </#if>
-	}
-
-}
-<#-- @formatter:on -->

plugins/generator-1.16.5/forge-1.16.5/templates/item.java.ftl
@@ -110,9 +110,28 @@ import net.minecraft.entity.ai.attributes.Attributes;
 					.isImmuneToFire()
 					</#if>
 					.rarity(Rarity.${data.rarity})
+					<#if data.isFood>
+					.food((new Food.Builder())
+					 	.hunger(${data.nutritionalValue})
+					 	.saturation(${data.saturation}f)
+                     	<#if data.isAlwaysEdible>.setAlwaysEdible()</#if>
+                     	<#if data.isMeat>.meat()</#if>.build())
+                    </#if>
 			);
 			setRegistryName("${registryname}");
 		}
+
+        <#if data.hasNonDefaultAnimation()>
+        @Override public UseAction getUseAction(ItemStack itemstack) {
+            return UseAction.${data.animation?upper_case};
+        }
+        </#if>
+
+        <#if data.isFood && (data.animation == "drink")>
+        @Override public net.minecraft.util.SoundEvent getEatSound() {
+            return net.minecraft.util.SoundEvents.ENTITY_GENERIC_DRINK;
+        }
+        </#if>
 
 		<#if data.stayInGridWhenCrafting>
 			@Override public boolean hasContainerItem() {
@@ -327,6 +346,38 @@ import net.minecraft.entity.ai.attributes.Attributes;
             <@procedureOBJToCode data.onDroppedByPlayer/>
             return true;
         }
+        </#if>
+
+        <#if hasProcedure(data.onFinishUsingItem) || data.hasEatResultItem()>
+            @Override public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+        	    ItemStack retval =
+        		    <#if data.hasEatResultItem()>
+        			    ${mappedMCItemToItemStackCode(data.eatResultItem, 1)};
+        			</#if>
+        		super.onItemUseFinish(itemstack, world, entity);
+
+        		<#if hasProcedure(data.onFinishUsingItem)>
+        			double x = entity.getPosX();
+        			double y = entity.getPosY();
+        			double z = entity.getPosZ();
+        			<@procedureOBJToCode data.onFinishUsingItem/>
+        		</#if>
+
+        		<#if data.hasEatResultItem()>
+        			if (itemstack.isEmpty()) {
+        				return retval;
+        			} else {
+        				if (entity instanceof PlayerEntity) {
+        					PlayerEntity player = (PlayerEntity) entity;
+        					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+        						player.dropItem(retval, false);
+        				}
+        				return itemstack;
+        			}
+        		<#else>
+        			return retval;
+        		</#if>
+        	}
         </#if>
 
 		<#if data.guiBoundTo?has_content && data.guiBoundTo != "<NONE>">

plugins/generator-1.18.2/forge-1.18.2/food.definition.yaml
@@ -1,20 +1,0 @@
-templates:
-  - template: food.java.ftl
-    name: "@SRCROOT/@BASEPACKAGEPATH/item/@NAMEItem.java"
-  - template: json/item.json.ftl
-    writer: json
-    condition: "renderType #= 0"
-    name: "@MODASSETSROOT/models/item/@registryname.json"
-  - template: json/item_cmodel.json.ftl
-    writer: json
-    condition: "renderType #= 1"
-    name: "@MODASSETSROOT/models/item/@registryname.json"
-  - template: json/item_cmodel_obj.json.ftl
-    writer: json
-    condition: "renderType #= 2"
-    variables: "type=item"
-    name: "@MODASSETSROOT/models/item/@registryname.json"
-
-localizationkeys:
-  - key: item.@modid.@registryname
-    mapto: name

plugins/generator-1.18.2/forge-1.18.2/templates/food.java.ftl
@@ -1,131 +1,0 @@
-<#--
- # MCreator (https://mcreator.net/)
- # Copyright (C) 2012-2020, Pylo
- # Copyright (C) 2020-2021, Pylo, opensource contributors
- # 
- # This program is free software: you can redistribute it and/or modify
- # it under the terms of the GNU General Public License as published by
- # the Free Software Foundation, either version 3 of the License, or
- # (at your option) any later version.
- # 
- # This program is distributed in the hope that it will be useful,
- # but WITHOUT ANY WARRANTY; without even the implied warranty of
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- # GNU General Public License for more details.
- # 
- # You should have received a copy of the GNU General Public License
- # along with this program.  If not, see <https://www.gnu.org/licenses/>.
- # 
- # Additional permission for code generator templates (*.ftl files)
- # 
- # As a special exception, you may create a larger work that contains part or 
- # all of the MCreator code generator templates (*.ftl files) and distribute 
- # that work under terms of your choice, so long as that work isn't itself a 
- # template for code generation. Alternatively, if you modify or redistribute 
- # the template itself, you may (at your option) remove this special exception, 
- # which will cause the template and the resulting code generator output files 
- # to be licensed under the GNU General Public License without this special 
- # exception.
--->
-
-<#-- @formatter:off -->
-<#include "procedures.java.ftl">
-<#include "triggers.java.ftl">
-<#include "mcitems.ftl">
-
-package ${package}.item;
-
-
-public class ${name}Item extends Item {
-
-	public ${name}Item() {
-		super(new Item.Properties().tab(${data.creativeTab}).stacksTo(${data.stackSize}).rarity(Rarity.${data.rarity})
-			.food((new FoodProperties.Builder()).nutrition(${data.nutritionalValue}).saturationMod(${data.saturation}f)
-			<#if data.isAlwaysEdible>.alwaysEat()</#if>
-			<#if data.forDogs>.meat()</#if>
-			.build()));
-	}
-
-	<#if data.eatingSpeed != 32>
-	@Override public int getUseDuration(ItemStack stack) {
-		return ${data.eatingSpeed};
-	}
-	</#if>
-
-	<#if data.hasGlow>
-	@Override @OnlyIn(Dist.CLIENT) public boolean isFoil(ItemStack itemstack) {
-		<#if hasProcedure(data.glowCondition)>
-		Player entity = Minecraft.getInstance().player;
-		Level world = entity.level;
-		double x = entity.getX();
-		double y = entity.getY();
-		double z = entity.getZ();
-		return <@procedureOBJToConditionCode data.glowCondition/>;
-		<#else>
-		return true;
-		</#if>
-	}
-	</#if>
-
-	<#if data.animation != "eat">
-	@Override public UseAnim getUseAnimation(ItemStack itemstack) {
-		return UseAnim.${data.animation?upper_case};
-	}
-	</#if>
-
-	<#if data.specialInfo?has_content>
-	@Override public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, world, list, flag);
-		<#list data.specialInfo as entry>
-		list.add(new TextComponent("${JavaConventions.escapeStringForJava(entry)}"));
-		</#list>
-	}
-	</#if>
-
-	<@onRightClickedInAir data.onRightClicked/>
-
-	<@onItemUseFirst data.onRightClickedOnBlock/>
-
-	<#if hasProcedure(data.onEaten) || (data.resultItem?? && !data.resultItem.isEmpty())>
-	@Override public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
-		ItemStack retval =
-			<#if data.resultItem?? && !data.resultItem.isEmpty()>
-				${mappedMCItemToItemStackCode(data.resultItem, 1)};
-			</#if>
-		super.finishUsingItem(itemstack, world, entity);
-
-		<#if hasProcedure(data.onEaten)>
-			double x = entity.getX();
-			double y = entity.getY();
-			double z = entity.getZ();
-			<@procedureOBJToCode data.onEaten/>
-		</#if>
-
-		<#if data.resultItem?? && !data.resultItem.isEmpty()>
-			if (itemstack.isEmpty()) {
-				return retval;
-			} else {
-				if (entity instanceof Player player && !player.getAbilities().instabuild) {
-					if (!player.getInventory().add(retval))
-						player.drop(retval, false);
-				}
-				return itemstack;
-			}
-		<#else>
-			return retval;
-		</#if>
-	}
-	</#if>
-
-	<@onEntityHitWith data.onEntityHitWith/>
-
-	<@onEntitySwing data.onEntitySwing/>
-
-	<@onCrafted data.onCrafted/>
-
-	<@onItemTick data.onItemInUseTick, data.onItemInInventoryTick/>
-
-	<@onDroppedByPlayer data.onDroppedByPlayer/>
-
-}
-<#-- @formatter:on -->

plugins/generator-1.18.2/forge-1.18.2/templates/item/item.java.ftl
@@ -54,8 +54,22 @@ import javax.annotation.Nullable;
 				.fireResistant()
 				</#if>
 				.rarity(Rarity.${data.rarity})
+				<#if data.isFood>
+				.food((new FoodProperties.Builder())
+				    .nutrition(${data.nutritionalValue})
+                	.saturationMod(${data.saturation}f)
+                    <#if data.isAlwaysEdible>.alwaysEat()</#if>
+                    <#if data.isMeat>.meat()</#if>
+                    .build())
+                </#if>
 		);
 	}
+
+	<#if data.hasNonDefaultAnimation()>
+    @Override public UseAnim getUseAnimation(ItemStack itemstack) {
+        return UseAnim.${data.animation?upper_case};
+    }
+    </#if>
 
 	<#if data.stayInGridWhenCrafting>
 		@Override public boolean hasCraftingRemainingItem() {
@@ -184,6 +198,37 @@ import javax.annotation.Nullable;
 		return ar;
 	}
     </#if>
+
+    <#if hasProcedure(data.onFinishUsingItem) || data.hasEatResultItem()>
+        @Override public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+            ItemStack retval =
+        	    <#if data.hasEatResultItem()>
+        		    ${mappedMCItemToItemStackCode(data.eatResultItem, 1)};
+        		</#if>
+        	super.finishUsingItem(itemstack, world, entity);
+
+        	<#if hasProcedure(data.onFinishUsingItem)>
+        		double x = entity.getX();
+        	    double y = entity.getY();
+        		double z = entity.getZ();
+        		<@procedureOBJToCode data.onFinishUsingItem/>
+        	</#if>
+
+        	<#if data.hasEatResultItem()>
+        		if (itemstack.isEmpty()) {
+        			return retval;
+        		} else {
+        			if (entity instanceof Player player && !player.getAbilities().instabuild) {
+        				if (!player.getInventory().add(retval))
+        					player.drop(retval, false);
+        			}
+        			return itemstack;
+        		}
+        	<#else>
+        		return retval;
+        	</#if>
+        }
+       </#if>
 
     <@onItemUsedOnBlock data.onRightClickedOnBlock/>
 

plugins/generator-addon-1.18.x/addon-1.18.x/food.definition.yaml
@@ -1,13 +1,0 @@
-templates:
-  - template: food.json.ftl
-    writer: json
-    name: "@SRCROOT/items/@registryname.json"
-  - template: resourcepack/food.json.ftl
-    writer: json
-    name: "@RESROOT/items/@registryname.json"
-
-localizationkeys:
-  - key: item.@modid:@registryname.name
-    mapto: name
-
-field_inclusions: [hasGlow, stackSize, eatingSpeed, nutritionalValue, saturation, isAlwaysEdible, forDogs, texture, name]

commit 4d0ac1e09b2c3158c41d74ff4e8e3452bf2aa2a9

Fix some items not generating due to bug introduced in #2002

src/main/java/net/mcreator/element/types/Item.java
@@ -153,6 +153,6 @@ import java.util.Map;
 	}
 
 	public boolean hasEatResultItem() {
-		return isFood && !eatResultItem.isEmpty();
+		return isFood && eatResultItem != null && !eatResultItem.isEmpty();
 	}
 }