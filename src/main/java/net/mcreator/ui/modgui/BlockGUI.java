a97a04ba14060dc9005310a6b5d7f6282e16432b
AlsoSomeoneElse <64546452+AlsoSomeoneElse@users.noreply.github.com>
2022-04-26 08:22:31 +0200

    Added direction dependency to emitted redstone power (#2663)

--- src/main/java/net/mcreator/ui/modgui/BlockGUI.java
+++ src/main/java/net/mcreator/ui/modgui/BlockGUI.java
@@ -320,7 +320,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 
 		emittedRedstonePower = new NumberProcedureSelector(null, mcreator,
 				new JSpinner(new SpinnerNumberModel(15, 0, 15, 1)),
-				Dependency.fromString("x:number/y:number/z:number/world:world/blockstate:blockstate"));
+				Dependency.fromString("x:number/y:number/z:number/world:world/direction:direction/blockstate:blockstate"));
 
 		placingCondition = new ProcedureSelector(this.withEntry("block/placing_condition"), mcreator,
 				L10N.t("elementgui.block.event_placing_condition"), VariableTypeLoader.BuiltInTypes.LOGIC,
