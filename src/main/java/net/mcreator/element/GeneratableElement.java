e64762992611e8f185f8565b3fb512bbcbbd0605
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2022-06-29 16:30:03 +0200

    Fix workspace import failing in some cases

--- src/main/java/net/mcreator/element/GeneratableElement.java
+++ src/main/java/net/mcreator/element/GeneratableElement.java
@@ -149,7 +149,7 @@ public abstract class GeneratableElement {
 					IConverter converter = ConverterRegistry.getConverterForModElementType(newType);
 					if (converter != null) {
 						LOG.debug("Converting mod element " + this.lastModElement.getName() + " of type " + newType
-								+ " to potential alternative.");
+								+ " to a potential alternative.");
 
 						GeneratableElement result = converter.convert(this.workspace, new Unknown(this.lastModElement),
 								jsonElement);
