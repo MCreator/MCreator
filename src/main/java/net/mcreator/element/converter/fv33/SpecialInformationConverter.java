d242abfc5d5d20de6a87f6ed8ed2ab8a2e7794a9
Max094Reikeb <maxencebekier@gmail.com>
2022-07-03 20:12:02 +0200

    Fixed converters

--- src/main/java/net/mcreator/element/converter/fv33/SpecialInformationConverter.java
+++ src/main/java/net/mcreator/element/converter/fv33/SpecialInformationConverter.java
@@ -17,26 +17,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * MCreator (https://mcreator.net/)
- * Copyright (C) 2012-2020, Pylo
- * Copyright (C) 2020-2022, Pylo, opensource contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-package net.mcreator.element.converter.fv32;
+package net.mcreator.element.converter.fv33;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -67,6 +48,6 @@ public class SpecialInformationConverter<T extends ISpecialInformationHolder> im
 	}
 
 	@Override public int getVersionConvertingTo() {
-		return 32;
+		return 33;
 	}
 }
