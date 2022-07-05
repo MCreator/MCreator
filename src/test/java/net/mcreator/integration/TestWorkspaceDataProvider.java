c144907c45846d4a5aca60c2c78fd81e9c3201cb
MISMASJETU <fikejzv@gmail.com>
2022-07-05 16:45:38 +0200

    Test Workspace Edit

--- src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
+++ src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -716,7 +716,7 @@ public class TestWorkspaceDataProvider {
 			livingEntity.movementSpeed = 0.76;
 			livingEntity.armorBaseValue = 0.123;
 			livingEntity.health = 42;
-			livingEntity.trackingRange = 27;
+			livingEntity.trackingRange = 64;
 			livingEntity.waterMob = !_true;
 			livingEntity.flyingMob = !_true;
 			livingEntity.inventorySize = 10;
