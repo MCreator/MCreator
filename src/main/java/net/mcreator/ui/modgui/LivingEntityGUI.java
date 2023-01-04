e492694101dde963a8a1823163172879d606837c
AlsoSomeoneElse <2000someoneelse@gmail.com>
2023-01-04 18:39:09 +0100

    Removed old extension, updated base XML for living entities

--- src/main/java/net/mcreator/ui/modgui/LivingEntityGUI.java
+++ src/main/java/net/mcreator/ui/modgui/LivingEntityGUI.java
@@ -244,13 +244,15 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> {
 	}
 
 	private void setDefaultAISet() {
-		blocklyPanel.setXML("<xml xmlns=\"https://developers.google.com/blockly/xml\">"
-				+ "<block type=\"aitasks_container\" deletable=\"false\" x=\"40\" y=\"40\"><next>"
-				+ "<block type=\"attack_on_collide\"><field name=\"speed\">1.2</field><field name=\"longmemory\">FALSE</field><next>"
-				+ "<block type=\"wander\"><field name=\"speed\">1</field><next>"
-				+ "<block type=\"attack_action\"><field name=\"callhelp\">FALSE</field><next>"
-				+ "<block type=\"look_around\"><next><block type=\"swim_in_water\"/></next>"
-				+ "</block></next></block></next></block></next></block></next></block></xml>");
+		blocklyPanel.setXML("""
+			<xml xmlns="https://developers.google.com/blockly/xml">
+			<block type="aitasks_container" deletable="false" x="40" y="40"><next>
+			<block type="attack_on_collide"><field name="speed">1.2</field><field name="longmemory">FALSE</field><field name="condition"/><next>
+			<block type="wander"><field name="speed">1</field><field name="condition"/><next>
+			<block type="attack_action"><field name="callhelp">FALSE</field><field name="condition"/><next>
+			<block type="look_around"><field name="condition"/><next>
+			<block type="swim_in_water"/><field name="condition"/></next>
+			</block></next></block></next></block></next></block></next></block></xml>""");
 	}
 
 	private void regenerateAITasks() {
