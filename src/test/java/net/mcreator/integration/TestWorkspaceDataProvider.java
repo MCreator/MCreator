74e63010c7de23ac949dd4566f117eb4fd77f169
PooPmeep <112079900+NerdyPuzzle@users.noreply.github.com>
2023-01-20 02:13:56 +0200

    Update TestWorkspaceDataProvider.java

--- src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
+++ src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -594,7 +594,7 @@ public class TestWorkspaceDataProvider {
 				components.add(new TextField("text2", 55, 231, 90, 20, ""));
 				components.add(new Checkbox("checkbox1", 100, 100, "Text", new Procedure("condition1")));
 				components.add(new Checkbox("checkbox2", 125, 125, "Other text", new Procedure("condition2")));
-				components.add(new EntityModel(60, 20, new Procedure("procedure1"), new Procedure("condition1"), 30));
+				components.add(new EntityModel(60, 20, new Procedure("procedure1"), new Procedure("condition3"), 30));
 			}
 			gui.components = components;
 			return gui;
