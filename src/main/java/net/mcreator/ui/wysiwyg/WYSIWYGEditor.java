4963d6f1e3895b282796bd2d058171039d45c0f1
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-02-05 17:08:30 +0100

    Reorder for better UX

--- src/main/java/net/mcreator/ui/wysiwyg/WYSIWYGEditor.java
+++ src/main/java/net/mcreator/ui/wysiwyg/WYSIWYGEditor.java
@@ -55,8 +55,8 @@ public class WYSIWYGEditor extends JPanel {
 
 	public static final List<WYSIWYGComponentRegistration<?>> COMPONENT_REGISTRY = new ArrayList<>() {{
 		add(new WYSIWYGComponentRegistration<>("text_label", "addlabel", true, Label.class, LabelDialog.class));
-		add(new WYSIWYGComponentRegistration<>("button", "addbutton", false, Button.class, ButtonDialog.class));
 		add(new WYSIWYGComponentRegistration<>("image", "addimage", true, Image.class, ImageDialog.class));
+		add(new WYSIWYGComponentRegistration<>("button", "addbutton", false, Button.class, ButtonDialog.class));
 		add(new WYSIWYGComponentRegistration<>("imagebutton", "addimagebutton", false, ImageButton.class, ImageButtonDialog.class));
 		add(new WYSIWYGComponentRegistration<>("checkbox", "addcheckbox", false, Checkbox.class, CheckboxDialog.class));
 		add(new WYSIWYGComponentRegistration<>("text_input", "addtextinput", false, TextField.class,
