04a04eadc25d37f4750bff825ddab9fc1dcf49ac
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-02-06 21:13:59 +0100

    Do not generate feature from TWDP

--- src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
+++ src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -68,7 +68,7 @@ public class TestWorkspaceDataProvider {
 	}
 
 	public static List<GeneratableElement> getModElementExamplesFor(Workspace workspace, ModElementType<?> type,
-			Random random) {
+			boolean uiTest, Random random) {
 		List<GeneratableElement> generatableElements = new ArrayList<>();
 
 		if (type == ModElementType.RECIPE) {
@@ -95,23 +95,23 @@ public class TestWorkspaceDataProvider {
 			generatableElements.add(getToolExample(me(workspace, type, "11"), "Shears", random, true, false));
 			generatableElements.add(getToolExample(me(workspace, type, "12"), "Fishing rod", random, true, false));
 		} else if (type == ModElementType.TAB) {
-			generatableElements.add(getExampleFor(me(workspace, type, "1"), random, true, true, 0));
-			generatableElements.add(getExampleFor(me(workspace, type, "2"), random, true, false, 1));
+			generatableElements.add(getExampleFor(me(workspace, type, "1"), uiTest, random, true, true, 0));
+			generatableElements.add(getExampleFor(me(workspace, type, "2"), uiTest, random, true, false, 1));
 		} else if (type == ModElementType.COMMAND || type == ModElementType.FUNCTION || type == ModElementType.PAINTING
 				|| type == ModElementType.KEYBIND || type == ModElementType.PROCEDURE || type == ModElementType.FEATURE
 				|| type == ModElementType.CODE) {
 			generatableElements.add(
-					getExampleFor(new ModElement(workspace, "Example" + type.getRegistryName(), type), random, true,
+					getExampleFor(new ModElement(workspace, "Example" + type.getRegistryName(), type), uiTest, random, true,
 							true, 0));
 		} else {
-			generatableElements.add(getExampleFor(me(workspace, type, "1"), random, true, true, 0));
-			generatableElements.add(getExampleFor(me(workspace, type, "2"), random, true, false, 1));
-			generatableElements.add(getExampleFor(me(workspace, type, "3"), random, false, true, 2));
-			generatableElements.add(getExampleFor(me(workspace, type, "4"), random, false, false, 3));
-			generatableElements.add(getExampleFor(me(workspace, type, "5"), random, true, true, 3));
-			generatableElements.add(getExampleFor(me(workspace, type, "6"), random, true, false, 2));
-			generatableElements.add(getExampleFor(me(workspace, type, "7"), random, false, true, 1));
-			generatableElements.add(getExampleFor(me(workspace, type, "8"), random, false, false, 0));
+			generatableElements.add(getExampleFor(me(workspace, type, "1"), uiTest, random, true, true, 0));
+			generatableElements.add(getExampleFor(me(workspace, type, "2"), uiTest, random, true, false, 1));
+			generatableElements.add(getExampleFor(me(workspace, type, "3"), uiTest, random, false, true, 2));
+			generatableElements.add(getExampleFor(me(workspace, type, "4"), uiTest, random, false, false, 3));
+			generatableElements.add(getExampleFor(me(workspace, type, "5"), uiTest, random, true, true, 3));
+			generatableElements.add(getExampleFor(me(workspace, type, "6"), uiTest, random, true, false, 2));
+			generatableElements.add(getExampleFor(me(workspace, type, "7"), uiTest, random, false, true, 1));
+			generatableElements.add(getExampleFor(me(workspace, type, "8"), uiTest, random, false, false, 0));
 		}
 
 		generatableElements.removeAll(Collections.singleton(null));
@@ -268,7 +268,14 @@ public class TestWorkspaceDataProvider {
 		}
 	}
 
-	private static GeneratableElement getExampleFor(ModElement modElement, Random random, boolean _true,
+	/**
+	 * Provides list of GEs for tests
+	 *
+	 * @param modElement ME for this GE
+	 * @param uiTest true if test is UI test and not generator test
+	 * @return List of GEs for tests
+	 */
+	private static GeneratableElement getExampleFor(ModElement modElement, boolean uiTest, Random random, boolean _true,
 			boolean emptyLists, int valueIndex) {
 		List<MCItem> blocksAndItems = ElementUtil.loadBlocksAndItems(modElement.getWorkspace());
 		List<MCItem> blocks = ElementUtil.loadBlocks(modElement.getWorkspace());
@@ -1589,7 +1596,10 @@ public class TestWorkspaceDataProvider {
 			command.permissionLevel = getRandomString(random, List.of("No requirement", "1", "2", "3", "4"));
 			command.argsxml = Command.XML_BASE;
 			return command;
-		}  else if (ModElementType.FEATURE.equals(modElement.getType())) {
+		}
+		// As feature requires placement and feature to place, this GE is only returned for uiTests
+		// For generator tests, it will be tested by GTFeatureBlocks anyway
+		else if (ModElementType.FEATURE.equals(modElement.getType()) && uiTest) {
 			Feature feature = new Feature(modElement);
 			feature.generationStep = TestWorkspaceDataProvider.getRandomItem(random,
 					ElementUtil.getDataListAsStringArray("generationsteps"));
