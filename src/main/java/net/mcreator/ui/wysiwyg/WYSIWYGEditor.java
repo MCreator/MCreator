fccb0ffa694c65e6f7e5d841beab6fa2bdaa1d63
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-02-08 18:20:48 +0100

    Reordered buttons

--- src/main/java/net/mcreator/ui/wysiwyg/WYSIWYGEditor.java
+++ src/main/java/net/mcreator/ui/wysiwyg/WYSIWYGEditor.java
@@ -60,11 +60,11 @@ public class WYSIWYGEditor extends JPanel {
 		add(new WYSIWYGComponentRegistration<>("checkbox", "addcheckbox", false, Checkbox.class, CheckboxDialog.class));
 		add(new WYSIWYGComponentRegistration<>("text_input", "addtextinput", false, TextField.class,
 				TextFieldDialog.class));
+		add(new WYSIWYGComponentRegistration<>("entity_model", "addmodel", true, EntityModel.class, EntityModelDialog.class));
 		add(new WYSIWYGComponentRegistration<>("input_slot", "addinslot", false, InputSlot.class,
 				InputSlotDialog.class));
 		add(new WYSIWYGComponentRegistration<>("output_slot", "addoutslot", false, OutputSlot.class,
 				OutputSlotDialog.class));
-		add(new WYSIWYGComponentRegistration<>("entity_model", "addmodel", true, EntityModel.class, EntityModelDialog.class));
 	}};
 
 	public WYSIWYG editor = new WYSIWYG(this);
