MCreator/MCreator

commit 08836d1fe441c1dd8cc5aa7b0ce079672e7ba932

Added a condition for solid bounding boxes for entities (boat-like) + fixed small visual bug (#3389)
* Changes to the GUI + Entity template

* Changed the option to a condition

(Yes, the one true on the first override is intentional, I tested it and if it was set to anything else then the hitbox wasn't fully solid when you touched it, and it doesn't affect anything when it isn't solid.

* Oops didn't mean to make that change

* Unused import fix

* Add files via upload

* Add files via upload

* Update condition_solid_bounding_box.md

* Some stuff I forgot to change (again)

* Moved position to more appropriate position

* Wording

* Requested changes.

* Small identation fix

* UI reorder

* Better tip

* Requested changes

---------

Co-authored-by: KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>

plugins/generator-1.18.2/forge-1.18.2/templates/livingentity/livingentity.java.ftl
@@ -612,6 +612,25 @@ public class ${name}Entity extends ${extendsClass} <#if data.ranged>implements R
    	}
 	</#if>
 
+	<#if hasProcedure(data.solidBoundingBox) || data.solidBoundingBox.getFixedValue()>
+	@Override
+	public boolean canCollideWith(Entity entity) {
+		return true;
+	}
+
+	@Override
+	public boolean canBeCollidedWith() {
+		<#if hasProcedure(data.solidBoundingBox)>
+		Entity entity = this;
+		Level world = entity.level;
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		</#if>
+		return <@procedureOBJToConditionCode data.solidBoundingBox true false/>;
+	}
+	</#if>
+
 	<#if data.isBoss>
 	@Override public boolean canChangeDimensions() {
 		return false;

plugins/generator-1.19.2/forge-1.19.2/templates/livingentity/livingentity.java.ftl
@@ -590,6 +590,25 @@ public class ${name}Entity extends ${extendsClass} <#if data.ranged>implements R
    	}
 	</#if>
 
+	<#if hasProcedure(data.solidBoundingBox) || data.solidBoundingBox.getFixedValue()>
+	@Override
+	public boolean canCollideWith(Entity entity) {
+		return true;
+	}
+
+	@Override
+	public boolean canBeCollidedWith() {
+		<#if hasProcedure(data.solidBoundingBox)>
+		Entity entity = this;
+		Level world = entity.level;
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		</#if>
+		return <@procedureOBJToConditionCode data.solidBoundingBox true false/>;
+	}
+	</#if>
+
 	<#if data.isBoss>
 	@Override public boolean canChangeDimensions() {
 		return false;

plugins/mcreator-localization/lang/texts.properties
@@ -2232,7 +2232,8 @@ elementgui.keybind.key_binding_category=Key binding category translation key:
 elementgui.keybind.key_procedure_triggers=Key binding procedure triggers
 elementgui.keybind.error_key_needs_name=Key binding needs a name
 elementgui.keybind.error_key_category_needs_name=Key binding category key name
-elementgui.living_entity.disable_collisions=Disable collision box
+elementgui.living_entity.condition_solid_bounding_box=Solid bounding box condition
+elementgui.living_entity.disable_collisions=Disable entity pushing (collisions)
 elementgui.living_entity.has_ai=<html><b>Enable AI</b><br><small>When using AI, make sure to have some AI tasks defined below
 elementgui.living_entity.immune_fire=Fire
 elementgui.living_entity.immune_arrows=Arrows

src/main/java/net/mcreator/element/types/LivingEntity.java
@@ -27,6 +27,7 @@ import net.mcreator.element.parts.BiomeEntry;
 import net.mcreator.element.parts.MItemBlock;
 import net.mcreator.element.parts.Sound;
 import net.mcreator.element.parts.TabEntry;
+import net.mcreator.element.parts.procedure.LogicProcedure;
 import net.mcreator.element.parts.procedure.Procedure;
 import net.mcreator.element.types.interfaces.ICommonType;
 import net.mcreator.element.types.interfaces.IEntityWithModel;
@@ -61,6 +62,7 @@ import java.util.List;
 	public String mobModelGlowTexture;
 	public Procedure transparentModelCondition;
 	public Procedure isShakingCondition;
+	public LogicProcedure solidBoundingBox;
 
 	public double modelWidth, modelHeight, modelShadowSize;
 	public double mountedYOffset;

src/main/java/net/mcreator/ui/modgui/LivingEntityGUI.java
@@ -53,6 +53,8 @@ import net.mcreator.ui.init.UIRES;
 import net.mcreator.ui.laf.renderer.ModelComboBoxRenderer;
 import net.mcreator.ui.laf.renderer.WTextureComboBoxRenderer;
 import net.mcreator.ui.minecraft.*;
+import net.mcreator.ui.procedure.AbstractProcedureSelector;
+import net.mcreator.ui.procedure.LogicProcedureSelector;
 import net.mcreator.ui.procedure.ProcedureSelector;
 import net.mcreator.ui.validation.AggregatedValidationResult;
 import net.mcreator.ui.validation.Validator;
@@ -93,6 +95,7 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> {
 	private ProcedureSelector spawningCondition;
 	private ProcedureSelector transparentModelCondition;
 	private ProcedureSelector isShakingCondition;
+	private LogicProcedureSelector solidBoundingBox;
 
 	private final SoundSelector livingSound = new SoundSelector(mcreator);
 	private final SoundSelector hurtSound = new SoundSelector(mcreator);
@@ -336,6 +339,9 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> {
 				VariableTypeLoader.BuiltInTypes.LOGIC,
 				Dependency.fromString("x:number/y:number/z:number/world:world/entity:entity")).setDefaultName(
 				L10N.t("condition.common.false")).makeInline();
+		solidBoundingBox = new LogicProcedureSelector(this.withEntry("entity/condition_solid_bounding_box"), mcreator,
+				L10N.t("elementgui.living_entity.condition_solid_bounding_box"), AbstractProcedureSelector.Side.BOTH,
+				L10N.checkbox("elementgui.common.enable"), 300, Dependency.fromString("x:number/y:number/z:number/world:world/entity:entity"));
 
 		restrictionBiomes = new BiomeListField(mcreator);
 		breedTriggerItems = new MCItemListField(mcreator, ElementUtil::loadBlocksAndItems);
@@ -477,7 +483,7 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> {
 
 		pane1.add("Center", PanelUtils.totalCenterInPanel(PanelUtils.northAndCenterElement(subpane1, subpanel2)));
 
-		JPanel spo2 = new JPanel(new GridLayout(14, 2, 2, 2));
+		JPanel spo2 = new JPanel(new GridLayout(15, 2, 2, 2));
 
 		spo2.setOpaque(false);
 
@@ -601,8 +607,11 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> {
 
 		spo2.add(HelpUtils.wrapWithHelpButton(this.withEntry("entity/bounding_box"),
 				L10N.label("elementgui.living_entity.bounding_box")));
-		spo2.add(PanelUtils.join(FlowLayout.LEFT, 0, 0, modelWidth, modelHeight, new JEmptyBox(7, 7), modelShadowSize,
+		spo2.add(PanelUtils.join(FlowLayout.LEFT, 0, 0, modelWidth, new JEmptyBox(7, 7), modelHeight, new JEmptyBox(7, 7), modelShadowSize,
 				new JEmptyBox(7, 7), mountedYOffset, new JEmptyBox(7, 7), disableCollisions));
+
+		spo2.add(new JEmptyBox());
+		spo2.add(solidBoundingBox);
 
 		spo2.add(HelpUtils.wrapWithHelpButton(this.withEntry("entity/spawn_egg_options"),
 				L10N.label("elementgui.living_entity.spawn_egg_options")));
@@ -881,6 +890,7 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> {
 		spawningCondition.refreshListKeepSelected();
 		transparentModelCondition.refreshListKeepSelected();
 		isShakingCondition.refreshListKeepSelected();
+		solidBoundingBox.refreshListKeepSelected();
 
 		ComboBoxUtil.updateComboBoxContents(mobModelTexture, ListUtils.merge(Collections.singleton(""),
 				mcreator.getFolderManager().getTexturesList(TextureType.ENTITY).stream().map(File::getName)
@@ -935,6 +945,7 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> {
 		mobModelGlowTexture.setSelectedItem(livingEntity.mobModelGlowTexture);
 		transparentModelCondition.setSelectedProcedure(livingEntity.transparentModelCondition);
 		isShakingCondition.setSelectedProcedure(livingEntity.isShakingCondition);
+		solidBoundingBox.setSelectedProcedure(livingEntity.solidBoundingBox);
 		mobSpawningType.setSelectedItem(livingEntity.mobSpawningType);
 		rangedItemType.setSelectedItem(livingEntity.rangedItemType);
 		spawnEggBaseColor.setColor(livingEntity.spawnEggBaseColor);
@@ -1062,6 +1073,7 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> {
 		livingEntity.spawnEggBaseColor = spawnEggBaseColor.getColor();
 		livingEntity.transparentModelCondition = transparentModelCondition.getSelectedProcedure();
 		livingEntity.isShakingCondition = isShakingCondition.getSelectedProcedure();
+		livingEntity.solidBoundingBox = solidBoundingBox.getSelectedProcedure();
 		livingEntity.spawnEggDotColor = spawnEggDotColor.getColor();
 		livingEntity.hasSpawnEgg = hasSpawnEgg.isSelected();
 		livingEntity.disableCollisions = disableCollisions.isSelected();

src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -602,6 +602,7 @@ public class TestWorkspaceDataProvider {
 			livingEntity.mobModelGlowTexture = emptyLists ? "" : "test.png";
 			livingEntity.transparentModelCondition = new Procedure("condition1");
 			livingEntity.isShakingCondition = new Procedure("condition2");
+			livingEntity.solidBoundingBox = new LogicProcedure(_true ? "condition3" : null, _true);
 			livingEntity.mobModelName = getRandomItem(random, LivingEntityGUI.builtinmobmodels).getReadableName();
 			livingEntity.spawnEggBaseColor = Color.red;
 			livingEntity.spawnEggDotColor = Color.green;

commit c87da09f9747729c26887ede6c8608298d2c4f6f

Improve converter tests and fix converter (#3550)
* Tests should fail if generation fails

* Added procedure blocks to 2020.2 workspace

* Fix a bug detected by the improved tests

* Made converters more vocal

* Added procedures to the tests for newer workspaces

Co-authored-by: Defeatomizer <drag7398@gmail.com>

* Fix a bug introduced by #3389

---------

Co-authored-by: Defeatomizer <drag7398@gmail.com>

plugins/generator-1.18.2/forge-1.18.2/templates/livingentity/livingentity.java.ftl
@@ -612,7 +612,7 @@ public class ${name}Entity extends ${extendsClass} <#if data.ranged>implements R
    	}
 	</#if>
 
-	<#if hasProcedure(data.solidBoundingBox) || data.solidBoundingBox.getFixedValue()>
+	<#if data.solidBoundingBox?? && (hasProcedure(data.solidBoundingBox) || data.solidBoundingBox.getFixedValue())>
 	@Override
 	public boolean canCollideWith(Entity entity) {
 		return true;

plugins/generator-1.19.2/forge-1.19.2/templates/livingentity/livingentity.java.ftl
@@ -590,7 +590,7 @@ public class ${name}Entity extends ${extendsClass} <#if data.ranged>implements R
    	}
 	</#if>
 
-	<#if hasProcedure(data.solidBoundingBox) || data.solidBoundingBox.getFixedValue()>
+	<#if data.solidBoundingBox?? && (hasProcedure(data.solidBoundingBox) || data.solidBoundingBox.getFixedValue())>
 	@Override
 	public boolean canCollideWith(Entity entity) {
 		return true;

src/main/java/net/mcreator/element/GeneratableElement.java
@@ -37,6 +37,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class GeneratableElement {
 
@@ -140,15 +141,18 @@ public abstract class GeneratableElement {
 				if (importedFormatVersion != GeneratableElement.formatVersion) {
 					List<IConverter> converters = ConverterRegistry.getConvertersForModElementType(modElementType);
 					if (converters != null) {
+						AtomicInteger versionIncrementer = new AtomicInteger(importedFormatVersion);
 						converters.stream()
 								.filter(converter -> importedFormatVersion < converter.getVersionConvertingTo())
 								.sorted().forEach(converter -> {
-									LOG.debug("Converting mod element " + this.lastModElement.getName() + " (" + modElementType
-											+ ") from FV" + importedFormatVersion + " to FV"
-											+ converter.getVersionConvertingTo());
+									LOG.debug(
+											"Converting " + this.lastModElement.getName() + " (" + modElementType + ") from FV"
+													+ versionIncrementer.get() + " to FV" + converter.getVersionConvertingTo()
+													+ " using " + converter.getClass().getSimpleName());
 									generatableElement[0] = converter.convert(this.workspace, generatableElement[0],
 											jsonElement);
 									generatableElement[0].conversionApplied = true;
+									versionIncrementer.set(converter.getVersionConvertingTo());
 								});
 					}
 				}
@@ -172,10 +176,12 @@ public abstract class GeneratableElement {
 								workspace.getModElementManager().storeModElement(result);
 
 								LOG.debug("Converted mod element " + this.lastModElement.getName() + " (" + newType
-										+ ") to " + result.getModElement().getType().getRegistryName());
+										+ ") to " + result.getModElement().getType().getRegistryName() + " using "
+										+ converter.getClass().getSimpleName());
 							} else {
 								LOG.debug("Converted mod element " + this.lastModElement.getName() + " (" + newType
-										+ ") to data format that is not a mod element");
+										+ ") to data format that is not a mod element using " + converter.getClass()
+										.getSimpleName());
 							}
 						} catch (Exception e2) {
 							LOG.warn("Failed to convert mod element " + this.lastModElement.getName() + " of type "

src/main/java/net/mcreator/element/converter/v2022_2/LegacyShootArrowProcedureRemover.java
@@ -76,6 +76,9 @@ public class LegacyShootArrowProcedureRemover implements IConverter {
 				if (projectileField != null) {
 					element.removeChild(projectileField); // This is moved to the arrow block
 					projectileField.setAttribute("name", "projectile");
+				} else { // projectile filed is null, really old procedure blocks did not have arrow field
+					projectileField = bh.createField("projectile", "Arrow");
+					projectileField.setAttribute("name", "projectile");
 				}
 
 				Element shooterValue = null, damageValue = null, knockbackValue = null;

src/test/java/net/mcreator/integration/WorkspaceConvertersTest.java
@@ -108,7 +108,7 @@ public class WorkspaceConvertersTest {
 						ge.getModElement().reinit(workspace);
 
 						// test if GE definition is valid enough to be generated
-						workspace.getGenerator().generateElement(ge);
+						assertTrue(workspace.getGenerator().generateElement(ge));
 					}
 				}
 			});