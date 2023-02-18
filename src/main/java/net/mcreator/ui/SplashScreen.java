209644b0d067fc06869e66b913d89d9b1f0932d9
Klemen <16374228+KlemenDEV@users.noreply.github.com>
2023-02-18 11:31:18 +0100

    Fix application loader (#3572)

--- src/main/java/net/mcreator/ui/SplashScreen.java
+++ src/main/java/net/mcreator/ui/SplashScreen.java
@@ -21,7 +21,6 @@ package net.mcreator.ui;
 import net.mcreator.Launcher;
 import net.mcreator.ui.component.ImagePanel;
 import net.mcreator.ui.component.ProgressBar;
-import net.mcreator.ui.component.util.EDTUtils;
 import net.mcreator.ui.init.UIRES;
 import net.mcreator.util.image.ImageUtils;
 
@@ -98,9 +97,10 @@ public class SplashScreen extends JWindow {
 	}
 
 	public void setProgress(int percentage, String message) {
-		initloadprogress.setCurrentValue(percentage);
-		loadstate.setText(message);
-		EDTUtils.requestNonBlockingUIRefresh();
+		SwingUtilities.invokeLater(() -> {
+			initloadprogress.setCurrentValue(percentage);
+			loadstate.setText(message);
+		});
 	}
 
 }
