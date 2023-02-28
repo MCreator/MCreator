486a539c3416989d8e47f272f72df6931694af59
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2023-02-28 12:47:57 +0100

    Modified yet again to match specifications

--- src/main/java/net/mcreator/ui/validation/validators/UniqueNameValidator.java
+++ src/main/java/net/mcreator/ui/validation/validators/UniqueNameValidator.java
@@ -21,6 +21,7 @@ package net.mcreator.ui.validation.validators;
 
 import net.mcreator.ui.init.L10N;
 import net.mcreator.ui.validation.Validator;
+import net.mcreator.util.StringUtils;
 
 import java.util.Collections;
 import java.util.List;
@@ -126,7 +127,7 @@ public class UniqueNameValidator implements Validator {
 			return new ValidationResult(ValidationResultType.ERROR, L10N.t("validators.unique_name.empty", name));
 		if (otherNames.get().filter(textCheck(uniqueName)).count() > (isPresentOnList ? 1 : 0)
 				|| forbiddenNames.contains(uniqueName))
-			return new ValidationResult(ValidationResultType.ERROR, L10N.t("validators.unique_name.duplicate", name.toLowerCase()));
+			return new ValidationResult(ValidationResultType.ERROR, L10N.t("validators.unique_name.duplicate", StringUtils.lowercaseFirstLetter(name)));
 
 		return extraValidator.validate();
 	}
