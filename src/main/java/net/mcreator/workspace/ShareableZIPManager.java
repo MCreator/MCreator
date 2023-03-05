a63fb67ed0ee4c80f49d70ccbc57899a30f7d942
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-03-05 11:40:17 +0100

    This one will need a different approach

--- src/main/java/net/mcreator/workspace/ShareableZIPManager.java
+++ src/main/java/net/mcreator/workspace/ShareableZIPManager.java
@@ -107,8 +107,7 @@ public class ShareableZIPManager {
 			dial.hideAll();
 		});
 		t.start();
-
-		SwingUtilities.invokeLater(() -> dial.setVisible(true));
+		dial.setVisible(true);
 
 		return retval.get();
 	}
@@ -139,7 +138,7 @@ public class ShareableZIPManager {
 			dial.hideAll();
 		});
 		t.start();
-		SwingUtilities.invokeLater(() -> dial.setVisible(true));
+		dial.setVisible(true);
 	}
 
 }
