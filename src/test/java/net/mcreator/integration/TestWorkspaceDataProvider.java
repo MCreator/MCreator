1d459cd59e1399e4fc819e2b62409648f94511e8
PooPmeep <112079900+NerdyPuzzle@users.noreply.github.com>
2023-03-06 23:58:31 +0200

    so condition 5 is not a thing

--- src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
+++ src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -616,7 +616,7 @@ public class TestWorkspaceDataProvider {
 								30, 270, !_true));
 				components.add(new Tooltip(AbstractWYSIWYGDialog.textToMachineName(components, null,
 						"This is --...p a test string ŽĐĆ @ /test//\" tes___"), 20, 40, 70, 10,
-							new StringProcedure(_true ? "string1" : null, "fixed value 1"), new Procedure("condition5")));
+							new StringProcedure(_true ? "string1" : null, "fixed value 1"), new Procedure("condition4")));
 			}
 			gui.components = components;
 			return gui;
