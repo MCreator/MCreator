acaf73f8214eafb66a9a519423b8e95dd0f02fb0
Will <Williamthebest666@gmail.com>
2023-03-29 11:20:51 -0400

    Compiled separate executable for x86

--- platform/mac/mcreator.sh
+++ platform/mac/mcreator.sh
@@ -6,6 +6,7 @@
 #
 
 # WARNING: WHEN CHANGING THIS FILE, "mcreator" binary needs to be recompiled using "shc -v -r -f mcreator.sh"
+# TO COMPILE FOR INTEL ON AN ARM MACHINE, USE "arch -x86_64" PREFIX FOR THE X86 FILE
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 
