1db740bc51a08bdb9b88bb6c172abe5a5deac7ff
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2023-04-09 00:15:58 +0200

    Splited sound selector into a generic selector and a new one

--- src/main/java/net/mcreator/ui/minecraft/SoundSelector.java
+++ src/main/java/net/mcreator/ui/minecraft/SoundSelector.java
@@ -1,6 +1,7 @@
 /*
  * MCreator (https://mcreator.net/)
- * Copyright (C) 2020 Pylo and contributors
+ * Copyright (C) 2012-2020, Pylo
+ * Copyright (C) 2020-2023, Pylo, opensource contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,60 +22,39 @@ package net.mcreator.ui.minecraft;
 import net.mcreator.element.parts.Sound;
 import net.mcreator.minecraft.ElementUtil;
 import net.mcreator.ui.MCreator;
-import net.mcreator.ui.component.util.ComponentUtils;
-import net.mcreator.ui.component.util.PanelUtils;
+import net.mcreator.ui.component.JSelector;
 import net.mcreator.ui.dialogs.StringSelectorDialog;
 import net.mcreator.ui.init.L10N;
 import net.mcreator.ui.init.UIRES;
-import net.mcreator.ui.validation.component.VTextField;
 import net.mcreator.util.ListUtils;
 import net.mcreator.util.SoundUtils;
 import net.mcreator.workspace.elements.SoundElement;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 
-public class SoundSelector extends JPanel {
+public class SoundSelector extends JSelector<Sound> {
 
-	private final VTextField tfe = new VTextField(14);
-	private final JButton bt = new JButton("...");
-	private final JButton rm = new JButton(UIRES.get("18px.remove"));
 	private final JButton play = new JButton(UIRES.get("16px.play"));
 
 	private final MCreator mcreator;
 
 	public SoundSelector(MCreator frame) {
+		super(false, false);
 		this.mcreator = frame;
 
-		setOpaque(false);
-		bt.addActionListener(event -> {
-			String s = StringSelectorDialog.openSelectorDialog(mcreator, ElementUtil::getAllSounds,
-					L10N.t("dialog.selector.sound.title"), L10N.t("dialog.selector.sound.message"));
-			if (s != null)
-				setSound(s);
-		});
-		rm.addActionListener(e -> setSound((String) null));
-		tfe.setEditable(false);
-		ComponentUtils.deriveFont(tfe, 16);
-		bt.setOpaque(false);
-
-		rm.setOpaque(false);
-		rm.setMargin(new Insets(0, 3, 0, 3));
-
+		play.setVisible(false);
 		play.setOpaque(false);
 		play.setMargin(new Insets(0, 0, 0, 0));
 		play.setContentAreaFilled(false);
-
-		play.setVisible(false);
-		rm.setEnabled(false);
-
 		play.addMouseListener(new MouseAdapter() {
 			@Override public void mousePressed(MouseEvent me) {
 				SoundElement soundElement = frame.getWorkspace().getSoundElements().stream()
-						.filter(e -> e.getName().equals(getSound().getUnmappedValue().replaceFirst("CUSTOM:", "")))
+						.filter(e -> e.getName().equals(getSelected().getUnmappedValue().replaceFirst("CUSTOM:", "")))
 						.findFirst().orElse(null);
 				if (soundElement != null) {
 					if (!soundElement.getFiles().isEmpty()) {
@@ -92,49 +72,47 @@ public class SoundSelector extends JPanel {
 
 		});
 
-		setLayout(new BorderLayout(0, 0));
-
 		add("West", play);
-		add("Center", tfe);
-		add("East", PanelUtils.gridElements(1, 2, 0, 0, bt, rm));
+	}
+
+	@Override public void btListener(ActionEvent event) {
+		String s = StringSelectorDialog.openSelectorDialog(mcreator, ElementUtil::getAllSounds,
+				L10N.t("dialog.selector.sound.title"), L10N.t("dialog.selector.sound.message"));
+		if (s != null)
+			setSelected(s);
+	}
+
+	@Override public void rmListener(ActionEvent event) {
+		setSelected((String) null);
 	}
 
 	@Override public void setEnabled(boolean enabled) {
-		tfe.setEnabled(enabled);
-		bt.setEnabled(enabled);
-		rm.setEnabled(enabled);
+		super.setEnabled(enabled);
+		play.setEnabled(enabled);
 	}
 
-	public VTextField getVTextField() {
-		return tfe;
-	}
-
-	public Sound getSound() {
+	@Override public Sound getSelected() {
 		return new Sound(mcreator.getWorkspace(), tfe.getText());
 	}
 
-	public void setSound(Sound sound) {
-		if (sound != null)
-			this.setSound(sound.getUnmappedValue());
+	@Override public void setSelected(Sound selected) {
+		if (selected != null)
+			this.setSelected(selected.getUnmappedValue());
 		else
-			this.setSound((String) null);
+			this.setSelected((String) null);
 	}
 
-	public void setSound(String sound) {
-		tfe.setText(sound);
+	@Override public void setSelected(String selected) {
+		tfe.setText(selected);
 		tfe.getValidationStatus();
 
-		if (sound != null && !sound.equals("")) {
-			play.setVisible(sound.startsWith("CUSTOM:"));
+		if (selected != null && !selected.equals("")) {
+			play.setVisible(selected.startsWith("CUSTOM:"));
 			rm.setEnabled(true);
 		} else {
 			play.setVisible(false);
 			rm.setEnabled(false);
 		}
-	}
-
-	public void setText(String text) {
-		this.setSound(text);
 	}
 
 }
