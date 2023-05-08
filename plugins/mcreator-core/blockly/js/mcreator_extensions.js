3ed8c9596b9d3c1b0892c13695bdf174baddfa68
Defeatomizer <drag7398@gmail.com>
2023-05-08 14:20:36 +0300

    Better

--- plugins/mcreator-core/blockly/js/mcreator_extensions.js
+++ plugins/mcreator-core/blockly/js/mcreator_extensions.js
@@ -54,8 +54,11 @@ Blockly.Extensions.register('procedure_dependencies_onchange_mixin',
                 this.getInput('arg' + i).setCheck(javabridge.getDependencyType(
                     this.getFieldValue('procedure'), this.getFieldValue('name' + i)));
                 const newType = this.getInput('arg' + i).connection.getCheck();
-                if ((prevType == null) != (newType == null) || (prevType.toString() !== newType.toString()))
+                if ((prevType && newType) ?
+                    JSON.stringify(prevType) !== JSON.stringify(newType) :
+                    (prevType == null) !== (newType == null)) {
                     Blockly.Events.fire(new InputCheckChange(this, 'arg' + i, prevType, newType));
+                }
             }
             Blockly.Events.setGroup(group);
         });
