ebc03f86284eb6c48fb5ccc4e2ef9bd79237c37a
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-05-12 11:23:59 +0200

    Unclear DOCS

--- src/main/java/net/mcreator/element/types/interfaces/IOtherModElementsDependent.java
+++ src/main/java/net/mcreator/element/types/interfaces/IOtherModElementsDependent.java
@@ -33,9 +33,10 @@ import java.util.Collections;
 public interface IOtherModElementsDependent {
 
 	/**
-	 * @return List of names of all elements used by mod element instance and not already provided as names of entries
-	 * in the list returned by {@link #getUsedElementMappings()} (with {@code CUSTOM:} prefix for entries created
-	 * from other mod elements).
+	 * @return List of names of all elements (mappable or MEs) used by mod element instance and not already provided as names of entries
+	 * in the list returned by {@link #getUsedElementMappings()}
+	 * <p>
+	 * Format of string in returned collection: with {@code CUSTOM:} prefix for entries created from other mod elements or unmapped name for datalist entries
 	 */
 	default Collection<String> getUsedElementNames() {
 		return Collections.emptyList();
