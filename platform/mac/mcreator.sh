dfb419312a7a3605c1e40d6582105eb63eb1e907
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-05-15 20:48:32 +0200

    Fix icon path

--- platform/mac/mcreator.sh
+++ platform/mac/mcreator.sh
@@ -21,7 +21,7 @@ cd ..
 ./jdk.bundle/Contents/Home/bin/java \
   --add-opens=java.base/java.lang=ALL-UNNAMED \
   -Xdock:name=MCreator \
-  -Xdock:icon=../Resources/mcreatorapp.icns \
+  -Xdock:icon=./Resources/mcreatorapp.icns \
   -Dapple.awt.application.appearance=system \
   -cp .:./lib/mcreator.jar:./lib/* \
   net.mcreator.Launcher
