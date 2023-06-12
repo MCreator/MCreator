08127bfc7e862386a019aec84609fcf40f4edcd4
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-06-12 18:02:52 +0200

    Remove comment

--- src/main/java/net/mcreator/blockly/data/ExternalBlockLoader.java
+++ src/main/java/net/mcreator/blockly/data/ExternalBlockLoader.java
@@ -219,7 +219,6 @@ public class ExternalBlockLoader {
 			return null;
 
 		return translation.replace("'", "\\'").replace("\"", "&quot;");
-		//return translation.replace("\\", "\\\\").replace("\"", "\\\"").replace("'", "\\'");
 	}
 
 }
