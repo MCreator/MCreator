9ceddf84ff9e36c2c25801005452921a8f25fc7d
Defeatomizer <drag7398@gmail.com>
2023-06-13 08:31:21 +0300

    Code reformat

--- src/main/java/net/mcreator/blockly/data/BlocklyXML.java
+++ src/main/java/net/mcreator/blockly/data/BlocklyXML.java
@@ -25,5 +25,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD) @Retention(RetentionPolicy.RUNTIME) public @interface BlocklyXML {
+
 	String value();
+
 }
