7efe9a66c20bea0f0ad5114eea05c9375344f0c6
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-09-13 18:24:00 +0200

    Tool UI improve

--- src/main/java/net/mcreator/ui/modgui/ToolGUI.java
+++ src/main/java/net/mcreator/ui/modgui/ToolGUI.java
@@ -189,16 +189,7 @@ public class ToolGUI extends ModElementGUI<Tool> {
 				HelpUtils.wrapWithHelpButton(this.withEntry("item/glowing_effect"),
 						L10N.label("elementgui.tool.glowing_effect")), hasGlow, glowCondition);
 
-		JPanel destal = new JPanel(new GridLayout(1, 1, 15, 15));
-		destal.setOpaque(false);
-		destal.setBorder(BorderFactory.createTitledBorder(
-				BorderFactory.createLineBorder((Color) UIManager.get("MCreatorLAF.BRIGHT_COLOR"), 1),
-				L10N.t("elementgui.common.special_information"), 0, 0, getFont().deriveFont(12.0f),
-				(Color) UIManager.get("MCreatorLAF.BRIGHT_COLOR")));
-
-		destal.add(specialInformation);
-
-		JComponent visualBottom = PanelUtils.centerAndSouthElement(destal, glow, 10, 10);
+		JComponent visualBottom = PanelUtils.centerAndSouthElement(glow, specialInformation, 10, 10);
 
 		pane2.setOpaque(false);
 		pane2.add("Center", PanelUtils.totalCenterInPanel(PanelUtils.northAndCenterElement(
