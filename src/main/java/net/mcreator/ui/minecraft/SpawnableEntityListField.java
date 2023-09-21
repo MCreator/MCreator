fb268487852ede45fc3f0ffed3a26e82d0171460
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-09-21 21:38:06 +0200

    Manual merge

--- src/main/java/net/mcreator/ui/minecraft/SpawnableEntityListField.java
+++ src/main/java/net/mcreator/ui/minecraft/SpawnableEntityListField.java
@@ -48,19 +48,8 @@ public class SpawnableEntityListField extends JItemListField<EntityEntry> {
 	@Override protected List<EntityEntry> getTagsToAdd() {
 		List<EntityEntry> tags = new ArrayList<>();
 
-		String tag = AddTagDialog.openAddTagDialog(mcreator, mcreator, "Entities", "bosses", "raiders", "fall_damage_immune",
+		String tag = AddTagDialog.openAddTagDialog(mcreator, "bosses", "raiders", "fall_damage_immune",
 				"powder_snow_walkable_mobs");
-		if (tag != null)
-			tags.add(new EntityEntry(mcreator.getWorkspace(), "#" + tag));
-
-		return tags;
-	}
-
-
-	@Override protected List<EntityEntry> getTagsToAdd() {
-		List<EntityEntry> tags = new ArrayList<>();
-
-		String tag = AddTagDialog.openAddTagDialog(mcreator, "bosses", "raiders", "fall_damage_immune", "powder_snow_walkable_mobs");
 		if (tag != null)
 			tags.add(new EntityEntry(mcreator.getWorkspace(), "#" + tag));
 
