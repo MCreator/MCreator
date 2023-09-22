06d9d11f81bbb4e8d29c8b5e7040f76e9f05c784
Defeatomizer <drag7398@gmail.com>
2023-09-22 22:58:12 +0300

    Gone too far

--- src/main/resources/blockly/js/mcreator_blockly.js
+++ src/main/resources/blockly/js/mcreator_blockly.js
@@ -173,3 +173,18 @@ function workspaceToXML() {
 
     return Blockly.Xml.domToText(treeXml);
 }
+
+class InputCheckChange extends Blockly.Events.BlockChange {
+    constructor(block, name, oldValue, newValue) {
+        super(block, null, name, oldValue, newValue);
+    }
+
+    run(forward) {
+        const block = this.blockId && this.getEventWorkspace_().getBlockById(this.blockId);
+        if (block) {
+            block.getInput(this.name).setCheck(forward ? this.newValue : this.oldValue);
+            Blockly.Events.fire(new InputCheckChange(block, this.name, forward ? this.oldValue : this.newValue,
+                block.getInput(this.name).connection.getCheck()));
+        }
+    }
+}
