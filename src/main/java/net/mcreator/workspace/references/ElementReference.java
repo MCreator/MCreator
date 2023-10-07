7894194e6b00eb1785ddac3efa19cc46935e489a
Defeatomizer <drag7398@gmail.com>
2023-10-07 05:30:15 +0000

    Element reference documentation

--- src/main/java/net/mcreator/workspace/references/ElementReference.java
+++ src/main/java/net/mcreator/workspace/references/ElementReference.java
@@ -19,13 +19,29 @@
 
 package net.mcreator.workspace.references;
 
+import net.mcreator.element.parts.procedure.Procedure;
+import net.mcreator.generator.mapping.MappableElement;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Collection;
 
+/**
+ * Used to mark fields storing names of custom elements (mappable or MEs) used by mod element instance
+ * and NOT already included as names of objects like {@link MappableElement} or {@link Procedure}.
+ * <br>Format of strings: with {@code CUSTOM:} prefix for entries created from other mod elements
+ * or unmapped name for data list entries.
+ *
+ * @apiNote This annotation can also be added to fields of type {@link Collection}, custom object, etc.
+ * with their values known to have fields/methods with matching values.
+ */
 @Target(ElementType.FIELD) @Retention(RetentionPolicy.RUNTIME) public @interface ElementReference {
 
+	/**
+	 * Special values indicating the element is not assigned.
+	 */
 	String[] defaultValues() default "";
 
 }
