d08882203b223bbea79ad126a742a4e8548c4184
Defeatomizer <drag7398@gmail.com>
2023-10-13 22:55:42 +0300

    Bump

--- src/main/java/net/mcreator/element/converter/v2023_4/CallProcedureAtBlockConverter.java
+++ src/main/java/net/mcreator/element/converter/v2023_4/CallProcedureAtBlockConverter.java
@@ -40,7 +40,7 @@ import java.io.StringWriter;
 public class CallProcedureAtBlockConverter extends ProcedureConverter {
 
 	@Override public int getVersionConvertingTo() {
-		return 54;
+		return 55;
 	}
 
 	@Override protected String fixXML(Procedure procedure, String xml) throws Exception {
