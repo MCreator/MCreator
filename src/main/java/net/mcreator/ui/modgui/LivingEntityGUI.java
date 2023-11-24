c3dbb111bbbb80a1173fa0b7b56c846751229fa0
Defeatomizer <drag7398@gmail.com>
2023-11-24 12:36:48 +0000

    Per-page scrolling (can only do it this way)

--- src/main/java/net/mcreator/ui/modgui/LivingEntityGUI.java
+++ src/main/java/net/mcreator/ui/modgui/LivingEntityGUI.java
@@ -909,7 +909,7 @@ public class LivingEntityGUI extends ModElementGUI<LivingEntity> implements IBlo
 		pane1.setOpaque(false);
 
 		addPage(L10N.t("elementgui.living_entity.page_visual"), pane2);
-		addPage(L10N.t("elementgui.living_entity.page_model_layers"), pane8);
+		addPage(L10N.t("elementgui.living_entity.page_model_layers"), pane8, false);
 		addPage(L10N.t("elementgui.living_entity.page_sound"), pane6);
 		addPage(L10N.t("elementgui.living_entity.page_behaviour"), pane1);
 		addPage(L10N.t("elementgui.living_entity.page_entity_data"), entityDataListPanel, false);
