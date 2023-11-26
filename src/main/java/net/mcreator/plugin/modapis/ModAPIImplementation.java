a39d69b32086c24f666f99b500ffa88690455034
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2023-11-26 11:05:24 +0100

    Minor refactoring of ModAPI to move it to more suitable package

--- src/main/java/net/mcreator/plugin/modapis/ModAPIImplementation.java
+++ src/main/java/net/mcreator/plugin/modapis/ModAPIImplementation.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package net.mcreator.minecraft.api;
+package net.mcreator.plugin.modapis;
 
 import javax.annotation.Nullable;
 import java.util.List;
