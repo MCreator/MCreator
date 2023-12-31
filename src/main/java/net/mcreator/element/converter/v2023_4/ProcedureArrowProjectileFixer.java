MCreator/MCreator

commit 114a1d6e3f328777bb1d0de1acce01908c0ec706

Projectile mod element and Remove ranged item mod element (#3456)
* Add a projectile mod element

* Update tooltips

English and French tooltips have been changed, but Russian tips still need to change the bullet and ranged item words for projectile.

* 1.19.2 projectile code + some more changes

* Forge 1.18.2 + Converter + Remove things from the ranged item ME + More changes

* Update tests

* Fix several things + change custom entities

* Add a converter for procedures

* Improve the conversion for the item projectile

Just in case a workspace would have no item selected for some reason

* Trying to remove completely the ranged item element, but the converter doesn't want to work for some reasons

* Remove ranged item from tests

* Added class back to resolve merge conflicts

* Removed class again and moved converters

* Removed redundant converter

* Fix ranged item item templates

* Missing texts and tooltips

* Reformat code

* Fix several problems and bugs + New Russian tips

* Changes to help tips

* Reverted line separator change

* Fixed tests

* Set bow as default animations for converted ranged items

* Partially fix 1.19.4

* Fix a change made by the merge

* Update converters

* Fix the Java models problem

* Change Forge's deprecated method to Vanilla method

* Update converters

* Help tips translations

* Help tips translations

* Not one of them actually

* Update/fre-add converters

* Handle ranged item specific model when normal model is used + Fix some problems

* Ranged item model can now be selected at any time as a builtin model

* Add 1.20.1 mappings and some fixes

1.20.1 contains a major problem when a item has a condition to shoot

* Change all items.AIR to their respective item + fix headers

src/main/java/net/mcreator/element/converter/v2023_4/ProcedureArrowProjectileFixer.java
@@ -1,0 +1,69 @@
+/*
+ * MCreator (https://mcreator.net/)
+ * Copyright (C) 2012-2020, Pylo
+ * Copyright (C) 2020-2023, Pylo, opensource contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.mcreator.element.converter.v2023_4;
+
+import net.mcreator.element.converter.ProcedureConverter;
+import net.mcreator.element.types.Procedure;
+import net.mcreator.util.XMLUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class ProcedureArrowProjectileFixer extends ProcedureConverter {
+
+	@Override protected String fixXML(Procedure procedure, String xml) throws Exception {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(new InputSource(new StringReader(xml)));
+		doc.getDocumentElement().normalize();
+
+		NodeList nodeList = doc.getElementsByTagName("block");
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Element element = (Element) nodeList.item(i);
+			String type = element.getAttribute("type");
+			if (type.equals("projectiles_arrow")) {
+				Element field = XMLUtil.getFirstChildrenWithName(element, "field");
+				if (field != null)
+					field.setTextContent(field.getTextContent() + "Projectile");
+			}
+		}
+
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+
+		return writer.getBuffer().toString();
+	}
+
+	@Override public int getVersionConvertingTo() {
+		return 52;
+	}
+}

commit a06fbb6df1bdd631d060a2f38baaec91c246cf3d

Fix procedure arrow projectile converter (bug introduced in #3456)

src/main/java/net/mcreator/element/converter/v2023_4/ProcedureArrowProjectileFixer.java
@@ -50,8 +50,13 @@ public class ProcedureArrowProjectileFixer extends ProcedureConverter {
 			String type = element.getAttribute("type");
 			if (type.equals("projectiles_arrow")) {
 				Element field = XMLUtil.getFirstChildrenWithName(element, "field");
-				if (field != null)
-					field.setTextContent(field.getTextContent() + "Projectile");
+				if (field != null) {
+					String textContent = field.getTextContent();
+					if (textContent.isBlank() || textContent.equals("Arrow") || textContent.equals("SpectralArrow"))
+						continue;
+
+					field.setTextContent(textContent + "Projectile");
+				}
 			}
 		}
 