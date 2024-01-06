4c6f8b87fae608df9f37545d4802bc1feadbb1b3
AlsoSomeoneElse <2000someoneelse@gmail.com>
2024-01-06 13:21:57 +0100

    Remove "Deal custom damage" procedure block

--- src/main/java/net/mcreator/element/converter/ConverterRegistry.java
+++ src/main/java/net/mcreator/element/converter/ConverterRegistry.java
@@ -47,6 +47,7 @@ import net.mcreator.element.converter.v2023_2.ExplodeProcedureConverter;
 import net.mcreator.element.converter.v2023_2.PaintingFieldsFixer;
 import net.mcreator.element.converter.v2023_3.*;
 import net.mcreator.element.converter.v2023_4.*;
+import net.mcreator.element.converter.v2024_1.ProcedureCustomDamageRemover;
 
 import java.util.HashMap;
 import java.util.List;
@@ -153,7 +154,8 @@ public class ConverterRegistry {
 			new MaterialProcedureConverter(),
 			new ProcedureDamageSourceFixer(),
 			new LegacyGameModeConverter(),
-			new ProcedureArrowProjectileFixer()
+			new ProcedureArrowProjectileFixer(),
+			new ProcedureCustomDamageRemover()
 		));
 		put(ModElementType.RECIPE, List.of(
 			new RecipeTypeConverter()
