d496aae0cc119b4f6f2b4dcf10304d87d4e31abe
InfoyTech <90406016+PSGitHubUser1@users.noreply.github.com>
2024-03-12 13:14:04 +0530

    Update mcreator.bat

--- platform/windows/mcreator.bat
+++ platform/windows/mcreator.bat
@@ -1,20 +1,21 @@
 @echo off
 color a
+echo.
 title MCreator Launcher
 ::
 :: ---------------------------------------------------------------------
 :: MCreator Launcher
 :: ---------------------------------------------------------------------
 echo.
+echo.
 echo   -------------------------------------
 echo            MCreator Launcher
 echo    ------------------------------------
-
-::
+echo.
 echo.
 :: set the classpath
 set CLASSPATH=./lib/mcreator.jar;./lib/*
-
+echo.
 echo.
 :: launch MCreator with bundled java
 "./jdk/bin/java.exe" --add-opens=java.base/java.lang=ALL-UNNAMED net.mcreator.Launcher
