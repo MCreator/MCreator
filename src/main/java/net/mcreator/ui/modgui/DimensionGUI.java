MCreator/MCreator

commit f98203a96126bfd3408e00738ee1b42c815dc990

Added Portal Igniter rarity (#4408)
* Rarity for Portal Igniter

* Fix

* Fix

* Name changed to igniterRarity (from rarity)

* Fix

* Fix

* Updated to 1.20.4

* Deleted generator-1.19.4 gen

src/main/java/net/mcreator/ui/modgui/DimensionGUI.java
@@ -63,6 +63,7 @@ import java.net.URISyntaxException;
 public class DimensionGUI extends ModElementGUI<Dimension> {
 
 	private final VTextField igniterName = new VTextField(14);
+	private final JComboBox<String> igniterRarity = new JComboBox<>(new String[] { "COMMON", "UNCOMMON", "RARE", "EPIC" });
 
 	private StringListProcedureSelector specialInformation;
 
@@ -231,7 +232,7 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		JPanel proper = new JPanel(new GridLayout(4, 2, 5, 2));
 		proper.setOpaque(false);
 
-		JPanel proper22 = new JPanel(new GridLayout(2, 2, 5, 2));
+		JPanel proper22 = new JPanel(new GridLayout(3, 2, 5, 2));
 		proper22.setOpaque(false);
 
 		proper.add(HelpUtils.wrapWithHelpButton(this.withEntry("dimension/portal_particles"),
@@ -253,6 +254,9 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		proper22.add(HelpUtils.wrapWithHelpButton(this.withEntry("common/gui_name"),
 				L10N.label("elementgui.dimension.portal_igniter_name")));
 		proper22.add(igniterName);
+
+		proper22.add(HelpUtils.wrapWithHelpButton(this.withEntry("item/rarity"), L10N.label("elementgui.common.rarity")));
+		proper22.add(igniterRarity);
 
 		proper22.add(HelpUtils.wrapWithHelpButton(this.withEntry("common/creative_tab"),
 				L10N.label("elementgui.dimension.portal_igniter_tab")));
@@ -411,6 +415,7 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		portalSound.setSound(dimension.portalSound);
 		enableIgniter.setSelected(dimension.enableIgniter);
 		igniterName.setText(dimension.igniterName);
+		igniterRarity.setSelectedItem(dimension.igniterRarity);
 		specialInformation.setSelectedProcedure(dimension.specialInformation);
 		portalTexture.setTextureFromTextureName(dimension.portalTexture);
 		texture.setTextureFromTextureName(dimension.texture);
@@ -456,6 +461,7 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		dimension.portalFrame = portalFrame.getBlock();
 		dimension.enableIgniter = enableIgniter.isSelected();
 		dimension.igniterName = igniterName.getText();
+		dimension.igniterRarity = (String) igniterRarity.getSelectedItem();
 		dimension.specialInformation = specialInformation.getSelectedProcedure();
 		dimension.worldGenType = (String) worldGenType.getSelectedItem();
 		dimension.sleepResult = (String) sleepResult.getSelectedItem();

plugins/generator-1.20.1/forge-1.20.1/templates/dimension/portaltrigger.java.ftl
@@ -37,7 +37,7 @@ package ${package}.item;
 public class ${name}Item extends Item {
 
 	public ${name}Item() {
-		super(new Item.Properties().durability(64));
+		super(new Item.Properties().rarity(Rarity.${data.igniterRarity}).durability(64));
 	}
 
 	<@addSpecialInformation data.specialInformation/>

plugins/generator-1.20.4/neoforge-1.20.4/templates/dimension/portaltrigger.java.ftl
@@ -37,7 +37,7 @@ package ${package}.item;
 public class ${name}Item extends Item {
 
 	public ${name}Item() {
-		super(new Item.Properties().durability(64));
+		super(new Item.Properties().rarity(Rarity.${data.igniterRarity}).durability(64));
 	}
 
 	<@addSpecialInformation data.specialInformation/>

src/main/java/net/mcreator/element/types/Dimension.java
@@ -71,6 +71,7 @@ import java.util.*;
 	public Sound portalSound;
 	public boolean enableIgniter;
 	public String igniterName;
+	public String igniterRarity;
 	public StringListProcedure specialInformation;
 	public TabEntry igniterTab;
 	@TextureReference(TextureType.ITEM) public String texture;
@@ -91,6 +92,7 @@ import java.util.*;
 		// DEFAULT VALUES
 		this.enablePortal = true;
 		this.enableIgniter = true;
+		this.igniterRarity = "COMMON";
 		this.sleepResult = "ALLOW";
 	}
 

src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -681,6 +681,7 @@ public class TestWorkspaceDataProvider {
 			dimension.portalFrame = new MItemBlock(modElement.getWorkspace(),
 					getRandomMCItem(random, blocks).getName());
 			dimension.igniterName = modElement.getName();
+			dimension.igniterRarity = getRandomString(random, Arrays.asList("COMMON", "UNCOMMON", "RARE", "EPIC"));
 			dimension.specialInformation = new StringListProcedure(emptyLists ? null : "string1",
 					Arrays.asList("info 1", "info 2", "test, is this", "another one"));
 			dimension.worldGenType = new String[] { "Nether like gen", "Normal world gen", "End like gen",

commit 05157d12df5d1a4ff056210fc47b3152cd5ad445

Fix a bug introduced in #4408

src/main/java/net/mcreator/ui/modgui/DimensionGUI.java
@@ -380,6 +380,7 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		specialInformation.setEnabled(enabled);
 		texture.setEnabled(enabled);
 		portalMakeCondition.setEnabled(enabled);
+		igniterRarity.setEnabled(enabled);
 	}
 
 	@Override public void reloadDataLists() {