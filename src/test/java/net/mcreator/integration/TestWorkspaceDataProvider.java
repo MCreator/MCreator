c51798011b87b9596f60a15fd221ccb8ca3cddc4
PooPmeep <112079900+NerdyPuzzle@users.noreply.github.com>
2024-03-22 18:39:55 +0200

    Fix tests error

--- src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
+++ src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -998,7 +998,7 @@ public class TestWorkspaceDataProvider {
 			item.projectile = new ProjectileEntry(modElement.getWorkspace(),
 					getRandomDataListEntry(random, ElementUtil.loadArrowProjectiles(modElement.getWorkspace())));
 			item.shootConstantly = emptyLists;
-			item.rangedItemChargesPower = emptyLists;
+			item.rangedItemChargesPower = !item.shootConstantly;
 			item.onRangedItemUsed = new Procedure("procedure4");
 			item.rangedUseCondition = new Procedure("condition1");
 			return item;
