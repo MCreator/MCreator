73d62c3beea7cce9d7aa8312bb2c3e2a1534846d
Defeatomizer <drag7398@gmail.com>
2024-05-18 22:02:26 +0300

    Dynamic dependencies mutator for call procedure block (#3503)
    * Dynamic dependencies mutator for call procedure block
    
    * Minor changes
    
    * Minor fixes
    
    * Resolved notes
    
    * Updated converters
    
    * Mutator mixin now remembers dependency names
    
    * Replaced hardcoded mixin with helper function return value
    Will not build due to merge conflicts with upstream branch
    
    * Fixed tests?
    
    * Fixed block itself
    
    * Replaced manual mutator addition with extension applying
    
    * Removed Minecraft 1.18.x support
    
    * Fixes
    
    * Fixes
    
    * Reverted command element type changes
    
    * More fixes
    
    * FV bump
    
    * Validation functions from #3660
    
    * Repeating values fixes
    
    * Dependency type validation
    
    * Possible lag reason
    
    * Obsolete changes
    
    * Event fixes
    
    * 3 loops instead of 4
    
    * Event fixes
    
    * Better
    
    * Dependencies were removed at a wrong moment
    
    * More event fixes
    
    * Warn if extra dependencies are provided
    
    * Required dependencies tooltip
    
    * "Separate" helper function
    
    * A tweak to use less fields
    
    * Function to constant, slight rewording
    
    * Simpler condition, parentheses optimization
    
    * Comments
    
    * Direct check
    
    * Mixin move
    
    * Better checks
    
    * Validate only if procedure is selected
    
    * Ignore unused dependencies
    
    * Converter bump
    
    * Too far changes
    
    * Pass types to code templates
    
    * Fix procedure converter
    
    * Changes to code
    
    * Changed warning message for ignored dependencies
    
    * Directly pass the list of DependencyInputs
    
    * 1.19.2 -> 1.20.1
    
    * Remember groups
    
    * Bump
    
    * Revert what belongs to another PR
    
    * This too
    
    * Gone too far
    
    * Bump
    
    * Turns out we can do without this
    
    * Less events, more fields
    
    * Bump
    
    * Convert converter
    
    * Bump
    
    * Bump
    
    * Common party for the new functions
    
    * Dependency names and types could be mixed when rebuilding (finally)
    
    * JS strings, more convenient validator
    
    * Bump
    
    * Revert what belongs to another PR
    
    * Bump
    
    * Bump
    
    * Fix procedures with empty name being logged
    
    * Bump
    
    * Bump
    
    * Well
    
    * Here
    
    * Bump
    
    * Remember dummy names
    
    * Avoid extra method call and argument
    
    * 1.20.4 support
    
    * Revert 1.19.4
    
    * Bump
    
    * Notes
    
    * An attempt to reduce duplicated code
    
    * No need for visual validation
    
    * Optimization
    
    * More
    
    * Notes
    
    * Bump
    
    * Common method
    
    * Supress unused (so we know it is intentionally "unused")
    
    * Minor change
    
    * Documentation
    
    * Confusing JavaDOC
    
    * Minor comment change
    
    * Weak equality check
    
    * Minor JS improvements
    
    * More minor JS improvements
    
    * Minor code improvement
    
    * More minor improvements
    
    * Oops, too much changes
    
    * Notes
    
    * Simpler updateShape_, remove validOnLoad
    
    * Initial firstFreeIndex code
    
    * Remove firstFreeIndex
    
    * Comments
    
    * PR author really does not like comments (easy-to-understand code) + some spacing to make code more readable
    
    * Minor changes
    
    * Bump
    
    * Readability
    
    ---------
    
    Co-authored-by: AlsoSomeoneElse <64546452+AlsoSomeoneElse@users.noreply.github.com>
    Co-authored-by: AlsoSomeoneElse <2000someoneelse@gmail.com>
    Co-authored-by: KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>

--- plugins/generator-1.20.1/forge-1.20.1/procedures/_call_procedure.java.ftl
+++ plugins/generator-1.20.1/forge-1.20.1/procedures/_call_procedure.java.ftl
@@ -1,2 +1,13 @@
+<#include "mcitems.ftl">
 <#include "procedures.java.ftl">
-<@procedureToCode name=procedure dependencies=dependencies />
+<#assign customVals = {}>
+<#list depInputs as depInput>
+	<#if depInput.type() == "blockstate">
+		<#assign customVals += {depInput.name(): mappedBlockToBlockStateCode(depInput.value())}>
+	<#elseif depInput.type() == "itemstack">
+		<#assign customVals += {depInput.name(): mappedMCItemToItemStackCode(depInput.value())}>
+	<#else>
+		<#assign customVals += {depInput.name(): depInput.value()}>
+	</#if>
+</#list>
+<@procedureToCode name=procedure dependencies=dependencies customVals=customVals />
--- src/main/java/net/mcreator/blockly/java/blocks/ProcedureRetvalBlock.java
+++ src/main/java/net/mcreator/blockly/java/blocks/ProcedureRetvalBlock.java
@@ -21,6 +21,7 @@ package net.mcreator.blockly.java.blocks;
 import net.mcreator.blockly.BlocklyCompileNote;
 import net.mcreator.blockly.BlocklyToCode;
 import net.mcreator.blockly.IBlockGenerator;
+import net.mcreator.blockly.data.Dependency;
 import net.mcreator.element.parts.procedure.Procedure;
 import net.mcreator.generator.template.TemplateGeneratorException;
 import net.mcreator.ui.init.L10N;
@@ -30,7 +31,9 @@ import net.mcreator.workspace.elements.VariableTypeLoader;
 import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Element;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ProcedureRetvalBlock implements IBlockGenerator {
@@ -43,17 +46,29 @@ public class ProcedureRetvalBlock implements IBlockGenerator {
 
 	@Override public void generateBlock(BlocklyToCode master, Element block) throws TemplateGeneratorException {
 		String type = StringUtils.removeStart(block.getAttribute("type"), "procedure_retval_");
+		Element procedureField = XMLUtil.getFirstChildrenWithName(block, "field");
 
-		Element element = XMLUtil.getFirstChildrenWithName(block, "field");
+		if (procedureField != null && procedureField.getTextContent() != null && !procedureField.getTextContent()
+				.isEmpty()) {
+			Procedure procedure = new Procedure(procedureField.getTextContent());
+			List<Dependency> dependencies = procedure.getDependencies(master.getWorkspace());
 
-		if (element != null) {
-			Procedure procedure = new Procedure(element.getTextContent());
-			procedure.getDependencies(master.getWorkspace()).forEach(master::addDependency);
-
+			// If the procedure doesn't actually exist, add a compile error
 			if (!procedure.exists) {
 				master.addCompileNote(new BlocklyCompileNote(BlocklyCompileNote.Type.ERROR,
 						L10N.t("blockly.errors.procedure_retval.nonexistent", procedure.getName())));
 				return;
+			}
+
+			List<String> skippedDepsNames = new ArrayList<>();
+			List<ProcedureCallBlock.DependencyInput> depInputs = ProcedureCallBlock.mapDependencies(master, block,
+					dependencies, skippedDepsNames);
+
+			// Add a warning for the passed dependencies that aren't used by the selected procedure
+			if (!skippedDepsNames.isEmpty()) {
+				master.addCompileNote(new BlocklyCompileNote(BlocklyCompileNote.Type.WARNING,
+						L10N.t("blockly.warnings.call_procedure.extra_deps", procedure.getName(),
+								String.join(", ", skippedDepsNames))));
 			}
 
 			if (master.getTemplateGenerator() != null) {
@@ -61,12 +76,14 @@ public class ProcedureRetvalBlock implements IBlockGenerator {
 				dataModel.put("procedure", procedure.getName());
 				dataModel.put("type", type);
 				dataModel.put("dependencies", procedure.getDependencies(master.getWorkspace()));
+				dataModel.put("depInputs", depInputs.toArray(ProcedureCallBlock.DependencyInput[]::new));
+
 				String code = master.getTemplateGenerator()
 						.generateFromTemplate("_procedure_retval.java.ftl", dataModel);
 				master.append(code);
 			}
-
 		} else {
+			// No procedure is selected, add a compile error
 			master.addCompileNote(new BlocklyCompileNote(BlocklyCompileNote.Type.ERROR,
 					L10N.t("blockly.errors.procedure_retval.empty")));
 		}
@@ -79,4 +96,5 @@ public class ProcedureRetvalBlock implements IBlockGenerator {
 	@Override public BlockType getBlockType() {
 		return BlockType.OUTPUT;
 	}
+
 }
