4b75c306a733cded1205f1371e59127f2c0af94e
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2024-06-16 15:45:07 +0200

    2024.2 splash

--- src/main/java/net/mcreator/ui/workspace/selector/WorkspaceSelector.java
+++ src/main/java/net/mcreator/ui/workspace/selector/WorkspaceSelector.java
@@ -548,7 +548,7 @@ public final class WorkspaceSelector extends JFrame implements DropTargetListene
 		if (!Launcher.version.isSnapshot()) {
 			soim = new ImagePanel(SplashScreen.getSplashImage(true));
 			((ImagePanel) soim).setFitToWidth(true);
-			((ImagePanel) soim).setOffsetY(-240);
+			((ImagePanel) soim).setOffsetY(-200);
 		} else {
 			soim = new JPanel();
 			soim.setBackground(Theme.current().getSecondAltBackgroundColor());
