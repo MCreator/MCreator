b3b10583b8026e0c961edd5d58051f45646b7896
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2024-06-18 17:51:11 +0200

    Avoid the use of word please as it is redundant in UX language

--- src/main/java/net/mcreator/Launcher.java
+++ src/main/java/net/mcreator/Launcher.java
@@ -95,7 +95,7 @@ public class Launcher {
 
 		if (!UserFolderManager.createUserFolderIfNotExists()) {
 			JOptionPane.showMessageDialog(null, "<html><b>MCreator failed to write to user directory!</b><br><br>"
-							+ "Please make sure that the user running MCreator has permissions to read and write to the directory<br>"
+							+ "Make sure that the user running MCreator has permissions to read and write to the directory<br>"
 							+ "in which MCreator tried to create user specific data storage. The path MCreator could not write to is:<br><br>"
 							+ UserFolderManager.getFileFromUserFolder("/") + "<br>", "MCreator file system error",
 					JOptionPane.WARNING_MESSAGE);
