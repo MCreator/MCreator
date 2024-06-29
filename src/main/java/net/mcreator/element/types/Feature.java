ffe46b621bf309ac38fccab2e787164a297ed4b6
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2024-06-29 14:43:40 +0200

    Fix #4911 - [Bugfix] When deleting a mod element, its files were not correctly removed in some cases

--- src/main/java/net/mcreator/element/types/Feature.java
+++ src/main/java/net/mcreator/element/types/Feature.java
@@ -78,7 +78,7 @@ import java.util.List;
 			additionalData.put("featuretype", blocklyToFeature.getFeatureType());
 			additionalData.put("featureblocks", blocklyToFeature.getUsedBlocks());
 
-			this.getModElement().clearMetadata().putMetadata("has_nbt_structure",
+			this.getModElement().putMetadata("has_nbt_structure",
 					blocklyToFeature.getUsedBlocks().contains("feature_custom_structure") ? true : null);
 		};
 	}
