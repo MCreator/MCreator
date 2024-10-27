d0a5b0f929d7b1bdf5c4c4e0c0930b97c22a6b75
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2024-10-27 17:20:54 +0100

    Change splash offset a bit on WorkspaceSelector

--- src/main/java/net/mcreator/ui/workspace/selector/WorkspaceSelector.java
+++ src/main/java/net/mcreator/ui/workspace/selector/WorkspaceSelector.java
@@ -631,7 +631,7 @@ public final class WorkspaceSelector extends JFrame implements DropTargetListene
 		if (!Launcher.version.isSnapshot()) {
 			soim = new ImagePanel(SplashScreen.getSplashImage(true));
 			((ImagePanel) soim).setFitToWidth(true);
-			((ImagePanel) soim).setOffsetY(-110);
+			((ImagePanel) soim).setOffsetY(-70);
 		} else {
 			soim = new JPanel();
 			soim.setBackground(Theme.current().getSecondAltBackgroundColor());
