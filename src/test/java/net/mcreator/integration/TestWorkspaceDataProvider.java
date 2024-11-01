4d352b6ba46fd39a45270d625ab8d234df907ce1
AlsoSomeoneElse <2000someoneelse@gmail.com>
2024-11-01 12:41:26 +0100

    Add separate parameter for fog color

--- src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
+++ src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -398,6 +398,7 @@ public class TestWorkspaceDataProvider {
 							"Mega spruce trees" });
 			biome.airColor = Color.red;
 			if (!emptyLists) {
+				biome.fogColor = Color.yellow;
 				biome.grassColor = Color.green;
 				biome.foliageColor = Color.magenta;
 				biome.waterColor = Color.blue;
