3ac96130ca49e4cbfd9083b933f7df0d9ee536e7
Defeatomizer <drag7398@gmail.com>
2024-11-04 21:41:19 +0300

    Minor texts fixes (#5162)
    * Minor texts fixes
    
    * Just to match other keys
    
    * Well

--- src/main/java/net/mcreator/ui/workspace/resources/WorkspacePanelAnimations.java
+++ src/main/java/net/mcreator/ui/workspace/resources/WorkspacePanelAnimations.java
@@ -63,7 +63,7 @@ public class WorkspacePanelAnimations extends AbstractResourcePanel<Animation> {
 
 				workspacePanel.getMCreator().setCursor(Cursor.getDefaultCursor());
 				SearchUsagesDialog.showUsagesDialog(workspacePanel.getMCreator(),
-						L10N.t("dialog.search_usages.type.resource.model"), refs);
+						L10N.t("dialog.search_usages.type.resource.animation"), refs);
 			}
 		});
 		addToolBarButton("common.delete_selected", UIRES.get("16px.delete"), e -> deleteCurrentlySelected());
