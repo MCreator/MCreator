f4d95751d9ac42c89f2ea195db0bc70fd4bb8521
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2024-11-16 12:45:06 +0100

    Fix #5177 - [Bugfix] Java plugin events could be triggered after the action they were listening to happened in some cases

--- src/main/java/net/mcreator/plugin/MCREvent.java
+++ src/main/java/net/mcreator/plugin/MCREvent.java
@@ -19,11 +19,51 @@
 
 package net.mcreator.plugin;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 public abstract class MCREvent {
 
+	private static final Logger LOG = LogManager.getLogger(MCREvent.class);
+
 	public static <T extends MCREvent> void event(T event) {
-		PluginLoader.INSTANCE.getJavaPlugins().forEach(javaPlugin -> javaPlugin.getListeners().get(event.getClass())
-				.forEach(listener -> javaPlugin.getEventQueue().submit(() -> listener.eventTriggered(event))));
+		PluginLoader.INSTANCE.getJavaPlugins()
+				.forEach(javaPlugin -> javaPlugin.getListeners().get(event.getClass()).forEach(listener -> {
+					Future<?> result = javaPlugin.getEventQueue().submit(() -> listener.eventTriggered(event));
+					if (event.isSynchronous()) { // Wait for synchronous events to finish
+						try {
+							result.get(event.getTimeout(), TimeUnit.MILLISECONDS);
+						} catch (Exception e) {
+							LOG.warn("Failed to wait for synchronous event", e);
+						}
+					}
+				}));
+	}
+
+	/**
+	 * Returns if the event is synchronous. MCreator waits for all synchronous events to finish before continuing with execution.
+	 * If the event is meant to change value that is needed after the MCREvent.event() call, it should be synchronous.
+	 *
+	 * @return if the event is synchronous
+	 */
+	protected boolean isSynchronous() {
+		return true;
+	}
+
+	/**
+	 * Returns the timeout for the event. Ignored if the event is not synchronous.
+	 * If the event is synchronous and the timeout is set to a value greater than 0, MCreator will wait for the event to finish for the given amount of time.
+	 * If the event does not finish in the given time, MCreator will continue with execution and the event execution will be cancelled. A warning will be logged.
+	 * <p>
+	 * This is used to prevent a plugin from blocking MCreator execution indefinitely.
+	 *
+	 * @return the timeout for the event in milliseconds
+	 */
+	protected int getTimeout() {
+		return 10000;
 	}
 
 }
