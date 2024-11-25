9ed1ad88a0041624a4c47e0855002b022e1e1466
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2024-11-25 20:05:45 +0100

    Pack loader system

---
+++ plugins/generator-1.21.1/datapack-1.21.1/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackloaderMod.java
@@ -0,0 +1,36 @@
+package net.mcreator.packloader;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.FolderRepositorySource;
+import net.minecraft.server.packs.repository.PackSource;
+import net.minecraft.world.level.validation.DirectoryValidator;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@Mod(PackloaderMod.MODID) public class PackloaderMod {
+
+	public static final String MODID = "packloader";
+
+	public PackloaderMod(IEventBus modEventBus) {
+		modEventBus.addListener(this::addPacks);
+	}
+
+	public void addPacks(AddPackFindersEvent event) {
+		event.addRepositorySource(new FolderRepositorySource(FMLPaths.getOrCreateGameRelativePath(Path.of("datapacks")),
+				PackType.SERVER_DATA, PackSource.DEFAULT, new DirectoryValidator(path -> false)));
+	}
+
+}
