bc92a4dccaf0e6e8497893d2a477eee3cc0eb8d5
AlsoSomeoneElse <2000someoneelse@gmail.com>
2025-02-18 15:00:58 +0100

    Add banner pattern ME (initial)

--- src/main/java/net/mcreator/element/ModElementType.java
+++ src/main/java/net/mcreator/element/ModElementType.java
@@ -136,6 +136,7 @@ public class ModElementType<GE extends GeneratableElement> {
 	public static ModElementType<?> ADVANCEMENT;
 	public static ModElementType<?> ARMOR;
 	public static ModElementType<?> ATTRIBUTE;
+	public static ModElementType<?> BANNERPATTERN;
 	public static ModElementType<?> BIOME;
 	public static ModElementType<?> BLOCK;
 	public static ModElementType<?> COMMAND;
