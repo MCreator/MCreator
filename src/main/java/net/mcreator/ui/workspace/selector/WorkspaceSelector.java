ec621989f952cdacc257b1598c81c7a1eebf5afc
cdc12345 <3154934427@qq.com>
2025-07-09 10:05:43 +0800

    fix some style

--- src/main/java/net/mcreator/ui/workspace/selector/WorkspaceSelector.java
+++ src/main/java/net/mcreator/ui/workspace/selector/WorkspaceSelector.java
@@ -95,7 +95,7 @@ public final class WorkspaceSelector extends JFrame implements DropTargetListene
 
 	private final DefaultListModel<RecentWorkspaceEntry> defaultListModel = new DefaultListModel<>();
 	private final JList<RecentWorkspaceEntry> recentsList = new JList<>(defaultListModel);
-	private JPopupMenu recentListMenu;
+	private final JPopupMenu recentListMenu;
 
 	public WorkspaceSelector(@Nullable MCreatorApplication application, WorkspaceOpenListener workspaceOpenListener) {
 		this.workspaceOpenListener = workspaceOpenListener;
@@ -242,7 +242,7 @@ public final class WorkspaceSelector extends JFrame implements DropTargetListene
 		JLabel norecents = L10N.label("dialog.workspace_selector.no_workspaces");
 		norecents.setForeground(Theme.current().getAltForegroundColor());
 
-		recentsList.setComponentPopupMenu(buildRightClickMenu());
+		recentsList.setComponentPopupMenu(recentListMenu = buildRightClickMenu());
 
 		recentsList.setBackground(Theme.current().getSecondAltBackgroundColor());
 		recentsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -331,7 +331,7 @@ public final class WorkspaceSelector extends JFrame implements DropTargetListene
 	}
 
 	private JPopupMenu buildRightClickMenu() {
-		recentListMenu = new JPopupMenu();
+		JPopupMenu recentListMenu = new JPopupMenu();
 
 		JMenuItem openSelectedWorkspace = new JMenuItem(L10N.t("dialog.workspace_selector.open_workspace_selected"));
 		openSelectedWorkspace.addActionListener(a -> {
