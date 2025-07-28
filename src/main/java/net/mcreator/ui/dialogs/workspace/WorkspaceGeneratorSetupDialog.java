fed87f89d7dc8aef8c0db94c09673e3f3ca375b2
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2025-07-28 10:47:17 +0200

    Fix #5660 - [Bugfix] An exported mod file was generated even if the workspace build failed

--- src/main/java/net/mcreator/ui/dialogs/workspace/WorkspaceGeneratorSetupDialog.java
+++ src/main/java/net/mcreator/ui/dialogs/workspace/WorkspaceGeneratorSetupDialog.java
@@ -20,7 +20,7 @@ package net.mcreator.ui.dialogs.workspace;
 
 import net.mcreator.generator.setup.WorkspaceGeneratorSetup;
 import net.mcreator.gradle.GradleDaemonUtils;
-import net.mcreator.gradle.GradleErrorCodes;
+import net.mcreator.gradle.GradleResultCode;
 import net.mcreator.ui.MCreator;
 import net.mcreator.ui.MCreatorApplication;
 import net.mcreator.ui.dialogs.ProgressDialog;
@@ -75,7 +75,7 @@ public class WorkspaceGeneratorSetupDialog {
 
 			m.getGradleConsole().exec(GradleConsole.GRADLE_SYNC_TASK, taskResult -> {
 				m.getGradleConsole().setGradleSetupTaskRunningFlag(false);
-				if (taskResult.statusByMCreator() == GradleErrorCodes.STATUS_OK) {
+				if (taskResult == GradleResultCode.STATUS_OK) {
 					p2.markStateOk();
 
 					finalizeTheSetup(m, dial);
