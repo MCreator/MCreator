e25d88ddcacc16646a03034106a5867cb49c44c0
AatreyuShau <146244925+AatreyuShau@users.noreply.github.com>
2025-09-24 13:23:31 +0530

    Updated PlantGUI.java

--- src/main/java/net/mcreator/ui/modgui/PlantGUI.java
+++ src/main/java/net/mcreator/ui/modgui/PlantGUI.java
@@ -253,7 +253,7 @@ public class PlantGUI extends ModElementGUI<Plant> {
 				Dependency.fromString("x:number/y:number/z:number/world:world/entity:entity/blockstate:blockstate"));
 		onEntityFallsOn = new ProcedureSelector(this.withEntry("common/when_entity_falls_on"), mcreator,
 				L10N.t("elementgui.common.event_on_entity_falls_on"),
-				Dependency.fromString("x:number/y:number/z:number/world:world/entity:entity/blockstate:blockstate"));
+				Dependency.fromString("x:number/y:number/z:number/world:world/entity:entity/blockstate:blockstate/distance:double"));
 		onHitByProjectile = new ProcedureSelector(this.withEntry("block/on_hit_by_projectile"), mcreator,
 				L10N.t("elementgui.common.event_on_block_hit_by_projectile"), Dependency.fromString(
 				"x:number/y:number/z:number/world:world/entity:entity/direction:direction/blockstate:blockstate/hitX:number/hitY:number/hitZ:number"));
@@ -1206,3 +1206,4 @@ public class PlantGUI extends ModElementGUI<Plant> {
 	}
 
 }
+
