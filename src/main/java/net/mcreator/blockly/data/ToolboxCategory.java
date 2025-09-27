3ceb02cd8bd0ec691467ca75107e4fe20424fb13
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2025-09-27 18:35:18 +0200

    Improved Blockly search to show more categories correctly and also to abbreviate long blocks

--- src/main/java/net/mcreator/blockly/data/ToolboxCategory.java
+++ src/main/java/net/mcreator/blockly/data/ToolboxCategory.java
@@ -20,11 +20,14 @@ package net.mcreator.blockly.data;
 
 import net.mcreator.blockly.BlocklyBlockUtil;
 import net.mcreator.ui.init.L10N;
+import org.apache.commons.text.WordUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nullable;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ToolboxCategory {
 	private static final Logger LOG = LogManager.getLogger("Toolbox category");
@@ -75,4 +78,67 @@ public class ToolboxCategory {
 			return Color.BLACK;
 		}
 	}
+
+	private static ToolboxCategory builtin(String id, String color) {
+		ToolboxCategory retval = new ToolboxCategory();
+		retval.id = id;
+		retval.name = id;
+		retval.color = color;
+		return retval;
+	}
+
+	private static ToolboxCategory builtin(String id, String color, String name) {
+		ToolboxCategory retval = new ToolboxCategory();
+		retval.id = id;
+		retval.name = name;
+		retval.color = color;
+		return retval;
+	}
+
+	private static final Map<String, ToolboxCategory> BUILTIN = new HashMap<>() {{
+		// toolbox_feature.xml
+		put("features", builtin("features", "0"));
+		put("orefeatures", builtin("orefeatures", "80"));
+		put("treefeatures", builtin("treefeatures", "110"));
+		put("trees", builtin("trees", "110"));
+		put("treedecorators", builtin("treedecorators", "110"));
+		put("advancedfeatures", builtin("advancedfeatures", "340"));
+		put("blocks", builtin("blocks.feature", "60"));
+		put("intproviders", builtin("intproviders", "230"));
+		put("placements", builtin("placements", "130"));
+		put("heightplacements", builtin("heightplacements", "190"));
+		put("blockpredicates", builtin("blockpredicates", "210"));
+
+		// toolbox_ai_builder.xml
+		put("aiadvanced", builtin("advanced", "250"));
+
+		// toolbox_procedure.xml
+		put("mcelements", builtin("minecraft_components", "360"));
+		put("customvariables", builtin("custom_variables", "150"));
+		put("logicloops", builtin("logic_and_loops", "120"));
+		put("logicoperations", builtin("logic", "210"));
+		put("math", builtin("math", "230"));
+		put("text", builtin("text", "160"));
+		put("time", builtin("time", "#628c94"));
+		put("advanced", builtin("advanced", "250"));
+
+		// toolbox_command.xml
+		put("actions", builtin("actions", "250"));
+	}};
+
+	/**
+	 * Call this to get ToolboxCategory instance for builtin categories.
+	 * This is only meant for visual use, e.g. in procedure search.
+	 *
+	 * @param id the id of the builtin category
+	 * @return the ToolboxCategory instance, or a generic one with gray color if the id is unknown
+	 */
+	public static ToolboxCategory tryGetBuiltin(String id) {
+		if (BUILTIN.containsKey(id)) {
+			return BUILTIN.get(id);
+		} else {
+			return builtin(id, "#555555", WordUtils.capitalizeFully(id));
+		}
+	}
+
 }
