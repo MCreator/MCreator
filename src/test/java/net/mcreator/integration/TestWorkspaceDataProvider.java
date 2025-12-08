352c9193117dc1507b3d833e323f8cb429f99166
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2025-12-08 21:37:24 +0100

    More RAM-friendly tests

--- src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
+++ src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -40,6 +40,8 @@ import net.mcreator.element.types.Fluid;
 import net.mcreator.element.types.interfaces.IBlockWithBoundingBox;
 import net.mcreator.generator.GeneratorConfiguration;
 import net.mcreator.generator.GeneratorStats;
+import net.mcreator.generator.mapping.MappableElement;
+import net.mcreator.generator.mapping.NameMapper;
 import net.mcreator.integration.generator.GTProcedureBlocks;
 import net.mcreator.io.FileIO;
 import net.mcreator.minecraft.*;
@@ -714,7 +716,8 @@ public class TestWorkspaceDataProvider {
 			ArrayList<GUIComponent> components = new ArrayList<>();
 
 			components.add(new Label("text", 100, 150, new StringProcedure(_true ? "string1" : null, "fixed value 1"),
-					Color.red, _true, new Procedure("condition1"), getRandomItem(random, GUIComponent.AnchorPoint.values())));
+					Color.red, _true, new Procedure("condition1"),
+					getRandomItem(random, GUIComponent.AnchorPoint.values())));
 			components.add(new Label("text2", 100, 150, new StringProcedure(!_true ? "string2" : null, "fixed value 2"),
 					Color.white, !_true, new Procedure("condition4"),
 					getRandomItem(random, GUIComponent.AnchorPoint.values())));
@@ -847,9 +850,7 @@ public class TestWorkspaceDataProvider {
 			dimension.biomesInDimension = new ArrayList<>();
 			if (!emptyLists) {
 				dimension.biomesInDimension.addAll(
-						biomes.stream().skip(_true ? 0 : ((long) (biomes.size() / 4) * valueIndex))
-								.limit(biomes.size() / 4)
-								.map(e -> new BiomeEntry(modElement.getWorkspace(), e.getName())).toList());
+						subset(random, 10, biomes, e -> new BiomeEntry(modElement.getWorkspace(), e.getName())));
 			} else {
 				dimension.biomesInDimension.add(
 						new BiomeEntry(modElement.getWorkspace(), getRandomDataListEntry(random, biomes)));
@@ -918,10 +919,8 @@ public class TestWorkspaceDataProvider {
 			structure.startHeightMax = 61;
 			structure.ignoredBlocks = new ArrayList<>();
 			if (!emptyLists) {
-				structure.ignoredBlocks.addAll(
-						blocks.stream().skip(_true ? 0 : ((long) (blocks.size() / 4) * valueIndex))
-								.limit(blocks.size() / 4)
-								.map(e -> new MItemBlock(modElement.getWorkspace(), e.getName())).toList());
+				structure.ignoredBlocks = subset(random, 5, blocks,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 			}
 			structure.terrainAdaptation = getRandomString(random,
 					Arrays.asList("none", "beard_thin", "beard_box", "bury", "encapsulate"));
@@ -930,8 +929,8 @@ public class TestWorkspaceDataProvider {
 			structure.spacing = 17;
 			structure.separation = 9;
 			if (_true) {
-				structure.restrictionBiomes.addAll(
-						biomes.stream().map(e -> new BiomeEntry(modElement.getWorkspace(), e.getName())).toList());
+				structure.restrictionBiomes = subset(random, 5, biomes,
+						e -> new BiomeEntry(modElement.getWorkspace(), e.getName()));
 			} else {
 				structure.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(), "#is_overworld"));
 			}
@@ -949,17 +948,15 @@ public class TestWorkspaceDataProvider {
 				part.weight = 3;
 				part.structure = "test1";
 				part.projection = "rigid";
-				part.ignoredBlocks = blocks.stream().skip(_true ? 0 : ((long) (blocks.size() / 4) * valueIndex))
-						.limit(blocks.size() / 4).map(e -> new MItemBlock(modElement.getWorkspace(), e.getName()))
-						.toList();
+				part.ignoredBlocks = subset(random, 5, blocks,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 				pool.poolParts.add(part);
 				part = new Structure.JigsawPool.JigsawPart();
 				part.weight = 7;
 				part.structure = "test2";
 				part.projection = "terrain_matching";
-				part.ignoredBlocks = blocks.stream().skip(_true ? 0 : ((long) (blocks.size() / 4) * valueIndex))
-						.limit(blocks.size() / 4).map(e -> new MItemBlock(modElement.getWorkspace(), e.getName()))
-						.toList();
+				part.ignoredBlocks = subset(random, 5, blocks,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 				pool.poolParts.add(part);
 				structure.jigsawPools.add(pool);
 
@@ -971,9 +968,8 @@ public class TestWorkspaceDataProvider {
 				part.weight = 1;
 				part.structure = "test3";
 				part.projection = "rigid";
-				part.ignoredBlocks = blocks.stream().skip(_true ? 0 : ((long) (blocks.size() / 4) * valueIndex))
-						.limit(blocks.size() / 4).map(e -> new MItemBlock(modElement.getWorkspace(), e.getName()))
-						.toList();
+				part.ignoredBlocks = subset(random, 5, blocks,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 				pool.poolParts.add(part);
 				structure.jigsawPools.add(pool);
 			}
@@ -1070,10 +1066,8 @@ public class TestWorkspaceDataProvider {
 			armor.knockbackResistance = 3.148;
 			armor.repairItems = new ArrayList<>();
 			if (!emptyLists) {
-				armor.repairItems = new ArrayList<>(blocksAndItemsAndTags.stream()
-						.skip(_true ? 0 : ((long) (blocksAndItemsAndTags.size() / 4) * valueIndex))
-						.limit(blocksAndItemsAndTags.size() / 4)
-						.map(e -> new MItemBlock(modElement.getWorkspace(), e.getName())).toList());
+				armor.repairItems = subset(random, blocksAndItemsAndTags.size() / 8, blocksAndItemsAndTags,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 				armor.repairItems.add(new MItemBlock(modElement.getWorkspace(), "TAG:walls"));
 			}
 			return armor;
@@ -1176,9 +1170,8 @@ public class TestWorkspaceDataProvider {
 			plant.jumpFactor = 17.732;
 			plant.canBePlacedOn = new ArrayList<>();
 			if (!emptyLists) {
-				plant.canBePlacedOn.addAll(blocksAndTags.stream().skip(_true ? 0 : ((blocks.size() / 4) * valueIndex))
-						.limit(blocks.size() / 4).map(e -> new MItemBlock(modElement.getWorkspace(), e.getName()))
-						.toList());
+				plant.canBePlacedOn = subset(random, blocksAndTags.size() / 16, blocksAndTags,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 				plant.canBePlacedOn.add(new MItemBlock(modElement.getWorkspace(), "TAG:walls"));
 			}
 			plant.restrictionBiomes = new ArrayList<>();
@@ -1186,8 +1179,8 @@ public class TestWorkspaceDataProvider {
 				if (_true) {
 					plant.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(), "#is_overworld"));
 				} else {
-					plant.restrictionBiomes.addAll(
-							biomes.stream().map(e -> new BiomeEntry(modElement.getWorkspace(), e.getName())).toList());
+					plant.restrictionBiomes = subset(random, 5, biomes,
+							e -> new BiomeEntry(modElement.getWorkspace(), e.getName()));
 				}
 			}
 			plant.onNeighbourBlockChanges = new Procedure("procedure7");
@@ -1453,7 +1446,6 @@ public class TestWorkspaceDataProvider {
 					new String[] { "Generic", "Entity", "Block", "Chest", "Fishing", "Empty", "Advancement reward" });
 
 			lootTable.pools = new ArrayList<>();
-
 			if (!emptyLists) {
 				int pools = random.nextInt(5) + 1;
 				for (int i = 0; i < pools; i++) {
@@ -1524,10 +1516,9 @@ public class TestWorkspaceDataProvider {
 			enchantment.incompatibleEnchantments = new ArrayList<>();
 			if (!emptyLists) {
 				if (_true) {
-					enchantment.incompatibleEnchantments.addAll(
-							ElementUtil.loadAllEnchantments(modElement.getWorkspace()).stream()
-									.map(e -> new net.mcreator.element.parts.Enchantment(modElement.getWorkspace(),
-											e.getName())).toList());
+					enchantment.incompatibleEnchantments = subset(random, 10,
+							ElementUtil.loadAllEnchantments(modElement.getWorkspace()),
+							e -> new net.mcreator.element.parts.Enchantment(modElement.getWorkspace(), e.getName()));
 				} else {
 					enchantment.incompatibleEnchantments.add(
 							new net.mcreator.element.parts.Enchantment(modElement.getWorkspace(),
@@ -1601,7 +1592,7 @@ public class TestWorkspaceDataProvider {
 			VillagerTrade villagerTrade = new VillagerTrade(modElement);
 			villagerTrade.tradeEntries = new ArrayList<>();
 			if (!emptyLists) {
-				int tradeEntries = random.nextInt(10) + 1;
+				int tradeEntries = random.nextInt(5) + 1;
 				for (int i = 0; i < tradeEntries; i++) {
 					VillagerTrade.CustomTradeEntry trade = new VillagerTrade.CustomTradeEntry();
 					trade.villagerProfession = new ProfessionEntry(modElement.getWorkspace(),
@@ -1609,7 +1600,7 @@ public class TestWorkspaceDataProvider {
 									ElementUtil.loadAllVillagerProfessions(modElement.getWorkspace())));
 					trade.entries = new ArrayList<>();
 
-					int entries = random.nextInt(10) + 1;
+					int entries = random.nextInt(5) + 1;
 					for (int j = 0; j < entries; j++) {
 						VillagerTrade.CustomTradeEntry.Entry entry = new VillagerTrade.CustomTradeEntry.Entry();
 						entry.price1 = new MItemBlock(modElement.getWorkspace(),
@@ -1633,7 +1624,7 @@ public class TestWorkspaceDataProvider {
 							"WANDERING_TRADER");
 					wanderingTrade.entries = new ArrayList<>();
 
-					int wanderingEntries = random.nextInt(10) + 1;
+					int wanderingEntries = random.nextInt(5) + 1;
 					for (int j = 0; j < wanderingEntries; j++) {
 						VillagerTrade.CustomTradeEntry.Entry entry = new VillagerTrade.CustomTradeEntry.Entry();
 						entry.price1 = new MItemBlock(modElement.getWorkspace(),
@@ -1682,10 +1673,8 @@ public class TestWorkspaceDataProvider {
 					ElementUtil.getDataListAsStringArray("generationsteps"));
 			feature.restrictionBiomes = new ArrayList<>();
 			if (!emptyLists) {
-				feature.restrictionBiomes.addAll(
-						biomes.stream().skip(_true ? 0 : ((long) (biomes.size() / 4) * valueIndex))
-								.limit(biomes.size() / 4)
-								.map(e -> new BiomeEntry(modElement.getWorkspace(), e.getName())).toList());
+				feature.restrictionBiomes = subset(random, 5, biomes,
+						e -> new BiomeEntry(modElement.getWorkspace(), e.getName()));
 				feature.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(), "#is_overworld"));
 				feature.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(), "#minecraft:test"));
 			}
@@ -1702,9 +1691,8 @@ public class TestWorkspaceDataProvider {
 			attribute.entities = new ArrayList<>();
 			attribute.sentiment = new String[] { "POSITIVE", "NEUTRAL", "NEGATIVE", "NEUTRAL" }[valueIndex];
 			if (!emptyLists) {
-				attribute.entities.addAll(ElementUtil.loadAllSpawnableEntities(modElement.getWorkspace()).stream()
-						.map(e -> new net.mcreator.element.parts.EntityEntry(modElement.getWorkspace(), e.getName()))
-						.toList());
+				attribute.entities = subset(random, 20, ElementUtil.loadAllSpawnableEntities(modElement.getWorkspace()),
+						e -> new net.mcreator.element.parts.EntityEntry(modElement.getWorkspace(), e.getName()));
 				attribute.addToPlayers = _true;
 			} else {
 				attribute.addToAllEntities = true;
@@ -1832,16 +1820,8 @@ public class TestWorkspaceDataProvider {
 		livingEntity.tameable = _true;
 		livingEntity.breedTriggerItems = new ArrayList<>();
 		if (!emptyLists) {
-			livingEntity.breedTriggerItems.add(new MItemBlock(modElement.getWorkspace(),
-					getRandomMCItem(random, blocksAndItemsAndTags).getName()));
-			livingEntity.breedTriggerItems.add(new MItemBlock(modElement.getWorkspace(),
-					getRandomMCItem(random, blocksAndItemsAndTags).getName()));
-			livingEntity.breedTriggerItems.add(new MItemBlock(modElement.getWorkspace(),
-					getRandomMCItem(random, blocksAndItemsAndTags).getName()));
-			livingEntity.breedTriggerItems.add(new MItemBlock(modElement.getWorkspace(),
-					getRandomMCItem(random, blocksAndItemsAndTags).getName()));
-			livingEntity.breedTriggerItems.add(new MItemBlock(modElement.getWorkspace(),
-					getRandomMCItem(random, blocksAndItemsAndTags).getName()));
+			livingEntity.breedTriggerItems = subset(random, 5, blocksAndItemsAndTags,
+					e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 		}
 		livingEntity.ranged = _true;
 		livingEntity.rangedAttackItem = new MItemBlock(modElement.getWorkspace(),
@@ -1857,8 +1837,8 @@ public class TestWorkspaceDataProvider {
 		livingEntity.restrictionBiomes = new ArrayList<>();
 		if (!emptyLists) {
 			if (_true) {
-				livingEntity.restrictionBiomes.addAll(
-						biomes.stream().map(e -> new BiomeEntry(modElement.getWorkspace(), e.getName())).toList());
+				livingEntity.restrictionBiomes = subset(random, 5, biomes,
+						e -> new BiomeEntry(modElement.getWorkspace(), e.getName()));
 			} else {
 				livingEntity.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(), "#is_overworld"));
 			}
@@ -2134,18 +2114,16 @@ public class TestWorkspaceDataProvider {
 		block.restrictionBiomes = new ArrayList<>();
 		if (!emptyLists) {
 			if (_true) {
-				block.restrictionBiomes.addAll(
-						biomes.stream().map(e -> new BiomeEntry(modElement.getWorkspace(), e.getName())).toList());
+				block.restrictionBiomes = subset(random, 5, biomes,
+						e -> new BiomeEntry(modElement.getWorkspace(), e.getName()));
 			} else {
 				block.restrictionBiomes.add(new BiomeEntry(modElement.getWorkspace(), "#is_overworld"));
 			}
 		}
 		block.blocksToReplace = new ArrayList<>();
 		if (!emptyLists) {
-			block.blocksToReplace = new ArrayList<>(
-					blocksAndTags.stream().skip(_true ? 0 : ((blocksAndTags.size() / 4) * valueIndex))
-							.limit(blocksAndTags.size() / 4)
-							.map(e -> new MItemBlock(modElement.getWorkspace(), e.getName())).toList());
+			block.blocksToReplace = subset(random, blocksAndTags.size() / 8, blocksAndTags,
+					e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 			block.blocksToReplace.add(new MItemBlock(modElement.getWorkspace(), "TAG:walls"));
 		}
 		block.generationShape = _true ? "UNIFORM" : "TRIANGLE";
@@ -2310,10 +2288,8 @@ public class TestWorkspaceDataProvider {
 					getRandomMCItem(random, blocksAndItemsNoAir).getName());
 			recipe.recipeSlots = recipeSlots;
 
-			recipe.unlockingItems = new ArrayList<>(
-					blocksAndItemsAndTags.stream().skip(blocksAndItemsAndTags.size() / 4)
-							.limit(blocksAndItemsAndTags.size() / 64)
-							.map(e -> new MItemBlock(modElement.getWorkspace(), e.getName())).toList());
+			recipe.unlockingItems = subset(random, 4, blocksAndItemsAndTags,
+					e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 		}
 		case "Smelting" -> {
 			recipe.smeltingInputStack = new MItemBlock(modElement.getWorkspace(),
@@ -2369,10 +2345,8 @@ public class TestWorkspaceDataProvider {
 			recipe.smithingReturnStack = new MItemBlock(modElement.getWorkspace(),
 					getRandomMCItem(random, blocksAndItemsNoAir).getName());
 
-			recipe.unlockingItems = new ArrayList<>(
-					blocksAndItemsAndTags.stream().skip(blocksAndItemsAndTags.size() / 4)
-							.limit(blocksAndItemsAndTags.size() / 64)
-							.map(e -> new MItemBlock(modElement.getWorkspace(), e.getName())).toList());
+			recipe.unlockingItems = subset(random, 4, blocksAndItemsAndTags,
+					e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
 		}
 		case "Brewing" -> {
 			recipe.brewingInputStack = new MItemBlock(modElement.getWorkspace(), getRandomMCItem(random,
@@ -2672,6 +2646,29 @@ public class TestWorkspaceDataProvider {
 		workspace.addModElement(generatableElement.getModElement());
 		assertTrue(workspace.getGenerator().generateElement(generatableElement));
 		workspace.getModElementManager().storeModElement(generatableElement);
+	}
+
+	public static <T extends MappableElement> List<T> subset(Random random, int n,
+			Collection<? extends DataListEntry> all, java.util.function.Function<DataListEntry, T> mapper) {
+		List<DataListEntry> pool = new ArrayList<>(all);
+
+		DataListEntry chosenCustom = pool.stream().filter(e -> {
+			String v = e.getName();
+			return v != null && v.startsWith(NameMapper.MCREATOR_PREFIX);
+		}).findAny().orElse(null);
+
+		List<T> result = new ArrayList<>(n);
+
+		if (chosenCustom != null) {
+			result.add(mapper.apply(chosenCustom));
+			pool.remove(chosenCustom);
+			n--;
+		}
+
+		Collections.shuffle(pool, random);
+		result.addAll(pool.subList(0, n).stream().map(mapper).toList());
+
+		return result;
 	}
 
 }
