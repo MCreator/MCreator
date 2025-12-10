3e2e2e8400e795c3f255038033208cfe0d0f6d50
Goldorion <38427877+Goldorion@users.noreply.github.com>
2025-12-10 16:44:29 -0500

    Revert "Convert Item in inventory trigger"
    This reverts commit aa3542c30f18f845ad7b47cc0f54f0606f45a1c6.

--- src/main/java/net/mcreator/element/converter/ConverterRegistry.java
+++ src/main/java/net/mcreator/element/converter/ConverterRegistry.java
@@ -58,7 +58,6 @@ import net.mcreator.element.converter.v2025_1.FeatureCarvingMaskRemover;
 import net.mcreator.element.converter.v2025_1.ParticleLitRemover;
 import net.mcreator.element.converter.v2025_2.BlockLegacyMaterialRemover;
 import net.mcreator.element.converter.v2025_2.GuistateProceduresConverter;
-import net.mcreator.element.converter.v2025_4.ItemInInventoryTriggerConverter;
 import net.mcreator.element.converter.v2025_4.ItemOpenBoundGUIConverter;
 
 import java.util.HashMap;
@@ -74,8 +73,7 @@ public class ConverterRegistry {
 			new AchievementFixer(),
 			new AdvancementTextureConverter(),
 			new AdvancementTriggerInverter(),
-			new AdvancementFunctionConverter(),
-			new ItemInInventoryTriggerConverter()
+			new AdvancementFunctionConverter()
 		));
 		put(ModElementType.ARMOR, List.of(
 			new ArmorTexturesConverter(),
