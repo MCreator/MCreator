42363785e60960c76051fea8d83bf9183588f2d7
AlsoSomeoneElse <64546452+AlsoSomeoneElse@users.noreply.github.com>
2026-01-03 19:29:12 +0100

    Sign block base (#5739)
    * Initial block GUI changes for signs
    
    * Additional "particles only" block model template
    
    * Initial 1.21.8 implementation
    
    * Initial 1.21.1 implementation
    
    * Disable block entity settings, use item texture for preview
    
    * Fix item description in 1.21.8
    
    * Fix drop/description of wall variant
    
    * Provide wall variant MCItem
    
    * Generate icon for wall variant entry
    
    * Special handling for ".wall" suffix -> prefix
    
    * Add wall variants to shared tags (mining tool etc.)
    
    * Do not show wall variant as item
    
    * Add sign-specific tags
    
    * Help tip & tests
    
    * Properly reset max stack size
    
    * CRLF
    
    * FTL begone
    
    * Fix merge error
    
    * Sign icon in block base dropdown
    
    * Move getProvidedBlocks() to IMCItemProvider
    
    * Use TextureComboBox for sign entity texture
    
    * Fix some merge issues
    
    * This should fix the tests
    
    * Directly use texture holder
    
    * Manual merge
    
    * Filter out items
    
    * Update block tags entry providers
    
    * Failed to copy "copy"
    
    * Remove unused import
    
    * Prevent creating signs if wall sign name would conflict with mod element name
    
    * Prevent creating new mod element if name would conflict with existing signs
    
    * Revert "Prevent creating new mod element if name would conflict with existing signs"
    
    This reverts commit ad77509bf29cb95e118fc5c44cdb6675bd681890.
    
    * Revert "Prevent creating signs if wall sign name would conflict with mod element name"
    
    This reverts commit a107e297357d21a07fa23e23a1960a82790ced65.
    
    * Minor changes
    
    * Reserve wall sign element name
    
    * Formatting
    
    * Try to follow wall sign naming conventions
    
    * Move wall sign naming logic inside generatable element
    
    * Match wall methods naming with methods in ModElement
    
    * Matchine code between getWallRegistryName and getWallName
    
    ---------
    
    Co-authored-by: KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>

--- src/main/java/net/mcreator/ui/modgui/BlockGUI.java
+++ src/main/java/net/mcreator/ui/modgui/BlockGUI.java
@@ -273,7 +273,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 	private final ValidationGroup page3group = new ValidationGroup();
 
 	public static final List<String> blockBases = List.of("Stairs", "Slab", "Fence", "Wall", "Leaves", "TrapDoor",
-			"Pane", "Door", "FenceGate", "EndRod", "PressurePlate", "Button", "FlowerPot");
+			"Pane", "Door", "FenceGate", "EndRod", "PressurePlate", "Button", "FlowerPot", "Sign");
 	private final SearchableComboBox<String> blockBase = new SearchableComboBox<>(
 			ListUtils.merge(List.of("Default basic block"), blockBases));
 	private final CardLayout blockBaseCardLayout = new CardLayout();
@@ -286,6 +286,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 			//@formatter:on
 	);
 	private final MCItemHolder pottedPlant = new MCItemHolder(mcreator, ElementUtil::loadBlocksWithItemForm);
+	private final TextureComboBox signEntityTexture = new TextureComboBox(mcreator, TextureType.ENTITY);
 
 	private final JCheckBox ignitedByLava = L10N.checkbox("elementgui.common.enable");
 	private final JSpinner flammability = new JSpinner(new SpinnerNumberModel(0, 0, 1024, 1));
@@ -302,6 +303,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 	}
 
 	@Override protected void initGUI() {
+		signEntityTexture.setAddPNGExtension(false);
 		destroyTool.setRenderer(new ItemTexturesComboBoxRenderer());
 		blockBase.setRenderer(new ItemTexturesComboBoxRenderer());
 
@@ -443,15 +445,22 @@ public class BlockGUI extends ModElementGUI<Block> {
 			rotationMode.setEnabled(!hasBlockBase);
 			isWaterloggable.setEnabled(!hasBlockBase);
 			hasGravity.setEnabled(!hasBlockBase);
+			hasInventory.setEnabled(true);
 			isBonemealable.setEnabled(true);
 			transparencyType.setEnabled(true);
 			hasTransparency.setEnabled(true);
 			connectedSides.setEnabled(true);
 			blockBasePropertiesPanel.setVisible(false);
-			// Re-enable block item if user switches from flower pot to any other block base option
-			if (!isEditingMode() && !hasBlockItem.isSelected()) {
-				hasBlockItem.setSelected(true);
-				updateBlockItemSettings();
+			if (!isEditingMode()) {
+				// Re-enable block item if user switches from flower pot to any other block base option
+				if (!hasBlockItem.isSelected()) {
+					hasBlockItem.setSelected(true);
+					updateBlockItemSettings();
+				}
+				// Reset max stack size to 64 if user switches from sign to any other block base option
+				if ((int) maxStackSize.getValue() == 16) {
+					maxStackSize.setValue(64);
+				}
 			}
 
 			if (hasBlockBase) {
@@ -536,6 +545,17 @@ public class BlockGUI extends ModElementGUI<Block> {
 						reactionToPushing.setSelectedItem("DESTROY");
 					}
 				}
+				case "Sign" -> {
+					showBlockBaseCard("sign");
+					hasInventory.setEnabled(false);
+					hasInventory.setSelected(false);
+					if (!isEditingMode()) {
+						lightOpacity.setValue(0);
+						hasTransparency.setSelected(true);
+						isNotColidable.setSelected(true);
+						maxStackSize.setValue(16);
+					}
+				}
 				case null, default -> {
 					if (!isEditingMode()) {
 						lightOpacity.setValue(0);
@@ -550,6 +570,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 
 			refreshBlockStatesList();
 			updateTextureOptions();
+			refreshFieldsTileEntity();
 			refreshBonemealProperties();
 		});
 
@@ -576,6 +597,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 
 		itemTexture.setOpaque(false);
 		particleTexture.setOpaque(false);
+		signEntityTexture.setOpaque(false);
 
 		isReplaceable.setOpaque(false);
 		canProvidePower.setOpaque(false);
@@ -593,6 +615,11 @@ public class BlockGUI extends ModElementGUI<Block> {
 						HelpUtils.wrapWithHelpButton(this.withEntry("block/potted_plant"),
 								L10N.label("elementgui.block.potted_plant")), PanelUtils.centerInPanel(pottedPlant)),
 				"flowerPot");
+
+		// Card for signs
+		blockBasePropertiesPanel.add(PanelUtils.gridElements(1, 2, 2, 2,
+				HelpUtils.wrapWithHelpButton(this.withEntry("block/sign_entity_texture"),
+						L10N.label("elementgui.block.sign_entity_texture")), signEntityTexture), "sign");
 
 		JComponent blockBasePanel = PanelUtils.northAndCenterElement(PanelUtils.gridElements(1, 2, 2, 2,
 				HelpUtils.wrapWithHelpButton(this.withEntry("block/base"), L10N.label("elementgui.block.block_base")),
@@ -1406,11 +1433,16 @@ public class BlockGUI extends ModElementGUI<Block> {
 		page1group.addValidationElement(textures);
 		page1group.addValidationElement(itemTexture);
 		page1group.addValidationElement(pottedPlant);
+		page1group.addValidationElement(signEntityTexture);
 
 		itemTexture.setValidator(new TextureSelectionButtonValidator(itemTexture, () -> {
+			String selectedBlockBase = blockBase.getSelectedItem();
 			Model model = renderType.getSelectedItem();
-			return model != null && model.getType() == Model.Type.JAVA;
+			return (model != null && model.getType() == Model.Type.JAVA) || ("Sign".equals(selectedBlockBase));
 		}));
+
+		signEntityTexture.requireValue("elementgui.block.error_sign_needs_entity_texture",
+				() -> "Sign".equals(blockBase.getSelectedItem()));
 
 		pottedPlant.setValidator(new MCItemHolderValidator(pottedPlant) {
 			@Override public ValidationResult validate() {
@@ -1564,7 +1596,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 			hasInventory.setSelected(true);
 			hasInventory.setEnabled(false);
 			refreshFieldsTileEntity();
-		} else {
+		} else if (!"Sign".equals(blockBase.getSelectedItem())) {
 			hasInventory.setEnabled(true);
 		}
 
@@ -1650,6 +1682,8 @@ public class BlockGUI extends ModElementGUI<Block> {
 
 	@Override public void reloadDataLists() {
 		super.reloadDataLists();
+
+		signEntityTexture.reload();
 
 		AbstractProcedureSelector.ReloadContext context = AbstractProcedureSelector.ReloadContext.create(
 				mcreator.getWorkspace());
@@ -1792,6 +1826,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 		}
 		blockSetType.setSelectedItem(block.blockSetType);
 		pottedPlant.setBlock(block.pottedPlant);
+		signEntityTexture.setTexture(block.signEntityTexture);
 
 		plantsGrowOn.setSelected(block.plantsGrowOn);
 		hasInventory.setSelected(block.hasInventory);
@@ -2015,6 +2050,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 			block.blockBase = blockBase.getSelectedItem();
 		block.blockSetType = (String) blockSetType.getSelectedItem();
 		block.pottedPlant = pottedPlant.getBlock();
+		block.signEntityTexture = signEntityTexture.getTextureHolder();
 
 		Model model = Objects.requireNonNull(renderType.getSelectedItem());
 		block.renderType = 10;
