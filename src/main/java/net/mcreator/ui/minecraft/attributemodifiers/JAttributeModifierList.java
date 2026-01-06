dd453832e7f8a6307e721b5af8a67bdb20f80ade
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2026-01-06 19:45:04 +0100

    Manual merge

--- src/main/java/net/mcreator/ui/minecraft/attributemodifiers/JAttributeModifierList.java
+++ src/main/java/net/mcreator/ui/minecraft/attributemodifiers/JAttributeModifierList.java
@@ -45,11 +45,7 @@ public class JAttributeModifierList
 
 		add.setText(L10N.t("elementgui.common.attribute_modifier.add_modifier_entry"));
 
-		ComponentUtils.makeSection(this, L10N.t("elementgui.common.attribute_modifier.modifiers"));
-		setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2),
-				BorderFactory.createCompoundBorder(
-						BorderFactory.createLineBorder(Theme.current().getForegroundColor(), 1),
-						BorderFactory.createEmptyBorder(2, 2, 2, 2))));
+		ComponentUtils.borderWrap(this);
 	}
 
 	@Override
