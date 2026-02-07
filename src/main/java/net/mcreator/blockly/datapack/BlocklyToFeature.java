e52d785bb12b0d01a9e3e7a9dda115f587fc0349
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2026-02-07 21:10:04 +0100

    Moved feature into datapack package

--- src/main/java/net/mcreator/blockly/datapack/BlocklyToFeature.java
+++ src/main/java/net/mcreator/blockly/datapack/BlocklyToFeature.java
@@ -1,7 +1,7 @@
 /*
  * MCreator (https://mcreator.net/)
  * Copyright (C) 2012-2020, Pylo
- * Copyright (C) 2020-2022, Pylo, opensource contributors
+ * Copyright (C) 2020-2026, Pylo, opensource contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package net.mcreator.blockly.feature;
+package net.mcreator.blockly.datapack;
 
 import net.mcreator.blockly.BlocklyCompileNote;
 import net.mcreator.blockly.IBlockGenerator;
