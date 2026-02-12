8ddb4b54c9fe820e4fbda437914969a4ee84cb42
NerdyPuzzle <112079900+NerdyPuzzle@users.noreply.github.com>
2026-02-12 20:20:34 +0200

    Fixed reference format and remove redundant mod element reference

--- src/main/java/net/mcreator/element/types/ArmorTrim.java
+++ src/main/java/net/mcreator/element/types/ArmorTrim.java
@@ -4,19 +4,13 @@ import net.mcreator.element.GeneratableElement;
 import net.mcreator.element.parts.MItemBlock;
 import net.mcreator.ui.workspace.resources.TextureType;
 import net.mcreator.workspace.elements.ModElement;
-import net.mcreator.workspace.references.ModElementReference;
 import net.mcreator.workspace.references.TextureReference;
 
 @SuppressWarnings("unused") public class ArmorTrim extends GeneratableElement {
 
 	public String name;
-	@ModElementReference
 	public MItemBlock item;
-	@TextureReference(
-			value = TextureType.ARMOR,
-			files = {"%s_layer_1", "%s_layer_2"}
-	)
-	public String armorTextureFile;
+	@TextureReference(value = TextureType.ARMOR, files = { "%s_layer_1", "%s_layer_2" }) public String armorTextureFile;
 
 	private ArmorTrim() {
 		this(null);
