c4a8b3df867e1892a4db6f9aaf9577453d3b59f3
NerdyPuzzle <112079900+NerdyPuzzle@users.noreply.github.com>
2026-02-14 21:03:49 +0200

    un-hardcode the file copying

--- src/main/java/net/mcreator/element/types/ArmorTrim.java
+++ src/main/java/net/mcreator/element/types/ArmorTrim.java
@@ -2,17 +2,20 @@ package net.mcreator.element.types;
 
 import net.mcreator.element.GeneratableElement;
 import net.mcreator.element.parts.MItemBlock;
-import net.mcreator.generator.GeneratorUtils;
 import net.mcreator.io.FileIO;
 import net.mcreator.ui.workspace.resources.TextureType;
 import net.mcreator.workspace.Workspace;
 import net.mcreator.workspace.elements.ModElement;
 import net.mcreator.workspace.references.TextureReference;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.lang.module.ModuleDescriptor;
 
 @SuppressWarnings("unused") public class ArmorTrim extends GeneratableElement {
+
+	private static final Logger LOG = LogManager.getLogger(ArmorTrim.class);
 
 	public String name;
 	public MItemBlock item;
@@ -27,24 +30,26 @@ import java.lang.module.ModuleDescriptor;
 	}
 
 	@Override public void finalizeModElementGeneration() {
-		boolean newPath = ModuleDescriptor.Version.parse(
-						getModElement().getGeneratorConfiguration().getGeneratorMinecraftVersion())
-				.compareTo(ModuleDescriptor.Version.parse("1.21.2")) >= 0;
-		Workspace workspace = getModElement().getWorkspace();
-		FileIO.copyFile(new File(
-				GeneratorUtils.getSpecificRoot(workspace, workspace.getGeneratorConfiguration(), "mod_assets_root"),
-				"textures/models/armor/" + armorTextureFile + "_layer_1.png"), new File(
-				GeneratorUtils.getSpecificRoot(workspace, workspace.getGeneratorConfiguration(), "mod_assets_root"),
-				newPath ?
-						"textures/trims/entity/humanoid/" + getModElement().getRegistryName() + ".png" :
-						"textures/trims/models/armor/" + getModElement().getRegistryName() + ".png"));
-		FileIO.copyFile(new File(
-				GeneratorUtils.getSpecificRoot(workspace, workspace.getGeneratorConfiguration(), "mod_assets_root"),
-				"textures/models/armor/" + armorTextureFile + "_layer_2.png"), new File(
-				GeneratorUtils.getSpecificRoot(workspace, workspace.getGeneratorConfiguration(), "mod_assets_root"),
-				newPath ?
-						"textures/trims/entity/humanoid_leggings/" + getModElement().getRegistryName() + ".png" :
-						"textures/trims/models/armor/" + getModElement().getRegistryName() + "_leggings.png"));
+		try {
+			boolean newPath = ModuleDescriptor.Version.parse(
+							getModElement().getGeneratorConfiguration().getGeneratorMinecraftVersion())
+					.compareTo(ModuleDescriptor.Version.parse("1.21.2")) >= 0;
+			Workspace workspace = getModElement().getWorkspace();
+			File armorDirectory = getModElement().getFolderManager().getTexturesFolder(TextureType.ARMOR);
+			File texturesDirectory = getModElement().getFolderManager().getTexturesFolder(TextureType.OTHER);
+			FileIO.copyFile(
+					new File(armorDirectory,armorTextureFile + "_layer_1.png"),
+					new File(texturesDirectory, newPath
+					? "trims/entity/humanoid/" + getModElement().getRegistryName() + ".png"
+					: "trims/models/armor/" + getModElement().getRegistryName() + ".png"));
+			FileIO.copyFile(
+					new File(armorDirectory,armorTextureFile + "_layer_2.png"),
+					new File(texturesDirectory, newPath
+					? "trims/entity/humanoid_leggings/" + getModElement().getRegistryName() + ".png"
+					: "trims/models/armor/" + getModElement().getRegistryName() + "_leggings.png"));
+		} catch (Exception e) {
+			LOG.error("Failed to copy armor trim textures", e);
+		}
 	}
 
 }
