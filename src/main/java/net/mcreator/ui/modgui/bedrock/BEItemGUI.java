d7cc0bd546eec4c484fe92bc6b82931dfd76bb9e
Goldorion <38427877+Goldorion@users.noreply.github.com>
2026-03-05 16:30:29 -0500

    Use a single method to get both equipmentslots DL

--- src/main/java/net/mcreator/ui/modgui/bedrock/BEItemGUI.java
+++ src/main/java/net/mcreator/ui/modgui/bedrock/BEItemGUI.java
@@ -107,7 +107,7 @@ public class BEItemGUI extends ModElementGUI<BEItem> {
 	);
 
 	private final JCheckBox isEnchantable = new JCheckBox();
-	private final DataListComboBox enchantmentSlot = new DataListComboBox(mcreator, ElementUtil.loadAllBEEquipmentSlots());
+	private final DataListComboBox enchantmentSlot = new DataListComboBox(mcreator, ElementUtil.loadAllEquipmentSlots(mcreator.getWorkspace()));
 	private final JSpinner enchantmentValue = new JSpinner(new SpinnerNumberModel(0, 0, 255, 1));
 	private final JCheckBox diggerUseEfficiency = L10N.checkbox("elementgui.common.enable");
 	private final JDiggerList diggerEntries = new JDiggerList(mcreator, this);
