7c9c01922e8cfd193f7df2f09d343a2c020a8bb1
omegabird113 <omegabird113@outlook.com>
2026-03-07 17:00:49 -0500

    Update help text & help text code

--- plugins/mcreator-localization/help/default/common/mod_version.md
+++ plugins/mcreator-localization/help/default/common/mod_version.md
@@ -8,4 +8,3 @@ SemVer allows mods to have suffixes in their versions. These can be used to conv
 - 3.0.0rc
 - 1.0+1.21.1
 - 2.2.0-snapshot+mc1.21.8
-
--- src/main/java/net/mcreator/ui/dialogs/workspace/WorkspaceDialogs.java
+++ src/main/java/net/mcreator/ui/dialogs/workspace/WorkspaceDialogs.java
@@ -420,8 +420,7 @@ public class WorkspaceDialogs {
 					BorderFactory.createLineBorder(Theme.current().getAltBackgroundColor(), 1),
 					L10N.t("dialog.workspace_settings.section.details")));
 			_basicSettings.add(descriptionSettings);
-			descriptionSettings.add(PanelUtils.gridElements(1, 2, 0, 0,
-					HelpUtils.wrapWithHelpButton(IHelpContext.NONE.withEntry("common/mod_version"), L10N.label("dialog.workspace_settings.version"))));
+			descriptionSettings.add(HelpUtils.wrapWithHelpButton(IHelpContext.NONE.withEntry("common/mod_version"), L10N.label("dialog.workspace_settings.version")));
 			descriptionSettings.add(version);
 			descriptionSettings.add(L10N.label("dialog.workspace_settings.description"));
 			descriptionSettings.add(description);
