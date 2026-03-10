38841e11a6622ec99ade2745dda2aeb732db97a7
omegabird113 <omegabird113@outlook.com>
2026-03-10 14:45:33 -0400

    Use FlowLayout constants for alignment

--- src/main/java/net/mcreator/ui/dialogs/workspace/WorkspaceDialogs.java
+++ src/main/java/net/mcreator/ui/dialogs/workspace/WorkspaceDialogs.java
@@ -420,8 +420,8 @@ public class WorkspaceDialogs {
 					BorderFactory.createLineBorder(Theme.current().getAltBackgroundColor(), 1),
 					L10N.t("dialog.workspace_settings.section.details")));
 			_basicSettings.add(descriptionSettings);
-			descriptionSettings.add(PanelUtils.join(-1, 0, 0, L10N.label("dialog.workspace_settings.version"),
-					PanelUtils.join(HelpUtils.helpButton(IHelpContext.NONE.withEntry("common/mod_version")))));
+			descriptionSettings.add(PanelUtils.join(FlowLayout.LEFT, 0, 0, L10N.label("dialog.workspace_settings.version"),
+					PanelUtils.join(FlowLayout.RIGHT, HelpUtils.helpButton(IHelpContext.NONE.withEntry("common/mod_version")))));
 			descriptionSettings.add(version);
 			descriptionSettings.add(L10N.label("dialog.workspace_settings.description"));
 			descriptionSettings.add(description);
