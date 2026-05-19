0b394438f84d181d46c33f7c4e59706a5428d170
AlsoSomeoneElse <2000someoneelse@gmail.com>
2026-05-19 11:33:27 +0200

    Use Math.clamp in some places

--- src/main/java/net/mcreator/element/converter/v2023_4/StructureToFeatureConverter.java
+++ src/main/java/net/mcreator/element/converter/v2023_4/StructureToFeatureConverter.java
@@ -98,9 +98,9 @@ public class StructureToFeatureConverter implements IConverter {
 		if (spawnLocation.equals("Ground"))
 			spawnYOffset -= 1; // Old ground structures were 1 block lower (#4917)
 
-		spawnXOffset = Math.max(-47, Math.min(47, spawnXOffset));
-		spawnYOffset = Math.max(-47, Math.min(47, spawnYOffset));
-		spawnZOffset = Math.max(-47, Math.min(47, spawnZOffset));
+		spawnXOffset = Math.clamp(spawnXOffset, -47, 47);
+		spawnYOffset = Math.clamp(spawnYOffset, -47, 47);
+		spawnZOffset = Math.clamp(spawnZOffset, -47, 47);
 
 		boolean randomlyRotateStructure =
 				!definition.has("randomlyRotateStructure") || definition.get("randomlyRotateStructure").getAsBoolean();
