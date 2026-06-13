3ff0f393e344dd7f96e0095010303754dbb42286
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2026-06-13 22:00:31 +0200

    Default bounding box is full block

--- src/main/java/net/mcreator/element/types/Block.java
+++ src/main/java/net/mcreator/element/types/Block.java
@@ -253,7 +253,7 @@ import java.util.stream.Collectors;
 		this.blockSetType = "OAK";
 		this.leavesParticleChance = 0;
 		this.tintType = "No tint";
-		this.boundingBoxes = new ArrayList<>();
+		this.boundingBoxes = new ArrayList<>(List.of(new IBlockWithBoundingBox.BoxEntry()));
 		this.restrictionBiomes = new ArrayList<>();
 		this.reactionToPushing = "NORMAL";
 		this.slipperiness = 0.6;
