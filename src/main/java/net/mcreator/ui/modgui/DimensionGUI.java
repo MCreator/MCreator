521301d1811f76bdb2c9b00ce6d9972015cbbccb
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2026-08-05 17:38:15 +0200

    Improved UX for dimension effects to follow 26.1 logic

--- src/main/java/net/mcreator/ui/modgui/DimensionGUI.java
+++ src/main/java/net/mcreator/ui/modgui/DimensionGUI.java
@@ -312,8 +312,7 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		canRespawnHere.setOpaque(false);
 		doesWaterVaporize.setOpaque(false);
 		hasFixedTime.setOpaque(false);
-		hasFixedTime.addActionListener(_ -> fixedTimeValue.setEnabled(hasFixedTime.isSelected()));
-		fixedTimeValue.setEnabled(false);
+		hasFixedTime.addActionListener(_ -> updateDimensionEffectSettings());
 		if (!isEditingMode()) {
 			bedWorks.setSelected(true);
 			imitateOverworldBehaviour.setSelected(true);
@@ -324,7 +323,8 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		ambientLight.setPreferredSize(new java.awt.Dimension(-1, 36));
 
 		useCustomEffects.setOpaque(false);
-		useCustomEffects.addActionListener(_ -> updateDimensionEffectSettings(useCustomEffects.isSelected()));
+		useCustomEffects.addActionListener(_ -> updateDimensionEffectSettings());
+		defaultEffects.addActionListener(_ -> updateDimensionEffectSettings());
 		hasClouds.setOpaque(false);
 		hasClouds.addActionListener(_ -> cloudHeight.setEnabled(hasClouds.isSelected()));
 		airColor.setOpaque(false);
@@ -337,7 +337,7 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 			useCustomEffects.setSelected(modElement.getGeneratorConfiguration().getGeneratorFlavor().getBaseLanguage()
 					== GeneratorFlavor.BaseLanguage.JAVA);
 			hasClouds.setSelected(true);
-			updateDimensionEffectSettings(useCustomEffects.isSelected());
+			updateDimensionEffectSettings();
 		}
 
 		propertiesPage.add("Center", PanelUtils.totalCenterInPanel(
@@ -668,11 +668,20 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		igniterRarity.setEnabled(enabled);
 	}
 
-	private void updateDimensionEffectSettings(boolean hasCustomEffects) {
+	private void updateDimensionEffectSettings() {
+		boolean hasCustomEffects = useCustomEffects.isSelected();
 		defaultEffects.setEnabled(!hasCustomEffects);
 		airColor.setEnabled(hasCustomEffects);
 		sunHeightAffectsFog.setEnabled(hasCustomEffects);
 		hasFog.setEnabled(hasCustomEffects);
+
+		// Fixed time is only supported when default overworld effects are used
+		boolean supportsFixedTime = !hasCustomEffects && "overworld".equals(defaultEffects.getSelectedItem());
+		hasFixedTime.setEnabled(supportsFixedTime);
+		if (!supportsFixedTime) {
+			hasFixedTime.setSelected(false);
+		}
+		fixedTimeValue.setEnabled(supportsFixedTime && hasFixedTime.isSelected());
 	}
 
 	private void updateWorldgenSettings() {
@@ -705,7 +714,7 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 	}
 
 	private void updateSkyboxElements() {
-		boolean skyNone = skyType.getSelectedItem().equals("NONE");
+		boolean skyNone = "NONE".equals(skyType.getSelectedItem());
 		if (skyNone) {
 			enableCustomSkyboxTextures.setSelected(false);
 			enableCustomSunMoonTextures.setSelected(false);
@@ -806,9 +815,8 @@ public class DimensionGUI extends ModElementGUI<Dimension> {
 		sunTexture.setTexture(dimension.sunTexture);
 		moonTexture.setTexture(dimension.moonTexture);
 
-		fixedTimeValue.setEnabled(dimension.hasFixedTime);
 		updateWorldgenSettings();
-		updateDimensionEffectSettings(dimension.useCustomEffects);
+		updateDimensionEffectSettings();
 		updatePortalElements();
 		updateSkyboxElements();
 	}
