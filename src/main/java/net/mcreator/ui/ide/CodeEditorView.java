bf12e374708161c182926ccc4ef615f197a886f2
KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>
2026-08-06 22:18:12 +0200

    Missing boolean flag

--- src/main/java/net/mcreator/ui/ide/CodeEditorView.java
+++ src/main/java/net/mcreator/ui/ide/CodeEditorView.java
@@ -399,6 +399,8 @@ public class CodeEditorView extends ViewBase implements ISearchable {
 
 			te.addKeyListener(new KeyAdapter() {
 
+				private volatile boolean completionInAction = false;
+
 				@Override public void keyPressed(KeyEvent keyEvent) {
 					super.keyPressed(keyEvent);
 					if (keyEvent.getKeyCode() == KeyEvent.VK_CONTROL) {
