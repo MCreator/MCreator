a7687742e01277c2c4396774df0de21af9c6127b
Goldorion <38427877+Goldorion@users.noreply.github.com>
2026-08-22 12:25:42 -0400

    Replacer JS bridge by method

--- plugins/mcreator-core/blockly/js/simple_repeating_input_mixins.js
+++ plugins/mcreator-core/blockly/js/simple_repeating_input_mixins.js
@@ -327,7 +327,7 @@ Blockly.Extensions.registerMutator('player_predicate_mutator', simpleRepeatingIn
         'player_predicate_mutator_container', 'player_predicate_mutator_input', 'predicateComponent',
         function (thisBlock, inputName, index) {
             thisBlock.appendValueInput(inputName + index).setCheck('PlayerPredicateComponent').setAlign(Blockly.ALIGN_RIGHT)
-                .appendField(javabridge.t('blockly.block.' + thisBlock.type + '.input'));
+                .appendField(translate('blockly.block.' + thisBlock.type + '.input'));
         }),
     undefined, ['player_predicate_mutator_input'], false, false);
 
@@ -335,7 +335,7 @@ Blockly.Extensions.registerMutator('location_component_mutator', simpleRepeating
         'location_component_mutator_container', 'location_component_mutator_input', 'locationComponent',
         function (thisBlock, inputName, index) {
             thisBlock.appendValueInput(inputName + index).setCheck('LocationParameter').setAlign(Blockly.ALIGN_RIGHT)
-                .appendField(javabridge.t('blockly.block.' + thisBlock.type + '.input'));
+                .appendField(translate('blockly.block.' + thisBlock.type + '.input'));
         }),
     undefined, ['location_component_mutator_input']);
 
@@ -343,7 +343,7 @@ Blockly.Extensions.registerMutator('biome_location_component_mutator', simpleRep
         'biome_location_component_mutator_container', 'biome_location_component_mutator_input', 'biome',
         function (thisBlock, inputName, index) {
             thisBlock.appendDummyInput(inputName + index).setAlign(Blockly.ALIGN_RIGHT)
-                .appendField(javabridge.t('blockly.block.' + thisBlock.type + '.input'))
+                .appendField(translate('blockly.block.' + thisBlock.type + '.input'))
                 .appendField(new FieldDataListSelector('biomes'), 'biome' + index);
         }, false, ['biome']),
     undefined, ['biome_location_component_mutator_input']);
