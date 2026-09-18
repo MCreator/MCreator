05dc966e27198a8dbe5012be6fccfed885b70a2e
omegabird113 <omegabird113@outlook.com>
2026-09-18 09:48:38 -0400

    JS whitespace fixes

--- src/main/java/net/mcreator/ui/chromium/CefEventUtils.java
+++ src/main/java/net/mcreator/ui/chromium/CefEventUtils.java
@@ -101,22 +101,22 @@ final class CefEventUtils {
 			if (frame == null)
 				return false;
 			frame.executeJavaScript("""
-						(() => {
-							const el = document.activeElement;
-							if (!(el instanceof HTMLInputElement || el instanceof HTMLTextAreaElement))
-							  return;
-							if (el.selectionStart === null)
-							  return;                                                                                                                                               \s
-							const sel = window.getSelection();
-							if (el.selectionStart === el.selectionEnd) {
-							  sel.modify('extend', 'forward', 'lineboundary');
-							  if (el.selectionStart === el.selectionEnd)
+					(() => {
+						const el = document.activeElement;
+						if (!(el instanceof HTMLInputElement || el instanceof HTMLTextAreaElement))
+							return;
+						if (el.selectionStart === null)
+							return;
+						const sel = window.getSelection();
+						if (el.selectionStart === el.selectionEnd) {
+							sel.modify('extend', 'forward', 'lineboundary');
+							if (el.selectionStart === el.selectionEnd)
 								sel.modify('extend', 'forward', 'character');
-							  if (el.selectionStart === el.selectionEnd)
+							if (el.selectionStart === el.selectionEnd)
 								return;
-							}
-							document.execCommand('delete');
-						})();
+						}
+						document.execCommand('delete');
+					})();
 					""", "http://mcreator/cmd-delete-shortcut", 0);
 			return true;
 		}
